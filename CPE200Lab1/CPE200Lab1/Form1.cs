7bc9b3b20b0f778b76b0b2c84eb0ed8c8ae3e32d
natchap0n <natchap0n@users.noreply.github.com>
2018-08-14 16:44:44 +0700

    1.Complete all operation.

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -14,7 +14,8 @@ namespace CPE200Lab1
     {
         string firstOperand = null;
         string secondOperand = null;
-        bool setFirstOperand;
+        bool reset = false;
+        int operation;
 
         public Form1()
         {
@@ -29,12 +30,14 @@ namespace CPE200Lab1
         private void btnX_Click(object sender, EventArgs e)
         {
             Button btn = (Button)sender;
-            if (lblDisplay.Text == "0" || (firstOperand != null && secondOperand != null)
+            if (lblDisplay.Text == "0" || (firstOperand != null && reset == true))
             {
                 lblDisplay.Text = "";
             }
 
-            if(lblDisplay.Text.Length < 8)
+            reset = false;
+
+            if (lblDisplay.Text.Length < 8)
             {
                 lblDisplay.Text = lblDisplay.Text + btn.Text;
             }
@@ -44,14 +47,60 @@ namespace CPE200Lab1
         private void btnPlus_Click(object sender, EventArgs e)
         {
             firstOperand = lblDisplay.Text;
+            reset = true;
+            operation = 1;
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            firstOperand = lblDisplay.Text;
+            reset = true;
+            operation = 2;
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            firstOperand = lblDisplay.Text;
+            reset = true;
+            operation = 3;
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            firstOperand = lblDisplay.Text;
+            reset = true;
+            operation = 4;
         }
 
         private void btnEqual_Click(object sender, EventArgs e)
         {
-            string result;
+            string result1;
+            string result2;
+            string result3;
+            string result4;
             secondOperand = lblDisplay.Text;
-            result = (float.Parse(firstOperand) + float.Parse(secondOperand)).ToString;
-            lblDisplay.Text = result;
+            if (operation == 1)
+            {
+                result1 = (float.Parse(firstOperand) + float.Parse(secondOperand)).ToString();
+                lblDisplay.Text = result1;
+            }
+            else if (operation == 2)
+            {
+                result2 = (float.Parse(firstOperand) - float.Parse(secondOperand)).ToString();
+                lblDisplay.Text = result2;
+            }
+            else if (operation == 3)
+            {
+                result3 = (float.Parse(firstOperand) * float.Parse(secondOperand)).ToString();
+                lblDisplay.Text = result3;
+            }
+            else if (operation == 4)
+            {
+                result4 = (float.Parse(firstOperand) / float.Parse(secondOperand)).ToString();
+                lblDisplay.Text = result4;
+            }
+            
         }
+
     }
 }
