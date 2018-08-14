214d481710872bb07ef271591c2e20f71aa8ace8
Chayootpong Promlee <chayootpong.chrom@gmail.com>
2018-08-14 16:21:01 +0700

    Complete Button Numbers

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -12,9 +12,33 @@ namespace CPE200Lab1
 {
     public partial class Form1 : Form
     {
+        string first = null, second = null;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (lblDisplay.Text == "0") lblDisplay.Text = "";
+            if (lblDisplay.Text.Length < 8) lblDisplay.Text += btn.Text;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lblDisplay.Text = "0";
+        }
+
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
