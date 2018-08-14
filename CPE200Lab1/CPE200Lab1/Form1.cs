93291ab3a7d3fa21a3420072e45004665476b055
natchap0n <natchap0n@users.noreply.github.com>
2018-08-14 16:21:18 +0700

    1.Complete all number buttons. 2.Almost complete plus and equal.

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -12,9 +12,46 @@ namespace CPE200Lab1
 {
     public partial class Form1 : Form
     {
+        string firstOperand = null;
+        string secondOperand = null;
+        bool setFirstOperand;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void lblDisplay_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void btnX_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (lblDisplay.Text == "0" || (firstOperand != null && secondOperand != null)
+            {
+                lblDisplay.Text = "";
+            }
+
+            if(lblDisplay.Text.Length < 8)
+            {
+                lblDisplay.Text = lblDisplay.Text + btn.Text;
+            }
+            
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            firstOperand = lblDisplay.Text;
+        }
+
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+            string result;
+            secondOperand = lblDisplay.Text;
+            result = (float.Parse(firstOperand) + float.Parse(secondOperand)).ToString;
+            lblDisplay.Text = result;
         }
     }
 }
