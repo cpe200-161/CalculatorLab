8a31e83240008c3f4ddbef90c2e0e5fdf82ebdb9
NONTAWAT UDPROM <nontawat_udprom@cmu.ac.th>
2018-08-14 16:27:54 +0700

    1.complete all number  bottoms code.

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -12,9 +12,26 @@ namespace CPE200Lab1
 {
     public partial class Form1 : Form
     {
+		string firstOperand = null ;
+		string secondOperand = null ; 
         public Form1()
         {
             InitializeComponent();
         }
-    }
+		
+		}
+		private void btnx_Click(object sender, EventArgs e)
+		{
+			Button btn = (Button)sender;
+			if (lblDisplay.Text == "0")
+			{
+				lblDisplay.Text = "";
+
+			}
+			if (lblDisplay.Text.Length < 8)
+			{
+				lblDisplay.Text += btn.Text;
+			}
+		}
+	}
 }
