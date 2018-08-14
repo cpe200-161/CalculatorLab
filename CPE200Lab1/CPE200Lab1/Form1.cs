a6dec4b84ec9c0e7413470045055a66cfd6c064e
wogwhale <35363093+wongwhale@users.noreply.github.com>
2018-08-14 16:25:11 +0700

    1.Complete all number button code.

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -10,11 +10,42 @@ using System.Windows.Forms;
 
 namespace CPE200Lab1
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
-        }
-    }
+	public partial class Form1 : Form
+	{
+		string firstOperand;
+		string secondOperand;
+		bool setFirstOperand;
+		bool setSecondOperand;
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void btnx_Click(object sender, EventArgs e)
+		{
+			
+
+			Button btn = (Button)sender;
+
+			if (lblDisplay.Text == "0" || firstOperand == "")
+			{
+				lblDisplay.Text = "";
+			}
+			if (lblDisplay.Text.Length < 8)
+			{
+				lblDisplay.Text += btn.Text;
+			}
+		}
+
+		private void btnPlus_Click(object sender, EventArgs e)
+		{
+			firstOperand = lblDisplay.Text;
+			setFirstOperand = true;
+		}
+
+		private void btnEqual_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
 }
