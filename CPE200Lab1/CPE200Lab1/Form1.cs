35a4c7f9a485debf1a1285628b896c278808b2ae
gamecup <gamecup84@gmail.com>
2018-08-14 16:21:07 +0700

    1.complete number button

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -12,9 +12,42 @@ namespace CPE200Lab1
 {
     public partial class Form1 : Form
     {
+        string firstOperand = null;
+        string secandOperand = null;
+        bool setfirstOperand = false;
+        bool setsecandOperand = false;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnx_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "";
+            }
+
+            if (lblDisplay.Text.Length < 8)
+            {
+                lblDisplay.Text += btn.Text;
+            }
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            firstOperand = lblDisplay.Text;
+            setfirstOperand = true;
+        }
+
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+            string result;
+            secandOperand = lblDisplay.Text;
+            result = (float.Parse(firstOperand) + float.Parse(secandOperand)).ToString();
+            lblDisplay.Text = result;
+
         }
     }
 }
