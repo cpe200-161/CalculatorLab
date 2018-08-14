3554332c276b2d602200aad63fec8e726d0309e3
macropusgiganteus <35326900+macropusgiganteus@users.noreply.github.com>
2018-08-14 16:49:42 +0700

    1.All numer button code. 2.Plus operand.

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -12,9 +12,39 @@ namespace CPE200Lab1
 {
     public partial class Form1 : Form
     {
+        string SecondOperand = null;
+        private string firstOperand;
+        bool setFirstOperand = false;
+        bool isStartSecondOperand = false;
         public Form1()
         {
             InitializeComponent();
         }
+        private void btnX_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (lblDisplay.Text == "0") { lblDisplay.Text = ""; }
+            if(setFirstOperand ==true && isStartSecondOperand == false)
+            {
+                lblDisplay.Text = "";
+                isStartSecondOperand = true;
+            }
+            if(lblDisplay.Text.Length < 8)
+            lblDisplay.Text += btn.Text ;
+            
+        }
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            firstOperand = lblDisplay.Text;
+            setFirstOperand = true;
+        }
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+            string result;
+            SecondOperand = lblDisplay.Text;
+            result = (float.Parse(firstOperand) + float.Parse(SecondOperand)).ToString();
+            lblDisplay.Text = result;
+        }
+
     }
 }
