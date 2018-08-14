83f956f06a9357180e404e320952f72f5b307668
ananaum <35338169+ananaum@users.noreply.github.com>
2018-08-14 16:23:50 +0700

    เพิ่ม + - * / .

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -12,9 +12,85 @@ namespace CPE200Lab1
 {
     public partial class Form1 : Form
     {
+        string input = string.Empty;
+        string num1 = string.Empty;
+        string num2 = string.Empty;
+        char operation;
+        double result = 0.0;
         public Form1()
         {
             InitializeComponent();
+        }
+        private void btnX_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            this.lblDisplay.Text = "";
+            input += btn.Text;
+            this.lblDisplay.Text += input;
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            num1 = input;
+            operation = '+';
+            input = string.Empty;
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            num1 = input;
+            operation = '-';
+            input = string.Empty;
+
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            num1 = input;
+            operation = '*';
+            input = string.Empty;
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            num1 = input;
+            operation = '/';
+            input = string.Empty;
+        }
+        
+        
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+            num2 = input;
+            if (operation == '+')
+            {
+                result = Convert.ToDouble(num1) + Convert.ToDouble(num2);
+            }else if (operation == '-')
+            {
+                result = Convert.ToDouble(num1) - Convert.ToDouble(num2);
+            }
+            else if (operation == '*')
+            {
+                result = Convert.ToDouble(num1) * Convert.ToDouble(num2);
+            }
+            else if (operation == '/')
+            {
+                result = Convert.ToDouble(num1) / Convert.ToDouble(num2);
+            }
+            
+            input = string.Empty;
+            this.lblDisplay.Text = result.ToString();
+        }
+
+
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            input = string.Empty;
+            num1 = string.Empty;
+            num2 = string.Empty;
+            result = 0;
+            this.lblDisplay.Text = "0";
         }
     }
 }
