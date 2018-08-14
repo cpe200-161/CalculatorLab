f95a5b8b39b73a3bac208f1bbdac19e32aa1cb25
AphisitKh <35342134+AphisitKh@users.noreply.github.com>
2018-08-14 16:43:02 +0700

    1. set number. 2. plus.

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -12,9 +12,60 @@ namespace CPE200Lab1
 {
     public partial class Form1 : Form
     {
+        string firstOperand;
+        string secondOperand;
+        bool setFirstOperand = false;
+        bool isStartSecondOperand = false;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+            string result;
+            secondOperand = lblDisplay.Text;
+            result = (float.Parse(firstOperand) + float.Parse(secondOperand)).ToString();
+            lblDisplay.Text = result;
+        }
+
+        private void btnX_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (lblDisplay.Text == "0" )
+            {
+                lblDisplay.Text = "";
+            }
+            if(setFirstOperand == true && isStartSecondOperand == false)
+            {
+                lblDisplay.Text = null;
+                isStartSecondOperand = true;
+            }
+            if (lblDisplay.Text.Length < 8)
+            {
+                lblDisplay.Text += btn.Text;
+            }    
+        }
+
+        private void btnDot_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            firstOperand = lblDisplay.Text;
+            setFirstOperand = true;
         }
     }
 }
