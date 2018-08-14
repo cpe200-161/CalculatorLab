d5282d72ebca44b970322c267d586d462f55f341
Rakpong Thorhun <rakpong_thorhun@cmu.ac.th>
2018-08-15 01:26:12 +0700

    numBotton CButton and DotBotton

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -12,9 +12,35 @@ namespace CPE200Lab1
 {
     public partial class Form1 : Form
     {
+        float temp = 0;
+        float num1, num2;
+        string display = "0";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnDot_Click(object sender, EventArgs e)
+        {
+            lblDisplay.Text += btnDot.Text;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lblDisplay.Text = " ";
+        }
+
+        private void btnClick(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (display == "0")
+            {
+                lblDisplay.Text = " ";
+            }
+            lblDisplay.Text += btn.Text;
+            display = lblDisplay.Text;
+
         }
     }
 }
