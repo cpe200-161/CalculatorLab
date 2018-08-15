b1393351a982241864b9c798ce2a18d398c65817
gamecup <gamecup84@gmail.com>
2018-08-15 23:42:49 +0700

    Done

--- CPE200Lab1/CPE200Lab1/Form1.Designer.cs
+++ CPE200Lab1/CPE200Lab1/Form1.Designer.cs
@@ -93,6 +93,7 @@
             this.btnMultiply.TabIndex = 3;
             this.btnMultiply.Text = "X";
             this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
             // 
             // btnMinus
             // 
@@ -103,6 +104,7 @@
             this.btnMinus.TabIndex = 7;
             this.btnMinus.Text = "-";
             this.btnMinus.UseVisualStyleBackColor = true;
+            this.btnMinus.Click += new System.EventHandler(this.btnMinus_Click);
             // 
             // btn6
             // 
@@ -201,6 +203,7 @@
             this.btnDot.TabIndex = 14;
             this.btnDot.Text = ".";
             this.btnDot.UseVisualStyleBackColor = true;
+            this.btnDot.Click += new System.EventHandler(this.btnDot_Click);
             // 
             // btn0
             // 
@@ -222,6 +225,7 @@
             this.btnSign.TabIndex = 12;
             this.btnSign.Text = "±";
             this.btnSign.UseVisualStyleBackColor = true;
+            this.btnSign.Click += new System.EventHandler(this.btnSign_Click);
             // 
             // btnDivide
             // 
@@ -232,6 +236,7 @@
             this.btnDivide.TabIndex = 16;
             this.btnDivide.Text = "÷";
             this.btnDivide.UseVisualStyleBackColor = true;
+            this.btnDivide.Click += new System.EventHandler(this.btnDivide_Click);
             // 
             // btnPercent
             // 
@@ -242,6 +247,7 @@
             this.btnPercent.TabIndex = 17;
             this.btnPercent.Text = "%";
             this.btnPercent.UseVisualStyleBackColor = true;
+            this.btnPercent.Click += new System.EventHandler(this.btnPercent_Click);
             // 
             // btnClear
             // 
@@ -252,6 +258,7 @@
             this.btnClear.TabIndex = 18;
             this.btnClear.Text = "C";
             this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
             // 
             // btnBack
             // 
@@ -262,6 +269,7 @@
             this.btnBack.TabIndex = 19;
             this.btnBack.Text = "<";
             this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
             // 
             // lblDisplay
             // 
--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -13,9 +13,17 @@ namespace CPE200Lab1
     public partial class Form1 : Form
     {
         string firstOperand = null;
-        string secandOperand = null;
+        string secondOperand = null;
+
         bool setfirstOperand = false;
-        bool setsecandOperand = false;
+        //bool isStartSecondOperand = false;
+        bool plus = false;
+        bool minus = false;
+        bool muti = false;
+        bool divide = false;
+
+        string result = null;
+
         public Form1()
         {
             InitializeComponent();
@@ -28,6 +36,12 @@ namespace CPE200Lab1
             {
                 lblDisplay.Text = "";
             }
+            if (setfirstOperand == true)
+            {
+                lblDisplay.Text = "";
+            //    isStartSecondOperand = false;
+                setfirstOperand = false;
+            }
 
             if (lblDisplay.Text.Length < 8)
             {
@@ -37,17 +51,143 @@ namespace CPE200Lab1
 
         private void btnPlus_Click(object sender, EventArgs e)
         {
-            firstOperand = lblDisplay.Text;
-            setfirstOperand = true;
+            if (firstOperand != null)
+            {
+                secondOperand = lblDisplay.Text;
+                result = (float.Parse(firstOperand) + float.Parse(secondOperand)).ToString();
+                lblDisplay.Text = result;
+                firstOperand = result;
+            }
+            else
+            {
+                firstOperand = lblDisplay.Text;
+                setfirstOperand = true;
+            }
+            plus = true;
+            minus = false;
+            muti = false;
+            divide = false;
         }
 
         private void btnEqual_Click(object sender, EventArgs e)
         {
-            string result;
-            secandOperand = lblDisplay.Text;
-            result = (float.Parse(firstOperand) + float.Parse(secandOperand)).ToString();
+            secondOperand = lblDisplay.Text;
+            if (plus == true)
+            {
+                result = (float.Parse(firstOperand) + float.Parse(secondOperand)).ToString();
+            }
+            else if(minus == true)
+            {
+                result = (float.Parse(firstOperand) - float.Parse(secondOperand)).ToString();
+            }
+            else if(muti == true)
+            {
+                result = (float.Parse(firstOperand) * float.Parse(secondOperand)).ToString();
+            }
+            else if(divide == true)
+            {
+                result = (float.Parse(firstOperand) / float.Parse(secondOperand)).ToString();
+            }
             lblDisplay.Text = result;
 
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lblDisplay.Text = "0";
+            firstOperand = null;
+            secondOperand = null;
+            setfirstOperand = false;
+            //    isStartsecondOperand = false;
+
+            plus = false;
+            minus = false;
+            muti = false;
+            divide = false;
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            if (setfirstOperand == true)
+            {
+                secondOperand = lblDisplay.Text;
+                firstOperand = (float.Parse(firstOperand) / float.Parse(secondOperand)).ToString();
+            }
+            else
+            {
+                firstOperand = lblDisplay.Text;
+                setfirstOperand = true;
+            }
+            plus = false;
+            minus = false;
+            muti = false;
+            divide = true;
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            if (setfirstOperand == true)
+            {
+                secondOperand = lblDisplay.Text;
+                firstOperand = (float.Parse(firstOperand) * float.Parse(secondOperand)).ToString();
+            }
+            else
+            {
+                firstOperand = lblDisplay.Text;
+                setfirstOperand = true;
+            }
+            plus = false;
+            minus = false;
+            muti = true;
+            divide = false;
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            if (setfirstOperand == true)
+            {
+                secondOperand = lblDisplay.Text;
+                firstOperand = (float.Parse(firstOperand) - float.Parse(secondOperand)).ToString();
+            }
+            else
+            {
+                firstOperand = lblDisplay.Text;
+                setfirstOperand = true;
+            }
+            plus = false;
+            minus = true;
+            muti = false;
+            divide = false;
+        }
+
+        private void btnPercent_Click(object sender, EventArgs e)
+        {
+            lblDisplay.Text = (float.Parse(lblDisplay.Text)/100).ToString();
+        }
+
+        private void btnSign_Click(object sender, EventArgs e)
+        {
+            lblDisplay.Text = (float.Parse(lblDisplay.Text) * (-1)).ToString();
+        }
+
+        private void btnDot_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text.Contains('.') == false)
+            {
+                lblDisplay.Text += ".";
+            }
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text.Length == 1)
+            {
+                lblDisplay.Text = "0";
+            }
+            else
+            {
+                lblDisplay.Text = lblDisplay.Text.Remove(lblDisplay.Text.Length - 1);
+            }
         }
     }
 }
