a41529d2b0a4266ffdfe957755793eca16683103
ElevaterCPE26 <oipz2001@gmail.com>
2018-08-15 15:27:23 +0700

    Complete all number

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -17,16 +17,19 @@ namespace CPE200Lab1
             InitializeComponent();
         }
 
-        
+        private void btnX_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if(lblDisplay.Text =="0" && lblDisplay.) { lblDisplay.Text = ""; }
+            lblDisplay.Text += btn.Text;
 
-		private void btn1_Click(object sender, EventArgs e)
-		{
-            if(lblDisplay.Text=="0")
-            {
-                lblDisplay.Text = "";
-            }
-			lblDisplay.Text += "1";
-		}
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            
+
+        }
 
         private void lblDisplay_Click(object sender, EventArgs e)
         {
@@ -35,83 +38,47 @@ namespace CPE200Lab1
 
         private void btn2_Click(object sender, EventArgs e)
         {
-            if (lblDisplay.Text == "0")
-            {
-                lblDisplay.Text = "";
-            }
-            lblDisplay.Text += "2";
+            
         }
 
         private void btn3_Click(object sender, EventArgs e)
         {
-            if (lblDisplay.Text == "0")
-            {
-                lblDisplay.Text = "";
-            }
-            lblDisplay.Text += "3";
+            
         }
 
         private void btn4_Click(object sender, EventArgs e)
         {
-            if (lblDisplay.Text == "0")
-            {
-                lblDisplay.Text = "";
-            }
-            lblDisplay.Text += "4";
+            
         }
 
         private void btn5_Click(object sender, EventArgs e)
         {
-            if (lblDisplay.Text == "0")
-            {
-                lblDisplay.Text = "";
-            }
-            lblDisplay.Text += "5";
+            
         }
 
         private void btn6_Click(object sender, EventArgs e)
         {
-            if (lblDisplay.Text == "0")
-            {
-                lblDisplay.Text = "";
-            }
-            lblDisplay.Text += "6";
+            
         }
 
         private void btn7_Click(object sender, EventArgs e)
         {
-            if (lblDisplay.Text == "0")
-            {
-                lblDisplay.Text = "";
-            }
-            lblDisplay.Text += "7";
+            
         }
 
         private void btn8_Click(object sender, EventArgs e)
         {
-            if (lblDisplay.Text == "0")
-            {
-                lblDisplay.Text = "";
-            }
-            lblDisplay.Text += "8";
+            
         }
 
         private void btn9_Click(object sender, EventArgs e)
         {
-            if (lblDisplay.Text == "0")
-            {
-                lblDisplay.Text = "";
-            }
-            lblDisplay.Text += "9";
+            
         }
 
         private void btnPlus_Click(object sender, EventArgs e)
         {
-            if (lblDisplay.Text == "0")
-            {
-                lblDisplay.Text = "";
-            }
-            lblDisplay.Text += "+";
+            
         }
 
         private void btnBack_Click(object sender, EventArgs e)
@@ -121,47 +88,32 @@ namespace CPE200Lab1
 
         private void btnMinus_Click(object sender, EventArgs e)
         {
-            if (lblDisplay.Text == "0")
-            {
-                lblDisplay.Text = "";
-            }
-            lblDisplay.Text += "-";
+            
         }
 
         private void btnMultiply_Click(object sender, EventArgs e)
         {
-            if (lblDisplay.Text == "0")
-            {
-                lblDisplay.Text = "";
-            }
-            lblDisplay.Text += "*";
+            
         }
 
         private void btnDivide_Click(object sender, EventArgs e)
         {
-            if (lblDisplay.Text == "0")
-            {
-                lblDisplay.Text = "";
-            }
-            lblDisplay.Text += "/";
+            
         }
 
         private void btnPercent_Click(object sender, EventArgs e)
         {
-            if (lblDisplay.Text == "0")
-            {
-                lblDisplay.Text = "";
-            }
-            lblDisplay.Text += "%";
+            
         }
 
         private void btnDot_Click(object sender, EventArgs e)
         {
-            if (lblDisplay.Text == "0")
-            {
-                lblDisplay.Text = "";
-            }
-            lblDisplay.Text += ".";
+            
+        }
+
+        private void operaTor(object sender, EventArgs e)
+        {
+
         }
     }
 }
