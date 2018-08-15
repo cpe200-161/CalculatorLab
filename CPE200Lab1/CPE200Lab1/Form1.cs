35d2b399ae89a9f5b35234d8a6524e731e07b451
PANNAWIT PANWONG <pannawit_panwong@cmu.ac.th>
2018-08-15 14:37:07 +0700

    Calculation only 2 data (+,-,*,/ and %)

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -12,9 +12,80 @@ namespace CPE200Lab1
 {
     public partial class Form1 : Form
     {
+        string first_data, second_data , Symbol , percent_num , sum;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "";
+            }
+            if(lblDisplay.Text.Length < 8)
+            {
+                lblDisplay.Text += btn.Text;
+            }
+        }
+
+        private void btnPercent_Click(object sender, EventArgs e)
+        {
+         
+            percent_num = (float.Parse(first_data) * float.Parse(lblDisplay.Text) / 100).ToString();
+            lblDisplay.Text = percent_num;
+        }
+
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+            second_data = lblDisplay.Text;
+
+            if (first_data != null && second_data != null)
+            {
+                lblDisplay.Text = "";
+                if (Symbol == "+")
+                {
+                    sum = (float.Parse(first_data) + float.Parse(second_data)).ToString();
+                    lblDisplay.Text = sum;
+                }
+                else if (Symbol == "-")
+                {
+                    sum = (float.Parse(first_data) - float.Parse(second_data)).ToString();
+                    lblDisplay.Text = sum;
+                }
+                else if (Symbol == "X")
+                {
+                    sum = (float.Parse(first_data) * float.Parse(second_data)).ToString();
+                    lblDisplay.Text = sum;
+                }
+                else if (Symbol == "÷")
+                {
+                    sum = (float.Parse(first_data) / float.Parse(second_data)).ToString();
+                    lblDisplay.Text = sum;
+                }
+                first_data = null;
+                second_data = null;
+                //lblDisplay.Text = "";
+            }
+
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            first_data = lblDisplay.Text;
+            lblDisplay.Text = "";
+            Symbol = btn.Text ;
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lblDisplay.Text = "0";
+            first_data = "";
+            second_data = "";
         }
     }
 }
