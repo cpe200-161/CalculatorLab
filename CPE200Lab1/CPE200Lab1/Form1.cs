81b9f556202292c60a1341704b75ff169ad5ecda
NirushaaaR <nirush_srijaimoon@cmu.ac.th>
2018-08-17 15:33:15 +0700

    add button action

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -12,9 +12,154 @@ namespace CPE200Lab1
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "";
+            }
+
+            if (lblDisplay.Text.Length < 8)
+            {
+                lblDisplay.Text += "1";
+            }
+
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "";
+            }
+
+            if (lblDisplay.Text.Length <= 8)
+            {
+                lblDisplay.Text += "2";
+            }
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "";
+            }
+
+            if (lblDisplay.Text.Length <= 8)
+            {
+                lblDisplay.Text += "3";
+            }
+            
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "";
+            }
+
+            if (lblDisplay.Text.Length <= 8)
+            {
+                lblDisplay.Text += "4";
+            }
+
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "";
+            }
+
+            if (lblDisplay.Text.Length <= 8)
+            {
+                lblDisplay.Text += "5";
+            }
+
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "";
+            }
+
+            if (lblDisplay.Text.Length <= 8)
+            {
+                lblDisplay.Text += "6";
+            }
+
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "";
+            }
+
+            if (lblDisplay.Text.Length <= 8)
+            {
+                lblDisplay.Text += "7";
+            }
+
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "";
+            }
+
+            if (lblDisplay.Text.Length <= 8)
+            {
+                lblDisplay.Text += "8";
+            }
+
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "";
+            }
+
+            if (lblDisplay.Text.Length <= 8)
+            {
+                lblDisplay.Text += "9";
+            }
+
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "";
+            }
+
+            if (lblDisplay.Text.Length <= 8)
+            {
+                lblDisplay.Text += "0";
+            }
+        }
+
+        
     }
 }
