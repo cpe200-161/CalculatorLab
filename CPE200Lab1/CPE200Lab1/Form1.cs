0d473a80be19d6c1e197db08036b67071a6669f7
phumthai <31661931+phumthai@users.noreply.github.com>
2018-08-17 15:27:17 +0700

    complete number button

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -12,9 +12,44 @@ namespace CPE200Lab1
 {
     public partial class Form1 : Form
     {
+        string operation = "";
+        Double result = 0;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void num_Button(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text == "0")
+                lblDisplay.Text = "";
+            if (lblDisplay.Text.Length <= 8)
+            {
+                Button num = (Button)sender;
+                lblDisplay.Text = lblDisplay.Text + num.Text;
+            }
+        }
+
+        private void operation_Button(object sender, EventArgs e)
+        {
+            Button operat = (Button)sender;
+            operation = operat.Text;
+            result = Double.Parse(lblDisplay.Text);
+            lblDisplay.Text = "";
+        }
+
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+            switch (operation)
+            {
+                case "+":
+                    lblDisplay.Text = result + double.Parse(lblDisplay.Text);
+                    break;
+
+
+
+
+            }
         }
     }
 }
