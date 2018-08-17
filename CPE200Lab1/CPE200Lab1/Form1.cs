b30ef8ea10bad12a424d505b7652aa890e319311
VACHIRA SUKKAPAT <vachira_s@cmu.ac.th>
2018-08-17 15:36:55 +0700

    ทำปุ่ม

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -16,5 +16,62 @@ namespace CPE200Lab1
         {
             InitializeComponent();
         }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "";
+            }
+            if (lblDisplay.Text.Length < 8) 
+            lblDisplay.Text = lblDisplay.Text + "1";
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            lblDisplay.Text = "2";
+          
+        }
+           
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            lblDisplay.Text = "3";
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            lblDisplay.Text = "4";
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            lblDisplay.Text = "5";
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            lblDisplay.Text = "6";
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            lblDisplay.Text = "7";
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            lblDisplay.Text = "8";
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            lblDisplay.Text = "9";
+        }
+
+        private void lblDisplay_Click(object sender, EventArgs e)
+        {
+          
+        }
     }
 }
