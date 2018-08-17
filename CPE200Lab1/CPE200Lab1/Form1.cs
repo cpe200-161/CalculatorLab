faccbbd1e7a1bc12b22e2f23f65b7fdcaae7a5c4
Thanawong Saneewong Na Ayutthaya <tomta555@hotmail.com>
2018-08-17 17:58:15 +0700

    add plus function

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -12,9 +12,80 @@ namespace CPE200Lab1
 {
     public partial class Form1 : Form
     {
+        float first;
+        float second;
+        float result;
+        bool recheck = true;
+        int pluscheck=0;
+        string show;
+        
+
         public Form1()
         {
             InitializeComponent();
+        }
+        private void btnX_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (pluscheck == 1 || pluscheck == 2)
+            {
+                if (recheck)
+                {
+                    lblDisplay.Text = "";
+                }
+                recheck=false;
+            }
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "";
+            }
+            if (lblDisplay.Text.Length <= 8)
+            {
+                lblDisplay.Text = lblDisplay.Text + btn.Text;
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lblDisplay.Text = "0";
+            pluscheck = 0;
+            first = 0;
+            second = 0;
+        }
+
+        private void btnDot_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            pluscheck = pluscheck + 1;
+            
+            if (pluscheck == 1)
+            {
+                first = float.Parse(lblDisplay.Text);
+                lblDisplay.Text = "";
+                lblDisplay.Text = first.ToString();
+            }
+            else if (pluscheck == 2)
+            {
+                second = float.Parse(lblDisplay.Text);
+                result = first + second;
+                show = result.ToString();
+                lblDisplay.Text = show;
+                first = result;
+                pluscheck = 1;
+                recheck = true;
+            }
+
+            
+        }
+
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+
+
         }
     }
 }
