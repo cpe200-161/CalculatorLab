92fd93a7977c9fd4b027dd7f5000e3c458158ebb
PAKHATAI KANTANG <pakhatai_kantang@cmu.ac.th>
2018-08-17 16:57:33 +0700

    600612161

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -12,9 +12,116 @@ namespace CPE200Lab1
 {
     public partial class Form1 : Form
     {
+        private double  val = 0;
+        private double je1,je2;
+        
         public Form1()
         {
             InitializeComponent();
+        }
+       
+ 
+        private void display(){
+            lblDisplay.Text = val.ToString();
+        }
+
+        private void numclick(int n)
+        {
+            val = val * 10 + n;
+            display();
+        }
+        
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            numclick(1);
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            numclick(2);
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            numclick(3);
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            numclick(4);
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            numclick(5);
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            numclick(6);
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            numclick(7);
+        }
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            numclick(8);
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            numclick(9);
+        }
+
+        private void lblDisplay_Click(object sender, EventArgs e)
+        {
+            
+           
+        }
+        private void btnSign_Click(object sender, EventArgs e)
+        {
+            val = (val * (-1));
+            lblDisplay.Text = val.ToString();
+        }
+       
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+             je1 = val;//จำ
+             //val = je2 ไม่จำเป็น;
+             val = 0;
+             je2 += je1;
+             lblDisplay.Text = je2.ToString();
+             
+        }
+        private void btnDot_Click(object sender, EventArgs e)
+            {
+            
+            }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            val = 0;
+            lblDisplay.Text = val.ToString();
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            je1 = val;//จำ
+            val = 0;
+            je2 -= je1;
+            lblDisplay.Text = je2.ToString();
+            
+        }
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+           
         }
     }
 }
