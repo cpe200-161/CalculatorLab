84924fdd203ae1eea20c13f2f2eb5c0b4c8402b8
THUT CHAYASATIT <thut_ch@cmu.ac.th>
2018-08-17 15:27:57 +0700

    code botton 0-9

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -16,5 +16,130 @@ namespace CPE200Lab1
         {
             InitializeComponent();
         }
+
+        private void btn1_Click_1(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "";
+            }
+            if (lblDisplay.Text.Length <= 8)
+            {
+                lblDisplay.Text = lblDisplay.Text + "หนึ่ง";
+            }
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "";
+            }
+            if (lblDisplay.Text.Length <= 8)
+            {
+                lblDisplay.Text = lblDisplay.Text + "สอง";
+            }
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "";
+            }
+            if (lblDisplay.Text.Length <= 8)
+            {
+                lblDisplay.Text = lblDisplay.Text + "สาม";
+            }
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "";
+            }
+            if (lblDisplay.Text.Length <= 8)
+            {
+                lblDisplay.Text = lblDisplay.Text + "สี่";
+            }
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "";
+            }
+            if (lblDisplay.Text.Length <= 8)
+            {
+                lblDisplay.Text = lblDisplay.Text + "ห้า";
+            }
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "";
+            }
+            if (lblDisplay.Text.Length <= 8)
+            {
+                lblDisplay.Text = lblDisplay.Text + "หก";
+            }
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "";
+            }
+            if (lblDisplay.Text.Length <= 8)
+            {
+                lblDisplay.Text = lblDisplay.Text + "เจ็ด";
+            }
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "";
+            }
+            if (lblDisplay.Text.Length <= 8)
+            {
+                lblDisplay.Text = lblDisplay.Text + "แปด";
+            }
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "";
+            }
+            if (lblDisplay.Text.Length <= 8)
+            {
+                lblDisplay.Text = lblDisplay.Text + "เก้า";
+            }
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "";
+            }
+            if (lblDisplay.Text.Length <= 8)
+            {
+                lblDisplay.Text = lblDisplay.Text + "0";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
