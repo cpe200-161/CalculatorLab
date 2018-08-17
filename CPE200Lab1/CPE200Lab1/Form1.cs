78b204ba1754477031f757831174e61d750c5c16
TimingKidz <35361317+TimingKidz@users.noreply.github.com>
2018-08-17 17:10:23 +0700

    Complete btn+, -, *, /, =

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -12,128 +12,93 @@ namespace CPE200Lab1
 {
     public partial class Form1 : Form
     {
+        double FirstNumber;
+        String Operator;
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void btn1_Click(object sender, EventArgs e)
+        private void btnX_Click(object sender, EventArgs e)
         {
-            if(lblDisplay.Text == "0")
-            {
-                lblDisplay.Text = "";
-            }
-            if(lblDisplay.Text.Length <= 8)
-            {
-                lblDisplay.Text = lblDisplay.Text + "1";
-            }
-        }
-
-        private void btn2_Click(object sender, EventArgs e)
-        {
+            Button btn = (Button)sender;
             if (lblDisplay.Text == "0")
             {
                 lblDisplay.Text = "";
             }
             if (lblDisplay.Text.Length <= 8)
             {
-                lblDisplay.Text = lblDisplay.Text + "2";
+                if (Operator == "+" || Operator == "-" || Operator == "*" || Operator == "/")
+                {
+                    lblDisplay.Text = btn.Text;
+                }
+                else
+                {
+                    lblDisplay.Text = lblDisplay.Text + btn.Text;
+                }
             }
         }
 
-        private void btn3_Click(object sender, EventArgs e)
+        private void btnPlus_Click(object sender, EventArgs e)
         {
-            if (lblDisplay.Text == "0")
-            {
-                lblDisplay.Text = "";
-            }
-            if (lblDisplay.Text.Length <= 8)
-            {
-                lblDisplay.Text = lblDisplay.Text + "3";
-            }
+            FirstNumber = Convert.ToDouble(lblDisplay.Text);
+            Operator = "+";
         }
 
-        private void btn4_Click(object sender, EventArgs e)
+        private void btnMinus_Click(object sender, EventArgs e)
         {
-            if (lblDisplay.Text == "0")
-            {
-                lblDisplay.Text = "";
-            }
-            if (lblDisplay.Text.Length <= 8)
-            {
-                lblDisplay.Text = lblDisplay.Text + "4";
-            }
+            FirstNumber = Convert.ToDouble(lblDisplay.Text);
+            Operator = "-";
         }
 
-        private void btn5_Click(object sender, EventArgs e)
+        private void btnMultiply_Click(object sender, EventArgs e)
         {
-            if (lblDisplay.Text == "0")
-            {
-                lblDisplay.Text = "";
-            }
-            if (lblDisplay.Text.Length <= 8)
-            {
-                lblDisplay.Text = lblDisplay.Text + "5";
-            }
+            FirstNumber = Convert.ToDouble(lblDisplay.Text);
+            Operator = "*";
         }
 
-        private void btn6_Click(object sender, EventArgs e)
+        private void btnDivide_Click(object sender, EventArgs e)
         {
-            if (lblDisplay.Text == "0")
-            {
-                lblDisplay.Text = "";
-            }
-            if (lblDisplay.Text.Length <= 8)
-            {
-                lblDisplay.Text = lblDisplay.Text + "6";
-            }
+            FirstNumber = Convert.ToDouble(lblDisplay.Text);
+            Operator = "/";
         }
 
-        private void btn7_Click(object sender, EventArgs e)
+        private void btnEqual_Click(object sender, EventArgs e)
         {
-            if (lblDisplay.Text == "0")
-            {
-                lblDisplay.Text = "";
-            }
-            if (lblDisplay.Text.Length <= 8)
-            {
-                lblDisplay.Text = lblDisplay.Text + "7";
-            }
-        }
+            double SecondNumber;
+            double Result;
 
-        private void btn8_Click(object sender, EventArgs e)
-        {
-            if (lblDisplay.Text == "0")
+            SecondNumber = Convert.ToDouble(lblDisplay.Text);
+            if (Operator == "+")
             {
-                lblDisplay.Text = "";
+                Result = (FirstNumber + SecondNumber);
+                lblDisplay.Text = Convert.ToString(Result);
+                FirstNumber = Result;
             }
-            if (lblDisplay.Text.Length <= 8)
+            else if (Operator == "-")
             {
-                lblDisplay.Text = lblDisplay.Text + "8";
+                Result = (FirstNumber - SecondNumber);
+                lblDisplay.Text = Convert.ToString(Result);
+                FirstNumber = Result;
             }
-        }
-
-        private void btn9_Click(object sender, EventArgs e)
-        {
-            if (lblDisplay.Text == "0")
+            else if (Operator == "*")
             {
-                lblDisplay.Text = "";
+                Result = (FirstNumber * SecondNumber);
+                lblDisplay.Text = Convert.ToString(Result);
+                FirstNumber = Result;
             }
-            if (lblDisplay.Text.Length <= 8)
+            else if (Operator == "/")
             {
-                lblDisplay.Text = lblDisplay.Text + "9";
-            }
-        }
-
-        private void btn0_Click(object sender, EventArgs e)
-        {
-            if (lblDisplay.Text == "0")
-            {
-                lblDisplay.Text = "";
-            }
-            if (lblDisplay.Text.Length <= 8)
-            {
-                lblDisplay.Text = lblDisplay.Text + "0";
+                if (SecondNumber == 0)
+                {
+                    lblDisplay.Text = "คูณ 0 ไม่ได้";
+                }
+                else
+                {
+                    Result = (FirstNumber / SecondNumber);
+                    lblDisplay.Text = Convert.ToString(Result);
+                    FirstNumber = Result;
+                }
             }
         }
     }
