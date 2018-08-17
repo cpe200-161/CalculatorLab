0553eb8e88df609b52507fb9726a79269965d9e8
Kritsana Manupha <guy_lll@hotmail.com>
2018-08-17 20:19:34 +0700

    //can press num-button //can't + - * /

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -12,9 +12,108 @@ namespace CPE200Lab1
 {
     public partial class Form1 : Form
     {
+        private int val = 0, temp = 0, sum = 0;
+   
+        
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void shownum()
+        {
+            lblDisplay.Text = val.ToString();
+        }
+
+        private void btClick(int num)
+        {
+            val = val * 10 + num;
+            shownum();
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            btClick(0);
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            btClick(4);
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            btClick(7);
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            btClick(8);
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            btClick(9);
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            btClick(6);
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            btClick(1);
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            btClick(2);
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            btClick(3);
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            btClick(5);
+        }
+
+      
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            temp = val;
+            val = 0;
+
+        }
+
+        private void lblDisplay_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+
+              
+            lblDisplay.Text = sum.ToString();
+           
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            sum = 0;
+            val = 0;
+            temp = 0;
+            shownum();
         }
     }
 }
