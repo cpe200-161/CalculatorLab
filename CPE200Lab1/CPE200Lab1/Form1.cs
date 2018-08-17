7743165d107549f0cc58589d0e358296c9dcf431
supakonzz <35358247+supakonzz@users.noreply.github.com>
2018-08-17 15:27:00 +0700

    Display buttonPress

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -16,5 +16,120 @@ namespace CPE200Lab1
         {
             InitializeComponent();
         }
+
+        public void conditionBtn(String num)
+        {
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "";
+            }
+            if (lblDisplay.Text.Length <= 8)
+            {
+                lblDisplay.Text = lblDisplay.Text + num;
+            }
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            conditionBtn("1");
+            
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            conditionBtn("2");
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            conditionBtn("3");
+
+        }
+
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            conditionBtn("4");
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            conditionBtn("5");
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            conditionBtn("6");
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            conditionBtn("7");
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            conditionBtn("8");
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            conditionBtn("9");
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            conditionBtn("x");
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            conditionBtn("0");
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            conditionBtn("/");
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            conditionBtn("+");
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            conditionBtn("-");
+        }
+
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+            conditionBtn("=");
+        }
+
+        private void btnDot_Click(object sender, EventArgs e)
+        {
+            conditionBtn(".");
+        }
+
+        private void btnSign_Click(object sender, EventArgs e)
+        {
+            conditionBtn("+-");
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            conditionBtn("c");
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            conditionBtn("<");
+        }
+
+        private void btnPercent_Click(object sender, EventArgs e)
+        {
+            conditionBtn("%");
+        }
     }
 }
