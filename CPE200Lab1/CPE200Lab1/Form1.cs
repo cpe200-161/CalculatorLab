ea8713793c702bbc8df964c066d6c97304d6fc43
chawitW <beck.bfts@gmail.com>
2018-08-20 00:31:23 +0700

    Nothing change

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Reflection;
 
 namespace CPE200Lab1
 {
@@ -15,6 +16,7 @@ namespace CPE200Lab1
         public Form1()
         {
             InitializeComponent();
+
         }
 
         private void btnN_Click(object sender, EventArgs e)
@@ -39,12 +41,23 @@ namespace CPE200Lab1
         private void btnPlus_Click(object sender, EventArgs e)
         {
             float x = float.Parse(lblDisplay.Text);
+            //typeof(Form1).GetMethod("btnEqual_Click", BindingFlags.NonPublic | BindingFlags.Instance).Invoke(new Form1(), null);
+            if (lblDisplay.Text != "")
+            {
+                lblDisplay.Text = string.Format("{0:0}", x);
+     
+            }
             
+
 
         }
 
         private void btnEqual_Click(object sender, EventArgs e)
         {
+
+            //float x = float.Parse(lblDisplay.Text);
+            //x += 1;
+           // lblDisplay.Text = string.Format("{0:0}", x);
 
         }
     }
