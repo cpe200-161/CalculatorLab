64fcf57fde919e174de007af7b444215e1ab0ab5
Thanaphon <Thanaphon@THANAPHON-PC>
2018-08-19 23:17:43 +0700

    add input output 0.1

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -12,19 +12,20 @@ namespace CPE200Lab1
 {
     public partial class Form1 : Form
     {
-        double num1=0, num2=0;
+        double num1 = 0, num2 = 0;
         string firstinput = null;
         string nextinput = null;
         int Operator = 0;
         int OpertorPressed = 0;
-        
+        int statnum = 0;
+
         public Form1()
         {
             InitializeComponent();
         }
         private void computing()
         {
-            switch(Operator)
+            switch (Operator)
             {
                 case 1:
                     nextinput = lblDisplay.Text;
@@ -52,22 +53,32 @@ namespace CPE200Lab1
         }
         private void btnPlus_Click(object sender, EventArgs e)
         {
-            firstinput = lblDisplay.Text;
-            num1 = double.Parse(lblDisplay.Text);
-            Operator = 1;
-            if(OpertorPressed == 1)
+            if (statnum == 1)
             {
-                computing();
+                
+                num1 = double.Parse(firstinput) + double.Parse(lblDisplay.Text);
+                Operator = 1;
+                
             }
-            OpertorPressed = 1;
+            if (statnum == 0)
+            {
+                firstinput = lblDisplay.Text;
+                num1 = double.Parse(lblDisplay.Text);
+                Operator = 1;
+                statnum = 1;
+                OpertorPressed = 1;
+                
+            }
             lblDisplay.Text = num1.ToString();
+
+            
         }
 
         private void btnClear_Click(object sender, EventArgs e)
         {
             lblDisplay.Text = "0";
             Operator = 0;
-            
+            statnum = 0;
             firstinput = null;
             nextinput = null;
         }
@@ -75,27 +86,27 @@ namespace CPE200Lab1
         private void btnEqual_Click(object sender, EventArgs e)
         {
             computing();
-            
+
         }
 
         private void btnDot_Click(object sender, EventArgs e)
         {
             if (!(lblDisplay.Text.Contains(".")))
             {
-                lblDisplay.Text = lblDisplay.Text+".";
+                lblDisplay.Text = lblDisplay.Text + ".";
             }
-            
+
         }
 
         private void btnX_Click(object sender, EventArgs e)
         {
-            
+
             Button btn = (Button)sender;
             if (lblDisplay.Text == "0")
             {
                 lblDisplay.Text = "";
             }
-            if (lblDisplay.Text.Length <= 8  )
+            if (lblDisplay.Text.Length <= 8)
             {
                 if (OpertorPressed == 0)
                 {
@@ -106,9 +117,10 @@ namespace CPE200Lab1
                 {
                     lblDisplay.Text = btn.Text;
                     OpertorPressed = 0;
+                    statnum = 0;
                 }
             }
-            
+
         }
 
 
