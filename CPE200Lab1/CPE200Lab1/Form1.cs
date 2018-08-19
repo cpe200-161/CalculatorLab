24bdf1dfcffa9ba924bb5221c381ce9917d83b3d
APHISIT KHAMNOI <aphisit_khamnoi@cmu.ac.th>
2018-08-19 17:47:43 +0700

    complete basic system -dot -reset No loop function -plus -Minus -Multiply -divide

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -14,8 +14,14 @@ namespace CPE200Lab1
     {
         string firstOperand;
         string secondOperand;
-        bool setFirstOperand = false;
+		string result;
+		bool setFirstOperand = false;
         bool isStartSecondOperand = false;
+		bool setDot = false;
+		bool setPlus = false;
+		bool setMinus = false;
+		bool setMultiply = false;
+		bool setDivide = false;
         public Form1()
         {
             InitializeComponent();
@@ -23,20 +29,47 @@ namespace CPE200Lab1
 
         private void btnDivide_Click(object sender, EventArgs e)
         {
-
-        }
+			setDivide = true;
+			setPlus = false;
+			setMinus = false;
+			setMultiply = false;
+			firstOperand = lblDisplay.Text;
+			setFirstOperand = true;
+		}
 
         private void btnMultiply_Click(object sender, EventArgs e)
         {
-
-        }
+			setMultiply = true;
+			setPlus = false;
+			setMinus = false;
+			setDivide = false;
+			firstOperand = lblDisplay.Text;
+			setFirstOperand = true;
+		}
 
         private void btnEqual_Click(object sender, EventArgs e)
         {
-            string result;
             secondOperand = lblDisplay.Text;
-            result = (float.Parse(firstOperand) + float.Parse(secondOperand)).ToString();
-            lblDisplay.Text = result;
+			if (setPlus == true)
+			{
+				result = (float.Parse(firstOperand) + float.Parse(secondOperand)).ToString();
+				lblDisplay.Text = result;
+			}
+			else if (setMinus == true)
+			{
+				result = (float.Parse(firstOperand) - float.Parse(secondOperand)).ToString();
+				lblDisplay.Text = result;
+			}
+			else if (setMultiply == true)
+			{
+				result = (float.Parse(firstOperand) * float.Parse(secondOperand)).ToString();
+				lblDisplay.Text = result;
+			}
+			else if (setDivide == true)
+			{
+				result = (float.Parse(firstOperand) / float.Parse(secondOperand)).ToString();
+				lblDisplay.Text = result;
+			}
         }
 
         private void btnX_Click(object sender, EventArgs e)
@@ -59,13 +92,66 @@ namespace CPE200Lab1
 
         private void btnDot_Click(object sender, EventArgs e)
         {
-           
+			if (setDot == false)
+			{
+				lblDisplay.Text += ".";
+			}
         }
 
         private void btnPlus_Click(object sender, EventArgs e)
         {
-            firstOperand = lblDisplay.Text;
-            setFirstOperand = true;
+			setPlus = true;
+			setMinus = false;
+			setMultiply = false;
+			setDivide = false;
+			if (setFirstOperand == true)
+			{
+				secondOperand = lblDisplay.Text;
+				result = (float.Parse(firstOperand) + float.Parse(secondOperand)).ToString();
+				lblDisplay.Text = result;
+				firstOperand = lblDisplay.Text;
+				isStartSecondOperand = false;
+				setPlus = false;
+			}
+			else if (setFirstOperand == false)
+			{
+				firstOperand = lblDisplay.Text;
+				setFirstOperand = true;
+			}
         }
-    }
+
+		private void btnClear_Click(object sender, EventArgs e)
+		{
+			lblDisplay.Text = "0";
+			setFirstOperand = false;
+			isStartSecondOperand = false;
+			setDot = false;
+			setPlus = false;
+			setMinus = false;
+			setMultiply = false;
+			setDivide = false;
+		}
+
+		private void btnMinus_Click(object sender, EventArgs e)
+		{
+			setMinus = true;
+			setPlus = false;
+			setMultiply = false;
+			setDivide = false;
+			if (setFirstOperand == true)
+			{
+				secondOperand = lblDisplay.Text;
+				result = (float.Parse(firstOperand) - float.Parse(secondOperand)).ToString();
+				lblDisplay.Text = result;
+				firstOperand = lblDisplay.Text;
+				isStartSecondOperand = false;
+				setMinus = false;
+			}
+			else if (setFirstOperand == false)
+			{
+				firstOperand = lblDisplay.Text;
+				setFirstOperand = true;
+			}
+		}
+	}
 }
