441c80475970bf2dfcdcc981efd4b81d67ff99de
Thanaphon <Thanaphon@THANAPHON-PC>
2018-08-20 20:59:24 +0700

    completed operator version1

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -12,12 +12,15 @@ namespace CPE200Lab1
 {
     public partial class Form1 : Form
     {
-        double num1 = 0, num2 = 0;
+        float num1 = 0, num2 = 0;
         string firstinput = null;
         string nextinput = null;
         int Operator = 0;
         int OpertorPressed = 0;
-        int statnum = 0;
+        int statnum1 = 0;
+        int statnum2 = 0;
+        int statnum3 = 0;
+        int statnum4 = 0;
 
         public Form1()
         {
@@ -28,23 +31,26 @@ namespace CPE200Lab1
             switch (Operator)
             {
                 case 1:
-                    nextinput = lblDisplay.Text;
-                    num2 = num1 + double.Parse(lblDisplay.Text);
+
+                    num2 = num1 + float.Parse(lblDisplay.Text);
                     lblDisplay.Text = num2.ToString();
+                    statnum1 = 0;
                     break;
                 case 2:
-                    nextinput = lblDisplay.Text;
-                    num2 = num1 - double.Parse(lblDisplay.Text);
+
+                    num2 = num1 - float.Parse(lblDisplay.Text);
                     lblDisplay.Text = num2.ToString();
+                    statnum2 = 0;
                     break;
                 case 3:
-                    nextinput = lblDisplay.Text;
-                    num2 = num1 * double.Parse(lblDisplay.Text);
+
+                    num2 = num1 * float.Parse(lblDisplay.Text);
                     lblDisplay.Text = num2.ToString();
+                    statnum3 = 0;
                     break;
                 case 4:
-                    nextinput = lblDisplay.Text;
-                    num2 = num1 / double.Parse(lblDisplay.Text);
+
+                    num2 = num1 / float.Parse(lblDisplay.Text);
                     lblDisplay.Text = num2.ToString();
                     break;
                 default:
@@ -53,33 +59,32 @@ namespace CPE200Lab1
         }
         private void btnPlus_Click(object sender, EventArgs e)
         {
-            if (statnum == 0)
+            if (statnum1 == 0)
             {
-                firstinput = lblDisplay.Text;
-                num1 = double.Parse(lblDisplay.Text);
-                Operator = 1;
-                
-                OpertorPressed = 1;
+
+                num1 = float.Parse(lblDisplay.Text);
                 lblDisplay.Text = num1.ToString();
-                statnum++;
+                statnum1++;
             }
-            else
+            else if (statnum1 != 0)
             {
-                num1 = num1 + double.Parse(lblDisplay.Text);
-                Operator = 1;
-                OpertorPressed = 1;
+                num1 = num1 + float.Parse(lblDisplay.Text);
                 lblDisplay.Text = lblDisplay.Text;
             }
-            
+            if (Operator == 4 || Operator == 2 || Operator == 3) lblDisplay.Text = "Can't calculate.";
+            Operator = 1;
+            OpertorPressed = 1;
 
-            
         }
 
         private void btnClear_Click(object sender, EventArgs e)
         {
             lblDisplay.Text = "0";
             Operator = 0;
-            statnum = 0;
+            statnum1 = 0;
+            statnum2 = 0;
+            statnum3 = 0;
+            statnum4 = 0;
             firstinput = null;
             nextinput = null;
         }
@@ -87,7 +92,6 @@ namespace CPE200Lab1
         private void btnEqual_Click(object sender, EventArgs e)
         {
             computing();
-
         }
 
         private void btnDot_Click(object sender, EventArgs e)
@@ -96,22 +100,84 @@ namespace CPE200Lab1
             {
                 lblDisplay.Text = lblDisplay.Text + ".";
             }
-
         }
 
         private void btnBack_Click(object sender, EventArgs e)
         {
-            if(lblDisplay.Text != "0")
+            if (lblDisplay.Text != "0")
             {
                 int length = lblDisplay.Text.Length - 1;
-                string text= lblDisplay.Text;
+                string text = lblDisplay.Text;
                 lblDisplay.Text = "";
-                for(int i =0;i<length;i++)
+                for (int i = 0; i < length; i++)
                 {
                     lblDisplay.Text = lblDisplay.Text + text[i];
                 }
                 if (lblDisplay.Text == "") lblDisplay.Text = "0";
             }
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            if (statnum2 == 0)
+            {
+
+                num1 = float.Parse(lblDisplay.Text);
+                lblDisplay.Text = num1.ToString();
+                statnum2++;
+            }
+            else if (statnum2 != 0)
+            {
+                num1 = num1 - float.Parse(lblDisplay.Text);
+                lblDisplay.Text = lblDisplay.Text;
+            }
+            if (Operator == 1 || Operator == 4 || Operator == 3) lblDisplay.Text = "Can't calculate.";
+            Operator = 2;
+            OpertorPressed = 1;
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            if (statnum3 == 0)
+            {
+
+                num1 = float.Parse(lblDisplay.Text);
+                lblDisplay.Text = num1.ToString();
+                statnum3++;
+            }
+            else if (statnum3 != 0)
+            {
+                num1 = num1 * float.Parse(lblDisplay.Text);
+                lblDisplay.Text = lblDisplay.Text;
+            }
+            if (Operator == 1 || Operator == 2 || Operator == 4) lblDisplay.Text = "Can't calculate.";
+            Operator = 3;
+            OpertorPressed = 1;
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            if (statnum4 == 0)
+            {
+
+                num1 = float.Parse(lblDisplay.Text);
+                lblDisplay.Text = num1.ToString();
+                statnum4++;
+            }
+            else if (statnum4 != 0)
+            {
+                num1 = num1 * float.Parse(lblDisplay.Text);
+                lblDisplay.Text = lblDisplay.Text;
+            } if (Operator == 1 || Operator == 2 || Operator ==3) lblDisplay.Text = "Can't calculate.";
+            Operator = 4;
+            OpertorPressed = 1;
+        }
+
+        private void btnPercent_Click(object sender, EventArgs e)
+        {
+            num1 = float.Parse(lblDisplay.Text);
+            num1 = num1 / 100f;
+            lblDisplay.Text = num1.ToString();
         }
 
         private void btnX_Click(object sender, EventArgs e)
@@ -122,7 +188,7 @@ namespace CPE200Lab1
             {
                 lblDisplay.Text = "";
             }
-            if (lblDisplay.Text.Length <= 8)
+            if (lblDisplay.Text.Length < 8)
             {
                 if (OpertorPressed == 0)
                 {
@@ -133,7 +199,7 @@ namespace CPE200Lab1
                 {
                     lblDisplay.Text = btn.Text;
                     OpertorPressed = 0;
-                    
+
                 }
             }
 
