1cbf6fc104ac76dabe93047ae0fec27bf37bd3e6
Annop_K <35335095+AnnopKTz@users.noreply.github.com>
2018-08-20 17:09:28 +0700

    Done

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -27,7 +27,7 @@ namespace CPE200Lab1
         bool divide = false;
         bool eqCheck = false;
         bool percent = false;
-        
+    
         private void btnClear_Click(object sender, EventArgs e)
         {
             lblDisplay.Text = "0";
@@ -40,24 +40,10 @@ namespace CPE200Lab1
             eqCheck = false;
         }
 
-        private void btn1_Click(object sender, EventArgs e)
-        {
-            if (lblDisplay.Text == "0")
-            {
-                lblDisplay.Text = "";
-            }else if (checkFirst)
-            {
-                lblDisplay.Text = "";
-                checkFirst = false;
-            }
-            if (lblDisplay.Text.Length < 8)
-            {
-                lblDisplay.Text += "1";
-            }
-        }
 
-        private void btn2_Click(object sender, EventArgs e)
+        private void btnx_Click(object sender, EventArgs e)
         {
+            Button btn  = (Button)sender;
             if (lblDisplay.Text == "0")
             {
                 lblDisplay.Text = "";
@@ -69,145 +55,9 @@ namespace CPE200Lab1
             }
             if (lblDisplay.Text.Length < 8)
             {
-                lblDisplay.Text += "2";
+                lblDisplay.Text += btn.Text;
             }
-        }
 
-        private void btn3_Click(object sender, EventArgs e)
-        {
-            if (lblDisplay.Text == "0")
-            {
-                lblDisplay.Text = "";
-            }
-            else if (checkFirst)
-            {
-                lblDisplay.Text = "";
-                checkFirst = false;
-            }
-            if (lblDisplay.Text.Length < 8)
-            {
-                lblDisplay.Text += "3";
-            }
-        }
-
-        private void btn4_Click(object sender, EventArgs e)
-        {
-            if (lblDisplay.Text == "0")
-            {
-                lblDisplay.Text = "";
-            }
-            else if (checkFirst)
-            {
-                lblDisplay.Text = "";
-                checkFirst = false;
-            }
-            if (lblDisplay.Text.Length < 8)
-            {
-                lblDisplay.Text += "4";
-            }
-        }
-
-        private void btn5_Click(object sender, EventArgs e)
-        {
-            if (lblDisplay.Text == "0")
-            {
-                lblDisplay.Text = "";
-            }
-            else if (checkFirst)
-            {
-                lblDisplay.Text = "";
-                checkFirst = false;
-            }
-            if (lblDisplay.Text.Length < 8)
-            {
-                lblDisplay.Text += "5";
-            }
-        }
-
-        private void btn6_Click(object sender, EventArgs e)
-        {
-            if (lblDisplay.Text == "0")
-            {
-                lblDisplay.Text = "";
-            }
-            else if (checkFirst)
-            {
-                lblDisplay.Text = "";
-                checkFirst = false;
-            }
-            if (lblDisplay.Text.Length < 8)
-            {
-                lblDisplay.Text += "6";
-            }
-        }
-
-        private void btn7_Click(object sender, EventArgs e)
-        {
-            if (lblDisplay.Text == "0")
-            {
-                lblDisplay.Text = "";
-            }
-            else if (checkFirst)
-            {
-                lblDisplay.Text = "";
-                checkFirst = false;
-            }
-            if (lblDisplay.Text.Length < 8)
-            {
-                lblDisplay.Text += "7";
-            }
-        }
-
-        private void btn8_Click(object sender, EventArgs e)
-        {
-            if (lblDisplay.Text == "0")
-            {
-                lblDisplay.Text = "";
-            }
-            else if (checkFirst)
-            {
-                lblDisplay.Text = "";
-                checkFirst = false;
-            }
-            if (lblDisplay.Text.Length < 8)
-            {
-                lblDisplay.Text += "8";
-            }
-        }
-
-        private void btn9_Click(object sender, EventArgs e)
-        {
-            if (lblDisplay.Text == "0")
-            {
-                lblDisplay.Text = "";
-            }
-            else if (checkFirst)
-            {
-                lblDisplay.Text = "";
-                checkFirst = false;
-            }
-            if (lblDisplay.Text.Length < 8)
-            {
-                lblDisplay.Text += "9";
-            }
-        }
-
-        private void btn0_Click(object sender, EventArgs e)
-        {
-            if (lblDisplay.Text == "0")
-            {
-                lblDisplay.Text = "";
-            }
-            else if (checkFirst)
-            {
-                lblDisplay.Text = "";
-                checkFirst = false;
-            }
-            if (lblDisplay.Text.Length < 8)
-            {
-                lblDisplay.Text += "0";
-            }
-            
         }
 
         private void btnDot_Click(object sender, EventArgs e)
@@ -453,21 +303,29 @@ namespace CPE200Lab1
 
         private void btnPercent_Click(object sender, EventArgs e)
         {
-            secondOp = lblDisplay.Text;
-            if (plus)
+            if (firstOp==null)
             {
-                total = ((float.Parse(firstOp) + (float.Parse(secondOp) * (float.Parse(firstOp) / 100.0)))).ToString();
-            }else if (minus)
-            {
-                total = ((float.Parse(firstOp) - (float.Parse(secondOp) * (float.Parse(firstOp) / 100.0)))).ToString();
-            }else if (multiply)
-            {
-                total = ((float.Parse(firstOp) * (float.Parse(secondOp) * (float.Parse(firstOp) / 100.0)))).ToString();
-            }else if (divide)
-            {
-                total = ((float.Parse(firstOp) / (float.Parse(secondOp) * (float.Parse(firstOp) / 100.0)))).ToString();
+                firstOp = lblDisplay.Text;
+                total = (float.Parse(firstOp) / 100).ToString();
+                firstOp = total;
             }
-            
+            else
+            {
+                secondOp = lblDisplay.Text;
+                if (plus)
+                {
+                    total = ((float.Parse(firstOp) + (float.Parse(secondOp) * (float.Parse(firstOp) / 100.0)))).ToString();
+                }else if (minus)
+                {
+                    total = ((float.Parse(firstOp) - (float.Parse(secondOp) * (float.Parse(firstOp) / 100.0)))).ToString();
+                }else if (multiply)
+                {
+                    total = ((float.Parse(firstOp) * (float.Parse(secondOp) * (float.Parse(firstOp) / 100.0)))).ToString();
+                }else if (divide)
+                {
+                    total = ((float.Parse(firstOp) / (float.Parse(secondOp) * (float.Parse(firstOp) / 100.0)))).ToString();
+                }
+            }
             firstOp = total;
             percent = true;
             minus = false;
