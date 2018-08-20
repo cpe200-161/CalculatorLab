bf4bb39a2f3999174fd269e837f5390e3db48ed0
600610794 <35362980+600610794@users.noreply.github.com>
2018-08-20 19:24:24 +0700

    all done but % cant + - * /

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -14,9 +14,57 @@ namespace CPE200Lab1
     {
         string firstSet = null;
         string secondSet = null;
-        bool check = false;
-       
-        
+        string percent = null;
+        bool checkset = false;
+        bool checkplus = false;
+        bool checkminus = false;
+        bool checkmulti = false;
+        bool checkdivide = false;
+        bool checkper = false;
+
+        void check (string x)
+        {
+            if( x == "+")
+            {
+                checkplus = true;
+                checkminus = false;
+                checkmulti = false;
+                checkdivide = false;
+                checkper = false;
+            }
+           else if (x == "-")
+            {
+                checkminus = true;
+                checkplus = false;
+                checkmulti = false;
+                checkdivide = false;
+                checkper = false;
+            }
+            else if (x == "*")
+            {
+                checkmulti = true;
+                checkminus = false;
+                checkplus = false;
+                checkdivide = false;
+                checkper = false;
+            }
+            else if (x == "/")
+            {
+                checkdivide = true;
+                checkminus = false;
+                checkmulti = false;
+                checkplus = false;
+                checkper = false;
+            }
+            else if (x == "%")
+            {
+                checkper = true;
+                checkminus = false;
+                checkmulti = false;
+                checkdivide = false;
+                checkplus = false;
+            }
+        }
 
         public Form1()
         {
@@ -26,9 +74,10 @@ namespace CPE200Lab1
         private void btnx_Click(object sender, EventArgs e)
         {
             Button btn = (Button)sender;
-            if (check == true)
+            if (checkset)
             {
                 lblDisplay.Text = "";
+                checkset = false;
             }
             if (lblDisplay.Text == "0")
             {
@@ -36,25 +85,215 @@ namespace CPE200Lab1
             }
             
             if (lblDisplay.Text.Length < 8)
-                {
-                    lblDisplay.Text = lblDisplay.Text + btn.Text;
-                }
-            
-            
+            {
+                lblDisplay.Text = lblDisplay.Text + btn.Text;
+            }  
         }
 
-        private void btnPlus_Click(object sender, EventArgs e)
+         private void btnDot_Click(object sender, EventArgs e)
         {
-            firstSet = lblDisplay.Text;
-            check = true;
+            if (checkset)
+            {
+                lblDisplay.Text = "";
+                checkset = false;
+            }
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "";
+            }
+
+            if (lblDisplay.Text.Length < 8)
+            {
+                lblDisplay.Text = lblDisplay.Text + ".";
+            }
+        }
+
+        private void btnplus_Click(object sender, EventArgs e)
+        {
+            if (firstSet == null)
+            {
+                firstSet = lblDisplay.Text;
+                checkset = true;
+            }
+            else
+            {
+                
+                    secondSet = lblDisplay.Text;
+                    if (checkplus == true)
+                    {
+                        lblDisplay.Text = (float.Parse(firstSet) + float.Parse(secondSet)).ToString();
+                    }
+                    if (checkminus == true)
+                    {
+                        lblDisplay.Text = (float.Parse(firstSet) - float.Parse(secondSet)).ToString();
+                    }
+                    if (checkmulti == true)
+                    {
+                        lblDisplay.Text = (float.Parse(firstSet) * float.Parse(secondSet)).ToString();
+                    }
+                    if (checkdivide == true)
+                    {
+                        lblDisplay.Text = (float.Parse(firstSet) / float.Parse(secondSet)).ToString();
+                    }
+                    firstSet = lblDisplay.Text;
+                    checkset = true;
+                
+            }
+            check("+");
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            if (firstSet == null)
+            {
+                firstSet = lblDisplay.Text;
+                checkset = true;
+            }
+            else
+            {
+                
+                    secondSet = lblDisplay.Text;
+                    if (checkplus == true)
+                    {
+                        lblDisplay.Text = (float.Parse(firstSet) + float.Parse(secondSet)).ToString();
+                    }
+                    if (checkminus == true)
+                    {
+                        lblDisplay.Text = (float.Parse(firstSet) - float.Parse(secondSet)).ToString();
+                    }
+                    if (checkmulti == true)
+                    {
+                        lblDisplay.Text = (float.Parse(firstSet) * float.Parse(secondSet)).ToString();
+                    }
+                    if (checkdivide == true)
+                    {
+                        lblDisplay.Text = (float.Parse(firstSet) / float.Parse(secondSet)).ToString();
+                    }
+                    firstSet = lblDisplay.Text;
+                    checkset = true;
+                
+            }
+            check("-");
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            if (firstSet == null)
+            {
+                firstSet = lblDisplay.Text;
+                checkset = true;
+            }
+            else
+            {
+                
+                    secondSet = lblDisplay.Text;
+                    if (checkplus == true)
+                    {
+                        lblDisplay.Text = (float.Parse(firstSet) + float.Parse(secondSet)).ToString();
+                    }
+                    if (checkminus == true)
+                    {
+                        lblDisplay.Text = (float.Parse(firstSet) - float.Parse(secondSet)).ToString();
+                    }
+                    if (checkmulti == true)
+                    {
+                        lblDisplay.Text = (float.Parse(firstSet) * float.Parse(secondSet)).ToString();
+                    }
+                    if (checkdivide == true)
+                    {
+                        lblDisplay.Text = (float.Parse(firstSet) / float.Parse(secondSet)).ToString();
+                    }
+                    firstSet = lblDisplay.Text;
+                    checkset = true;
+                
+            }
+            check("*");
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            if (firstSet == null)
+            {
+                firstSet = lblDisplay.Text;
+                checkset = true;
+            }
+            else
+            {
+                
+                    secondSet = lblDisplay.Text;
+                    if (checkplus == true)
+                    {
+                        lblDisplay.Text = (float.Parse(firstSet) + float.Parse(secondSet)).ToString();
+                    }
+                    if (checkminus == true)
+                    {
+                        lblDisplay.Text = (float.Parse(firstSet) - float.Parse(secondSet)).ToString();
+                    }
+                    if (checkmulti == true)
+                    {
+                        lblDisplay.Text = (float.Parse(firstSet) * float.Parse(secondSet)).ToString();
+                    }
+                    if (checkdivide == true)
+                    {
+                        lblDisplay.Text = (float.Parse(firstSet) / float.Parse(secondSet)).ToString();
+                    }
+                    firstSet = lblDisplay.Text;
+                    checkset = true;
+                
+            }
+            check("/");
+        }
+
+        private void btnPercent_Click(object sender, EventArgs e)
+        {
+            
+            if (firstSet == null)
+            {
+                
+                percent = (float.Parse(lblDisplay.Text) / 100).ToString();
+                firstSet = percent;
+            }
+            else
+            {
+               
+                secondSet = (float.Parse(lblDisplay.Text) * float.Parse(firstSet) / 100).ToString();
+                percent= (float.Parse(firstSet) + float.Parse(secondSet)).ToString();
+                secondSet = percent;
+
+            }
+            check("%");
+        }
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            firstSet = null;
+            secondSet = null;
+            checkset = false;
+            lblDisplay.Text = "0";
         }
 
         private void btnEqual_Click(object sender, EventArgs e)
         {
             secondSet = lblDisplay.Text;
-            lblDisplay.Text = (float.Parse(firstSet) + float.Parse(secondSet)).ToString();
+            if (checkplus == true)
+            {
+                lblDisplay.Text = (float.Parse(firstSet) + float.Parse(secondSet)).ToString();
+            }
+           if (checkminus == true)
+            {
+                lblDisplay.Text = (float.Parse(firstSet) - float.Parse(secondSet)).ToString();
+            }
+           if (checkmulti == true)
+            {
+                lblDisplay.Text = (float.Parse(firstSet) * float.Parse(secondSet)).ToString();
+            }
+           if (checkdivide == true)
+            {
+                lblDisplay.Text = (float.Parse(firstSet) / float.Parse(secondSet)).ToString();
+            }
+           if (checkper == true)
+            {
+                lblDisplay.Text = percent;  
+            }
         }
-
-      
     }
 }
