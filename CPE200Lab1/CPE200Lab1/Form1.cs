1bf6e74c706f8edacc96414843358d26c75330ff
TANCHAPAS CHANTARANIMI <tanchapas_ch@cmu.ac.th>
2018-08-21 14:04:29 +0700

    Complete number buttons and operator buttons ( + - * / and %)

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -12,9 +12,75 @@ namespace CPE200Lab1
 {
     public partial class Form1 : Form
     {
+        double value = 0, value_2 = 0;
+        string operation = "";
+        bool opereation_pressed = false;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnX_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if ((lblDisplay.Text == "0") || (opereation_pressed))
+            {
+                lblDisplay.Text = "";
+                opereation_pressed = false;
+            }
+            if (lblDisplay.Text.Length < 8)
+            {
+                lblDisplay.Text = lblDisplay.Text + btn.Text;
+            }
+        }
+
+        private void operator_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            operation = btn.Text;
+            value = Double.Parse(lblDisplay.Text);
+            opereation_pressed = true;
+        }
+
+        private void operator_Percent(object sender, EventArgs e)
+        {
+            if (value != 0)
+            {
+                value_2 = (Double.Parse(lblDisplay.Text) / 100) * value;
+            }
+            else if (value == 0)
+            {
+                value_2 = (Double.Parse(lblDisplay.Text) / 100);
+            }
+            lblDisplay.Text = value_2.ToString();
+            opereation_pressed = true;
+        }
+
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+            switch (operation)
+            {
+                case "+": lblDisplay.Text = (value + double.Parse(lblDisplay.Text)).ToString(); break;
+                case "-": lblDisplay.Text = (value - double.Parse(lblDisplay.Text)).ToString(); break;
+                case "X": lblDisplay.Text = (value * double.Parse(lblDisplay.Text)).ToString(); break;
+                case "÷": lblDisplay.Text = (value / double.Parse(lblDisplay.Text)).ToString(); break;
+                default: break;
+            }
+            opereation_pressed = true;
+        }
+
+        private void btnSign_Click(object sender, EventArgs e)
+        {
+            value = (Double.Parse(lblDisplay.Text) * -1);
+            lblDisplay.Text = value.ToString();
+            opereation_pressed = true;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lblDisplay.Text = "0";
+            value = 0;
         }
     }
 }
