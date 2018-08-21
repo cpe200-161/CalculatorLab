587135a1eb18ddc7d4fadb82478f5db860ef6bb5
PATHOMPORN PANKAEW <amashiikaze@gmail.com>
2018-08-21 14:13:58 +0700

    final version

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -12,8 +12,8 @@ namespace CPE200Lab1
 {
     public partial class Form1 : Form
     {
-        string firstoperand="";
-        string secondoperand="";
+        string firstoperand = "";
+        string secondoperand = "";
         bool setfirstop = false;
         bool set = false;
         bool plus;
@@ -22,8 +22,8 @@ namespace CPE200Lab1
         bool divide;
         bool per;
         string sum;
-       
-       
+
+
         public Form1()
         {
             InitializeComponent();
@@ -31,124 +31,130 @@ namespace CPE200Lab1
 
         private void lblDisplay_Click(object sender, EventArgs e)
         {
+   
 
         }
         private void bthX_Click(object sender, EventArgs e)
         {
             Button btn = (Button)sender;
-            if(firstoperand!="") set = true;
+            if (firstoperand != "") set = true;
             if (setfirstop == true)
             {
                 lblDisplay.Text = "";
                 setfirstop = false;
-                           
+
             }
 
             if (lblDisplay.Text.Length < 8)
             {
-            
+
                 if (lblDisplay.Text == "0")
                 {
                     lblDisplay.Text = "";
                 }
-                
+
                 lblDisplay.Text = lblDisplay.Text + btn.Text;
-                 
-                
+
+
             }
         }
         private void btnPlus_Click(object sender, EventArgs e)
         {
-            
-                
-                if(set==true){
+
+
+            if (set == true)
+            {
                 secondoperand = lblDisplay.Text;
                 if (per == true)
                 {
-                secondoperand = ((float.Parse(secondoperand) / 100) * (float.Parse(firstoperand))).ToString();
-                per = false;
+                    secondoperand = ((float.Parse(secondoperand) / 100) * (float.Parse(firstoperand))).ToString();
+                    per = false;
                 }
                 firstoperand = (float.Parse(firstoperand) + float.Parse(secondoperand)).ToString();
                 lblDisplay.Text = firstoperand;
-                set =false;
-                
-                }
-                if(set == false){
+                set = false;
+
+            }
+            if (set == false)
+            {
                 firstoperand = lblDisplay.Text;
                 setfirstop = true;
-                }
-                
+            }
 
-                
-                
-                plus = true;
-                
-         }
+            plus = true;
+
+        }
 
         private void btnMinus_Click(object sender, EventArgs e)
         {
-                if(set==true){
-               
+            if (set == true)
+            {
+
                 secondoperand = lblDisplay.Text;
                 if (per == true)
                 {
-                secondoperand = ((float.Parse(secondoperand) / 100) * (float.Parse(firstoperand))).ToString();
-                per = false;
+                    secondoperand = ((float.Parse(secondoperand) / 100) * (float.Parse(firstoperand))).ToString();
+                    per = false;
                 }
                 firstoperand = (float.Parse(firstoperand) - float.Parse(secondoperand)).ToString();
                 lblDisplay.Text = firstoperand;
-                set =false;
-                
-                }
-                if(set == false){
+                set = false;
+
+            }
+            if (set == false)
+            {
                 firstoperand = lblDisplay.Text;
                 setfirstop = true;
-                }
+            }
             minus = true;
         }
 
         private void btnMultiply_Click(object sender, EventArgs e)
         {
-            
-            if(set==true){
-               
+
+            if (set == true)
+            {
+
                 secondoperand = lblDisplay.Text;
                 if (per == true)
                 {
-                secondoperand = ((float.Parse(secondoperand) / 100) * (float.Parse(firstoperand))).ToString();
-                per = false;
+                    secondoperand = ((float.Parse(secondoperand) / 100) * (float.Parse(firstoperand))).ToString();
+                    per = false;
                 }
                 firstoperand = (float.Parse(firstoperand) * float.Parse(secondoperand)).ToString();
                 lblDisplay.Text = firstoperand;
-                set =false;
-                
-                }
-                if(set == false){
+                set = false;
+
+            }
+            if (set == false)
+            {
                 firstoperand = lblDisplay.Text;
                 setfirstop = true;
-                }
+            }
             multi = true;
         }
 
         private void btnDivide_Click(object sender, EventArgs e)
         {
-             if(set==true){
-               
+            if (set == true)
+            {
+
                 secondoperand = lblDisplay.Text;
                 if (per == true)
                 {
-                secondoperand = ((float.Parse(secondoperand) / 100) * (float.Parse(firstoperand))).ToString();
-                per = false;
+                    secondoperand = ((float.Parse(secondoperand) / 100) * (float.Parse(firstoperand))).ToString();
+                    per = false;
                 }
                 firstoperand = (float.Parse(firstoperand) / float.Parse(secondoperand)).ToString();
                 lblDisplay.Text = firstoperand;
-                set =false;
-                
-                }
-                if(set == false){
+                set = false;
+
+            }
+            if (set == false)
+            {
                 firstoperand = lblDisplay.Text;
                 setfirstop = true;
-                }
+            }
             divide = true;
         }
 
@@ -160,13 +166,10 @@ namespace CPE200Lab1
                 per = true;
             }
 
-          
-      
-            
         }
         private void btnEqual_Click(object sender, EventArgs e)
         {
-            
+
             secondoperand = lblDisplay.Text;
             if (per == true)
             {
@@ -176,22 +179,25 @@ namespace CPE200Lab1
             }
             if (plus == true)
             {
-                
+
                 sum = (float.Parse(firstoperand) + float.Parse(secondoperand)).ToString();
                 plus = false;
                 set = false;
-                
-            }else if(minus == true)
+
+            }
+            else if (minus == true)
             {
                 sum = (float.Parse(firstoperand) - float.Parse(secondoperand)).ToString();
                 minus = false;
                 set = false;
-            }else if(multi == true)
+            }
+            else if (multi == true)
             {
                 sum = (float.Parse(firstoperand) * float.Parse(secondoperand)).ToString();
                 multi = false;
                 set = false;
-            }else if(divide == true)
+            }
+            else if (divide == true)
             {
                 sum = (float.Parse(firstoperand) / float.Parse(secondoperand)).ToString();
                 divide = false;
@@ -200,24 +206,38 @@ namespace CPE200Lab1
 
 
 
-            
+
             lblDisplay.Text = sum;
-            
+
         }
 
         private void btnBack_Click(object sender, EventArgs e)
         {
-            
+            if (lblDisplay.Text.Length > 1)
+            {
+                string display = lblDisplay.Text;
+                string sub = display.Substring(0, display.Length - 1);
+                lblDisplay.Text = sub;
+            }
+            else if (lblDisplay.Text.Length == 1)
+            {
+                firstoperand = "";
+                secondoperand = "";
+                setfirstop = false;
+                set = false;
+                lblDisplay.Text = "0";
+
+            }
         }
 
         private void btnClear_Click(object sender, EventArgs e)
         {
-           
-            firstoperand="";
-            secondoperand="";
+
+            firstoperand = "";
+            secondoperand = "";
             setfirstop = false;
             set = false;
             lblDisplay.Text = "0";
-         }
+        }
     }
 }
