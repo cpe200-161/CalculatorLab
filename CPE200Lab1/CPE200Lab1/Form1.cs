7a5cbcaa66711682adf35da1bec625a8b32a07e0
wogwhale <35363093+wongwhale@users.noreply.github.com>
2018-08-21 14:14:55 +0700

    Add string temp.

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -12,8 +12,8 @@ namespace CPE200Lab1
 {
     public partial class Form1 : Form
     {
-        string firstOperand, secondOperand, format, result;
-        bool setFirstOperand = false, setDot = false, setAns = false, setBackspace= false;
+        string firstOperand, secondOperand, format, result, temp = null;
+        bool setFirstOperand = false, setDot = false, setAns = false, setBackspace = false;
         int setFormat = 0;
 
         public Form1()
@@ -48,7 +48,6 @@ namespace CPE200Lab1
         private void btnx_Click(object sender, EventArgs e)
         {
 
-
             Button btn = (Button)sender;
 
             if (lblDisplay.Text == "0" || setFirstOperand == true || setAns == true)
@@ -77,76 +76,85 @@ namespace CPE200Lab1
             setAns = false;
         }
 
-         private void Calc_Click(object sender, EventArgs e)
+        private void Calc_Click(object sender, EventArgs e)
         {
             firstOperand = lblDisplay.Text;
             setFirstOperand = true;
             setDot = false;
             setAns = false;
+            temp = null;
 
             Button btn = (Button)sender;
 
-                if (btn.Text == "+")
-                {
-                    format = "plus";
-                    setFormat += 1;
-                }
-                else if (btn.Text == "-")
-                {
-                    format = "minus";
-                    setFormat += 1;
-                }
-                else if (btn.Text == "X")
-                {
-                    format = "multi";
-                    setFormat += 1;
-                }
-                else if (btn.Text == "÷")
-                {
-                    format = "divide";
-                    setFormat += 1;
-                }
-            
+            if (btn.Text == "+")
+            {
+                format = "plus";
+                setFormat += 1;
+            }
+            else if (btn.Text == "-")
+            {
+                format = "minus";
+                setFormat += 1;
+            }
+            else if (btn.Text == "X")
+            {
+                format = "multi";
+                setFormat += 1;
+            }
+            else if (btn.Text == "÷")
+            {
+                format = "divide";
+                setFormat += 1;
+            }
         }
-
 
         private void btnEqual_Click(object sender, EventArgs e)
         {
             secondOperand = lblDisplay.Text;
+            if (temp == null)
+            {
+                temp = lblDisplay.Text;
+            }
+
             if (format == "plus")
             {
                 result = (float.Parse(firstOperand) + float.Parse(secondOperand)).ToString();
-                
+
             }
             else if (format == "minus")
             {
                 result = (float.Parse(firstOperand) - float.Parse(secondOperand)).ToString();
-                
+
             }
             else if (format == "multi")
             {
                 result = (float.Parse(firstOperand) * float.Parse(secondOperand)).ToString();
-                
+
             }
             else if (format == "divide")
             {
                 result = (float.Parse(firstOperand) / float.Parse(secondOperand)).ToString();
-                
+
             }
 
             lblDisplay.Text = result;
+            firstOperand = temp;
             setAns = true;
             setDot = false;
         }
 
         private void btnClear_Click(object sender, EventArgs e)
         {
-            if (lblDisplay.Text != "0" || setFirstOperand == true) lblDisplay.Text = "0";
+            if (lblDisplay.Text != "0" || setFirstOperand == true)
+            {
+                lblDisplay.Text = "0";
+            }
             firstOperand = "0";
             secondOperand = "0";
             setDot = false;
             setFirstOperand = false;
             setAns = false;
+            temp = null;
         }
     }
 }
