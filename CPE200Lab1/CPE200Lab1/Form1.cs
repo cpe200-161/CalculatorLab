d5798ffecf6d3947dd3daccb2c2262ef53801996
PacharaBu <pachara_bu@cm.ac.th>
2018-08-21 15:24:53 +0700

    lab0 finished(แก้การจำกัดจำนวน8หลัก)

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -46,73 +46,118 @@ namespace CPE200Lab1
 
         private void btn1_Click(object sender, EventArgs e)
         {
+            if (lblDisplay.Text.Length < 8)
+            {
             lblDisplay.Text = "";
             Input += 1;
             lblDisplay.Text += Input;
+
+            }
+            
             
         }
 
         private void btn2_Click(object sender, EventArgs e)
         {
+            if (lblDisplay.Text.Length < 8)
+            {
             lblDisplay.Text = "";
             Input += 2;
             lblDisplay.Text += Input;
+
+            }
+            
 
         }
 
         private void btn3_Click(object sender, EventArgs e)
         {
+            if (lblDisplay.Text.Length < 8)
+            {
             lblDisplay.Text = "";
             Input += 3;
             lblDisplay.Text += Input;
+
+            }
+                
 
         }
 
         private void btn4_Click(object sender, EventArgs e)
         {
+            if (lblDisplay.Text.Length < 8)
+            {
             lblDisplay.Text = "";
             Input += 4;
             lblDisplay.Text += Input;
+
+            }
+                
 
         }
 
         private void btn5_Click(object sender, EventArgs e)
         {
+            if (lblDisplay.Text.Length < 8)
+            {
             lblDisplay.Text = "";
             Input += 5;
             lblDisplay.Text += Input;
+
+            }
+            
 
         }
 
         private void btn6_Click(object sender, EventArgs e)
         {
+            if (lblDisplay.Text.Length < 8)
+            {
             lblDisplay.Text = "";
             Input += 6;
             lblDisplay.Text += Input;
+
+            }
+               
 
         }
 
         private void btn7_Click(object sender, EventArgs e)
         {
+            if (lblDisplay.Text.Length < 8)
+            {
             lblDisplay.Text = "";
             Input += 7;
             lblDisplay.Text = Input;
+
+            }
+                
 
         }
 
         private void btn8_Click(object sender, EventArgs e)
         {
+            if (lblDisplay.Text.Length < 8)
+            {
             lblDisplay.Text = "";
             Input += 8;
             lblDisplay.Text += Input;
+
+            }
+                
 
         }
 
         private void btn9_Click(object sender, EventArgs e)
         {
+            if (lblDisplay.Text.Length < 8)
+            {
             lblDisplay.Text = "";
             Input += 9;
             lblDisplay.Text += Input;
+
+            }
+            
 
         }
 
@@ -120,9 +165,12 @@ namespace CPE200Lab1
         {
             if ((Input != string.Empty))
             {
+                if (lblDisplay.Text.Length < 8)
+                { 
                 lblDisplay.Text = "";
                 Input += 0;
                 lblDisplay.Text += Input;
+                }
             }
             else
             {
@@ -135,46 +183,56 @@ namespace CPE200Lab1
         }
 
         private void btnPlus_Click(object sender, EventArgs e)
-        {   if(Num1 == string.Empty) { 
-            Num1 = Input;}
+        {   if(Num1 == string.Empty)
+            { 
+            Num1 = Input;
+            }
+
             Operation = '+';
             Input = string.Empty;
-            Percent = true;
+            lblDisplay.Text = "";
+            
 
 
         }
 
         private void btnMinus_Click(object sender, EventArgs e)
         {
-            if (Num1 == string.Empty) {
-            Num1 = Input;}
-                
-            
+            if (Num1 == string.Empty)
+            {
+            Num1 = Input;
+            }
+
             Operation = '-';
             Input = string.Empty;
-            Percent = true;
+            lblDisplay.Text = "";
+
         }
 
         private void btnMultiply_Click(object sender, EventArgs e)
         {
-            if (Num1 == string.Empty){
-            Num1 = Input;}
-                
-            
+            if (Num1 == string.Empty)
+            {
+            Num1 = Input;
+            }
+ 
             Operation = '*';
             Input = string.Empty;
-            Percent = true;
+            lblDisplay.Text = "";
+
         }
 
         private void btnDivide_Click(object sender, EventArgs e)
         {
-            if (Num1 == string.Empty){
-            Num1 = Input;}
-                
-            
+            if (Num1 == string.Empty)
+            {
+            Num1 = Input;
+            }
+
             Operation = '/';
             Input = string.Empty;
-            Percent = true;
+            lblDisplay.Text = "";
+
         }
 
         private void btnEqual_Click(object sender, EventArgs e)
@@ -201,7 +259,7 @@ namespace CPE200Lab1
 
                 lblDisplay.Text = Result.ToString();
                 Plus = true;
-                Percent = true;
+                
             }
             else if (Operation == '-')
             {
@@ -209,14 +267,14 @@ namespace CPE200Lab1
 
                 lblDisplay.Text = Result.ToString();
 
-                Percent = true;
+                
             }
             else if (Operation == '*')
             {
                 Result = num1 * num2;
                 lblDisplay.Text = Result.ToString();
 
-                Percent =  true;
+                
             }
             else if (Operation == '/')
             {
@@ -228,7 +286,7 @@ namespace CPE200Lab1
                     Result = 0;
                     Num2 = string.Empty;
 
-                    Percent =  true;
+                    
 
                 }
                 else
@@ -269,12 +327,16 @@ namespace CPE200Lab1
         private void btnDot_Click(object sender, EventArgs e)
         {
             if (!Input.Contains("."))
-            {
-              
+            { 
             Input += ".";
             lblDisplay.Text += ".";
             }
             
+        }
+
+        private void btnSign_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
