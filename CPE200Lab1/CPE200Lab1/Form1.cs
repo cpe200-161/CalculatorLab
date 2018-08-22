5c0b87b8ed3a0dcc3d5e7e9cbf444bf7097dd577
RAKSAMON KUMPAPHAN <raksamon_k@cmu.ac.th>
2018-08-22 22:58:39 +0700

    -ใส่ตัวเลข -กดจุดทศนิยมไม่ซ้ำ -คำนวณได้ -ลบตัวหลังได้ -เปลี่ยนค่าบวกลบได้ -เซทค่าC

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -12,128 +12,94 @@ namespace CPE200Lab1
 {
     public partial class Form1 : Form
     {
+        Double value = 0;
+        string oparetion = "";
+        bool oparetion_pressed = false;
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void btn1_Click(object sender, EventArgs e)
+        private void btnX_click(object sender, EventArgs e)
         {
-            if (lblDisplay.Text == "0")
+            Button btn = (Button)sender;
+            if ((lblDisplay.Text == "0") || (oparetion_pressed))
             {
                 lblDisplay.Text = "";
             }
-            if(lblDisplay.Text.Length <= 7)
+            oparetion_pressed = false;
+
+            if (btn.Text == ".")
             {
-                lblDisplay.Text = lblDisplay.Text + "1";
+                if (!lblDisplay.Text.Contains("."))
+                    lblDisplay.Text = lblDisplay.Text + btn.Text;
+            }
+            else if (lblDisplay.Text.Length <= 7)
+            {
+                lblDisplay.Text = lblDisplay.Text + btn.Text;
+            }
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            value = 0;
+            lblDisplay.Text = "0";
+        }
+
+        private void operator_click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            oparetion = btn.Text;
+            value = Double.Parse(lblDisplay.Text);
+            oparetion_pressed = true;
+        }
+
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+            switch (oparetion)
+            {
+                case "+":
+                    lblDisplay.Text = (value + Double.Parse(lblDisplay.Text)).ToString();
+                    break;
+                case "-":
+                    lblDisplay.Text = (value - Double.Parse(lblDisplay.Text)).ToString();
+                    break;
+                case "X":
+                    lblDisplay.Text = (value * Double.Parse(lblDisplay.Text)).ToString();
+                    break;
+                case "÷":
+                    lblDisplay.Text = (value / Double.Parse(lblDisplay.Text)).ToString();
+                    break;
+                default:
+                    break;
+            }
+            oparetion_pressed = false;
+        }
+
+        private void btnPercent_Click(object sender, EventArgs e)
+        {
+            lblDisplay.Text = (value * (Double.Parse(lblDisplay.Text) / 100)).ToString();
+        }
+
+        private void btnSign_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text.StartsWith("-"))
+            {
+                lblDisplay.Text = lblDisplay.Text.Substring(1);
+            }
+            else
+            {
+                lblDisplay.Text = "-" + lblDisplay.Text;
             }
         }
 
-        private void btn2_Click(object sender, EventArgs e)
+        private void btnBack_Click(object sender, EventArgs e)
         {
-            if (lblDisplay.Text == "0")
+            if(lblDisplay.Text.Length > 0)
             {
-                lblDisplay.Text = "";
-            }
-            if (lblDisplay.Text.Length <= 7)
-            {
-                lblDisplay.Text = lblDisplay.Text + "2";
-            }
-        }
-
-        private void btn3_Click(object sender, EventArgs e)
-        {
-            if (lblDisplay.Text == "0")
-            {
-                lblDisplay.Text = "";
-            }
-            if (lblDisplay.Text.Length <= 7)
-            {
-                lblDisplay.Text = lblDisplay.Text + "3";
-            }
-        }
-
-        private void btn4_Click(object sender, EventArgs e)
-        {
-            if (lblDisplay.Text == "0")
-            {
-                lblDisplay.Text = "";
-            }
-            if (lblDisplay.Text.Length <= 7)
-            {
-                lblDisplay.Text = lblDisplay.Text + "4";
-            }
-        }
-
-        private void btn5_Click(object sender, EventArgs e)
-        {
-            if (lblDisplay.Text == "0")
-            {
-                lblDisplay.Text = "";
-            }
-            if (lblDisplay.Text.Length <= 7)
-            {
-                lblDisplay.Text = lblDisplay.Text + "5";
-            }
-        }
-
-        private void btn6_Click(object sender, EventArgs e)
-        {
-            if (lblDisplay.Text == "0")
-            {
-                lblDisplay.Text = "";
-            }
-            if (lblDisplay.Text.Length <= 7)
-            {
-                lblDisplay.Text = lblDisplay.Text + "6";
-            }
-        }
-
-        private void btn7_Click(object sender, EventArgs e)
-        {
-            if (lblDisplay.Text == "0")
-            {
-                lblDisplay.Text = "";
-            }
-            if (lblDisplay.Text.Length <= 7)
-            {
-                lblDisplay.Text = lblDisplay.Text + "7";
-            }
-        }
-
-        private void btn8_Click(object sender, EventArgs e)
-        {
-            if (lblDisplay.Text == "0")
-            {
-                lblDisplay.Text = "";
-            }
-            if (lblDisplay.Text.Length <= 7)
-            {
-                lblDisplay.Text = lblDisplay.Text + "8";
-            }
-        }
-
-        private void btn9_Click(object sender, EventArgs e)
-        {
-            if (lblDisplay.Text == "0")
-            {
-                lblDisplay.Text = "";
-            }
-            if (lblDisplay.Text.Length <= 7)
-            {
-                lblDisplay.Text = lblDisplay.Text + "9";
-            }
-        }
-
-        private void btn0_Click(object sender, EventArgs e)
-        {
-            if (lblDisplay.Text == "0")
-            {
-                lblDisplay.Text = "";
-            }
-            if (lblDisplay.Text.Length <= 7)
-            {
-                lblDisplay.Text = lblDisplay.Text + "0";
+                lblDisplay.Text = lblDisplay.Text.Remove(lblDisplay.Text.Length - 1, 1);
             }
         }
     }
