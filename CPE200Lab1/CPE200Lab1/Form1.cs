bd4150a321d7689841068beae9658d7c4f2daabd
EKAWIT JAIDEE <ekawit_jaidee@cmu.ac.th>
2018-08-22 15:30:11 +0700

    operator

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -12,9 +12,31 @@ namespace CPE200Lab1
 {
     public partial class Form1 : Form
     {
+        private string Fristnum = null;
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void btnX_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "";
+            }
+            if (lblDisplay.Text.Length <= 8)
+            {
+                lblDisplay.Text = lblDisplay.Text + btn.Text;
+
+            }
+
+        }
+
+        
+
+        
+
+        
     }
 }
