17b62ec1cbf509e68de944313bdc3807231eb30e
Admin <Admin@LAPTOP-A2PKKB67>
2018-08-24 15:25:08 +0700

    1.completed 2. TA checked

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -12,128 +12,146 @@ namespace CPE200Lab1
 {
     public partial class Form1 : Form
     {
+        float resultValue = 0;
+        float reverse = 0;
+        float percent = 0;
+        int count = 0;
+        string operationPerformed = "";
+        bool Performed1 = false;
+        bool Performed2 = false;
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void btn1_Click(object sender, EventArgs e)
+        private void btnX_Click(object sender, EventArgs e)
         {
-            if (lblDisplay.Text == "0")
+            Button btn = (Button)sender;
+            if (count == 2)
             {
-                lblDisplay.Text = "";
+                result.Text = string.Empty;
+                count = 1;
             }
-            if (lblDisplay.Text.Length <= 7)
+            if (result.Text == "0")
             {
-                lblDisplay.Text = lblDisplay.Text + "1";
+                result.Text = "";
             }
+            if (result.Text.Length <8)
+            {
+                result.Text = result.Text + btn.Text;
+            }
+            Performed2 = true;
+        }
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            switch (operationPerformed)
+            {
+                    case "+":
+                        if (count == 1)
+                        {
+                            resultValue = resultValue + float.Parse(result.Text);
+                            result.Text = resultValue.ToString();
+                        }
+                        break;
+                    case "-":
+                        if (count == 1)
+                        {
+                            resultValue = resultValue - float.Parse(result.Text);
+                            result.Text = resultValue.ToString();
+                        }
+                        break;
+                    case "X":
+                        if (count == 1)
+                        {
+                            resultValue = resultValue * float.Parse(result.Text);
+                            result.Text = resultValue.ToString();
+                        }
+                        break;
+                    case "/":
+                        if (count == 1)
+                        {
+                            resultValue = resultValue / float.Parse(result.Text);
+                            result.Text = resultValue.ToString();
+                        }
+                        break;
+         
+            }
+                    resultValue = float.Parse(result.Text);
+                    if (resultValue % 2 != 0)
+                    {
+                        Performed1 = true;
+                    }
+                    operationPerformed = btn.Text;
+                    count = 2;
+                    Performed2 = false;
+                    Performed1 = false;
         }
 
-        private void btn2_Click(object sender, EventArgs e)
+
+        private void btnSign_Click(object sender, EventArgs e)
         {
-            if (lblDisplay.Text == "0")
-            {
-                lblDisplay.Text = "";
-            }
-            if (lblDisplay.Text.Length <= 7)
-            {
-                lblDisplay.Text = lblDisplay.Text + "2";
-            }
+            reverse = -float.Parse(result.Text);
+            result.Text = reverse.ToString();
         }
 
-        private void btn3_Click(object sender, EventArgs e)
+        private void btnDot_Click(object sender, EventArgs e)
         {
-            if (lblDisplay.Text == "0")
+            if (Performed1 == false && count == 2)
             {
-                lblDisplay.Text = "";
+                result.Text = "0" + ".";
+                Performed1 = true;
             }
-            if (lblDisplay.Text.Length <= 7)
+            else if (Performed1 == false)
             {
-                lblDisplay.Text = lblDisplay.Text + "3";
+                result.Text = result.Text + ".";
+                Performed1 = true;
             }
+           
         }
 
-        private void btn4_Click(object sender, EventArgs e)
+        private void btnClear_Click(object sender, EventArgs e)
         {
-            if (lblDisplay.Text == "0")
-            {
-                lblDisplay.Text = "";
-            }
-            if (lblDisplay.Text.Length <= 7)
-            {
-                lblDisplay.Text = lblDisplay.Text + "4";
-            }
+            result.Text = "0";
+            resultValue = 0;
         }
 
-        private void btn5_Click(object sender, EventArgs e)
+        private void btnPercent_Click(object sender, EventArgs e)
         {
-            if (lblDisplay.Text == "0")
+            percent = float.Parse(result.Text);
+            switch (operationPerformed)
             {
-                lblDisplay.Text = "";
+                case "+":
+                    resultValue = resultValue + (resultValue*(percent / 100));
+                    break;
+                case "-":
+                    resultValue = resultValue - (resultValue * (percent / 100));
+                    break;
+                case "X":
+                    resultValue = (resultValue * (percent / 100));
+                    break;
+                case "/":
+                    resultValue = (resultValue / (percent / 100));
+                    break;
             }
-            if (lblDisplay.Text.Length <= 7)
-            {
-                lblDisplay.Text = lblDisplay.Text + "5";
-            }
+            result.Text = resultValue.ToString();
+            resultValue = float.Parse(result.Text);
+            operationPerformed = "";
         }
 
-        private void btn6_Click(object sender, EventArgs e)
+        private void btnBack_Click(object sender, EventArgs e)
         {
-            if (lblDisplay.Text == "0")
+            if (Performed2 == true)
             {
-                lblDisplay.Text = "";
-            }
-            if (lblDisplay.Text.Length <= 7)
-            {
-                lblDisplay.Text = lblDisplay.Text + "6";
-            }
-        }
-
-        private void btn7_Click(object sender, EventArgs e)
-        {
-            if (lblDisplay.Text == "0")
-            {
-                lblDisplay.Text = "";
-            }
-            if (lblDisplay.Text.Length <= 7)
-            {
-                lblDisplay.Text = lblDisplay.Text + "7";
-            }
-        }
-
-        private void btn8_Click(object sender, EventArgs e)
-        {
-            if (lblDisplay.Text == "0")
-            {
-                lblDisplay.Text = "";
-            }
-            if (lblDisplay.Text.Length <= 7)
-            {
-                lblDisplay.Text = lblDisplay.Text + "8";
-            }
-        }
-
-        private void btn9_Click(object sender, EventArgs e)
-        {
-            if (lblDisplay.Text == "0")
-            {
-                lblDisplay.Text = "";
-            }
-            if (lblDisplay.Text.Length <= 7)
-            {
-                lblDisplay.Text = lblDisplay.Text + "9";
-            }
-        }
-
-        private void btn0_Click(object sender, EventArgs e)
-        {
-            if (lblDisplay.Text == "0")
-            {
-                lblDisplay.Text = "";
-            }
-            if (lblDisplay.Text.Length <= 7)
-            {
-                lblDisplay.Text = lblDisplay.Text + "0";
+                if (result.Text.Length > 1)
+                {
+                    result.Text = result.Text.Remove(result.Text.Length - 1);
+                }
+                else
+                {
+                    result.Text = "0";
+                }
             }
         }
     }
