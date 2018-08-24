aacc3f18606a4308d40dc870622665cd899f20c6
attakorn0798 <35348851+attakorn0798@users.noreply.github.com>
2018-08-24 15:54:15 +0700

    complete

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -12,9 +12,148 @@ namespace CPE200Lab1
 {
     public partial class Form1 : Form
     {
+        float Sum = 0;
+        float R = 0;
+        float value1 = 0;
+        int count = 0;
+        string X = "";
+        bool check1 = false;
+        bool check2 = false;
+        float percent;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void btnx_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (count == 2)
+            {
+                lblDisplay.Text = string.Empty;
+                count = 1;
+            }
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "";
+            }
+            if (lblDisplay.Text.Length < 8)
+            {
+                lblDisplay.Text = lblDisplay.Text + btn.Text;
+            }
+            check2 = true;
+        }
+
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            value1 = float.Parse(lblDisplay.Text);
+            switch (X)
+            {
+                case "+":
+                    if (count == 1)
+                    {
+                        Sum = Sum + float.Parse(lblDisplay.Text);
+                        lblDisplay.Text = Sum.ToString();
+                    }
+                    break;
+                case "-":
+                    if (count == 1)
+                    {
+                        Sum = Sum - float.Parse(lblDisplay.Text);
+                        lblDisplay.Text = Sum.ToString();
+                    }
+                    break;
+                case "/":
+                    if (count == 1)
+                    {
+                        Sum = Sum / float.Parse(lblDisplay.Text);
+                        lblDisplay.Text = Sum.ToString();
+                    }
+                    break;
+                case "X":
+                    if (count == 1)
+                    {
+                        Sum = Sum * float.Parse(lblDisplay.Text);
+                        lblDisplay.Text = Sum.ToString();
+                    }
+                    break;
+
+
+            }
+
+            Sum = float.Parse(lblDisplay.Text);
+            if (Sum % 2 != 0)
+            {
+                check1 = true;
+            }
+            X = btn.Text;
+            count = 2;
+            check2 = false;
+        }
+
+        private void btndot_Click(object sender, EventArgs e)
+        {
+            if (!(lblDisplay.Text.Contains(".")))
+            {
+                lblDisplay.Text = lblDisplay.Text + ".";
+            }
+        }
+
+        private void btnSign_Click(object sender, EventArgs e)
+        {
+            R = -float.Parse(lblDisplay.Text);
+            lblDisplay.Text = R.ToString();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lblDisplay.Text = string.Empty;
+            lblDisplay.Text = "0";
+            Sum = 0;
+            R = 0;
+            count = 0;
+            X = "";
+            check1 = false;
+            check2 = false;
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            if (check2 == true)
+            {
+                if (lblDisplay.Text.Length > 1)
+                {
+                    lblDisplay.Text = lblDisplay.Text.Remove(lblDisplay.Text.Length - 1);
+                }
+                else
+                {
+                    lblDisplay.Text = "0";
+                }
+            }
+        }
+
+        private void btnPercent_Click(object sender, EventArgs e)
+        {
+            switch (X) {
+                case "+":
+                    Sum = Sum + (value1 * (float.Parse(lblDisplay.Text) / 100));
+                    break; 
+                case "-":
+                    lblDisplay.Text = (value1 * (float.Parse(lblDisplay.Text) / 100)).ToString();
+                    break;
+                case "X":
+                    lblDisplay.Text = (float.Parse(lblDisplay.Text) / 100).ToString();
+                    break;
+                case "÷":
+                    lblDisplay.Text = (float.Parse(lblDisplay.Text) / 100).ToString();
+                    break;
+
+            }
+            lblDisplay.Text = Sum.ToString();
+
+
+    }
     }
 }
