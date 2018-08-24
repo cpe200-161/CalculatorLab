62ef4267972b9f614932e64670e2036c0d6f922a
Madhannya <35358377+Madhannya@users.noreply.github.com>
2018-08-24 14:11:42 +0700

    1.)  ตัวโปรเเกรมสามารถ บวก ลบ คูณ และ หารได้ 2.)  ตัวโปรเเกรมสามารถคิดเลขเป็นจุดทศนิยนได้ 3.)  ตัวโปรเเกรมสามารถใช้ฟังก์ชัน เปอร์เซนต์ ได้

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -12,9 +12,126 @@ namespace CPE200Lab1
 {
     public partial class Form1 : Form
     {
+        double N1 = 0;
+        double N2 = 0;
+        double Forwhat;
+        bool haveN1= false;
+        bool finishyet = false;
+        bool havedot = false;
+        double N3;
+        string Operator ="";
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnX_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            
+            if(haveN1 == true)
+            {
+                lblDisplay.Text = "";
+                haveN1 = false;
+            }
+            if(lblDisplay.Text == "0" || finishyet == true)
+            {
+                lblDisplay.Text = "";
+                finishyet = false;
+            }
+            if (lblDisplay.Text.Length < 8)
+            {
+                lblDisplay.Text = lblDisplay.Text + btn.Text;
+            }
+        }
+
+        private void btnY_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if(Operator != "0")
+            {
+                N2 = Convert.ToDouble(lblDisplay.Text);
+                if (Operator == "+")
+                {
+                    lblDisplay.Text = Convert.ToString(N1 + N2);
+                }
+                if (Operator == "-")
+                {
+                    lblDisplay.Text = Convert.ToString(N1 - N2);
+                }
+                if (Operator == "X")
+                {
+                    lblDisplay.Text = Convert.ToString(N1 * N2);
+                }
+                if (Operator == "÷")
+                {
+                    lblDisplay.Text = Convert.ToString(N1 / N2);
+                }
+            }
+            N1 = double.Parse(lblDisplay.Text);
+            haveN1 = true;
+            havedot = false;
+            Operator = (btn.Text);
+
+        }
+       
+
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+            N2 = Convert.ToDouble(lblDisplay.Text);
+            if(Operator == "+")
+            {
+                lblDisplay.Text = Convert.ToString(N1 + N2);
+            }
+            if (Operator == "-")
+            {
+                lblDisplay.Text = Convert.ToString(N1 - N2);
+            }
+            if (Operator == "X")
+            {
+                lblDisplay.Text = Convert.ToString(N1 * N2);
+            }
+            if (Operator == "÷")
+            {
+                lblDisplay.Text = Convert.ToString(N1 / N2);
+            }
+            finishyet = true;
+            havedot = false;
+            Operator = "0";
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lblDisplay.Text = "0";
+            N1 = 0;
+            N2 = 0;
+        }
+
+        private void btnDot_Click(object sender, EventArgs e)
+        {
+            if(havedot != true)
+            {
+                lblDisplay.Text = lblDisplay.Text + ".";
+                havedot = true;
+            }
+        }
+
+        private void btnPercent_Click(object sender, EventArgs e)
+        {
+            
+            if (Operator == "+" || Operator =="-")
+            {
+                N2 = Convert.ToDouble(lblDisplay.Text);
+                Forwhat = (N1 * N2) / 100;
+                lblDisplay.Text = Convert.ToString(Forwhat);
+                finishyet = true;
+                N1 = 0;
+                N2 = 0;
+                Operator = "";
+                
+            }
+            else Forwhat = Convert.ToDouble(lblDisplay.Text) / 100;
+                 lblDisplay.Text = Convert.ToString(Forwhat);
         }
     }
 }
