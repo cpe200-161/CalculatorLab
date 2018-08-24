f6fac276c9df301d113da3c09414d8815a2d6952
SIRIN RATTANARUANG-AMPHAI <sirin_r@cmu.ac.th>
2018-08-24 15:44:36 +0700

    Complete........

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -12,8 +12,8 @@ namespace CPE200Lab1
 {
     public partial class Form1 : Form
     {
-        double a = 0;
-        double b = 0;
+        float a = 0;
+        float b = 0;
         string result = null;
         bool check = false;
         bool check2 = false;
@@ -45,6 +45,7 @@ namespace CPE200Lab1
             if (lblDisplay.Text.Length < 8)
             {
                 lblDisplay.Text = lblDisplay.Text + ".";
+                check3 = true;
             }
         }
 
@@ -56,30 +57,24 @@ namespace CPE200Lab1
             {
                 case "+":
                     a = a + b;
-                    result = Convert.ToString(a);
-                    lblDisplay.Text = result;
                     break;
                 case "-":
                     a = a - b;
-                    result = Convert.ToString(a);
-                    lblDisplay.Text = result;
                     break;
                 case "X":
                     a = a * b;
-                    result = Convert.ToString(a);
-                    lblDisplay.Text = result;
                     break;
                 case "÷":
                     a = a / b;
-                    result = Convert.ToString(a);
-                    lblDisplay.Text = result;
                     break;
             }
+            result = Convert.ToString(a);
+            lblDisplay.Text = result;
         }
 
         private void btnEqual_Click(object sender, EventArgs e)
         {
-            b = Convert.ToDouble(lblDisplay.Text);
+            b = float.Parse(lblDisplay.Text);
             if (check3)
             {
                 Cal();
@@ -93,11 +88,11 @@ namespace CPE200Lab1
             operate = btn.Text;
             if (!check2)
             {
-                a = Convert.ToDouble(lblDisplay.Text);
+                a = float.Parse(lblDisplay.Text);
             }
             else
             {
-                b = Convert.ToDouble(lblDisplay.Text);
+                b = float.Parse(lblDisplay.Text);
                 if (check3)
                 {
                     Cal();
@@ -112,7 +107,7 @@ namespace CPE200Lab1
         {
             if (!check2)
             {
-                a = Convert.ToDouble(lblDisplay.Text);
+                a = float.Parse(lblDisplay.Text);
                 lblDisplay.Text = "0";
                 a = 0;
             }
@@ -120,34 +115,25 @@ namespace CPE200Lab1
             {
                 if (check3)
                 {
-                    b = Convert.ToDouble(lblDisplay.Text);
+                    b = float.Parse(lblDisplay.Text);
                     System.Console.WriteLine("b in check3 is " + b);
                     switch (operate)
                     {
                         case "+":
-                            b = (b / 100) * a;
-                            a = a + b;
-                            result = Convert.ToString(a);
-                            lblDisplay.Text = result;
+                            a = a + (b / 100) * a;
                             break;
                         case "-":
-                            b = (b / 100) * a;
-                            a = a - b;
-                            result = Convert.ToString(a);
-                            lblDisplay.Text = result;
+                            a = a - (b / 100) * a;
                             break;
                         case "X":
-                            a = a * b/100;
-                            result = Convert.ToString(a);
-                            System.Console.WriteLine("b in X " + a);
-                            lblDisplay.Text = result;
+                            a = a * (b/100);
                             break;
                         case "÷":
-                            a = a / b/100;
-                            result = Convert.ToString(a);
-                            lblDisplay.Text = result;
+                            a = a / (b/100);
                             break;
                     }
+                    result = Convert.ToString(a);
+                    lblDisplay.Text = result;
                 }      
             }
             check = true;
