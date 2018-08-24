611712c27d1194685d7b17383d6a538dc7efbc80
Kritsana Manupha <guy_lll@hotmail.com>
2018-08-24 13:14:56 +0700

    can press number

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -12,11 +12,7 @@ namespace CPE200Lab1
 {
     public partial class Form1 : Form
     {
-        private int val = 0, temp = 0, sum = 0, temp2 = 0;
-        private bool plusval = false;
-        private bool minusval = false;
-        private bool multival = false;
-        private bool divideval = false;
+        
 
         public Form1()
         {
@@ -28,74 +24,148 @@ namespace CPE200Lab1
 
         }
         
-        private void shownum()
-        {
-            lblDisplay.Text = val.ToString();
-        }
 
-        private void btClick(int num)
-        {
-            val = val * 10 + num;
-            shownum();
-        }
-
+          
         private void btn0_Click(object sender, EventArgs e)
         {
-            btClick(0);
-        }
-
-        private void btn4_Click(object sender, EventArgs e)
-        {
-            btClick(4);
-        }
-
-        private void btn7_Click(object sender, EventArgs e)
-        {
-            btClick(7);
-        }
-
-        private void btn8_Click(object sender, EventArgs e)
-        {
-            btClick(8);
-        }
-
-        private void btn9_Click(object sender, EventArgs e)
-        {
-            btClick(9);
-        }
-
-        private void btn6_Click(object sender, EventArgs e)
-        {
-            btClick(6);
+           
         }
 
         private void btn1_Click(object sender, EventArgs e)
         {
-            btClick(1);
+            if (lblDisplay.Text.Length < 8){
+                if (lblDisplay.Text == "0" && lblDisplay.Text != null)
+                {
+                    lblDisplay.Text = "1";
+                }
+                else
+                {
+                    lblDisplay.Text = lblDisplay.Text + "1";
+                }
+            }
         }
 
         private void btn2_Click(object sender, EventArgs e)
         {
-            btClick(2);
+            if (lblDisplay.Text.Length < 8)
+            {
+                if (lblDisplay.Text == "0" && lblDisplay.Text != null)
+                {
+                    lblDisplay.Text = "2";
+                }
+                else
+                {
+                    lblDisplay.Text = lblDisplay.Text + "2";
+                }
+            }
         }
 
         private void btn3_Click(object sender, EventArgs e)
         {
-            btClick(3);
+           if (lblDisplay.Text.Length < 8){
+                if (lblDisplay.Text == "0" && lblDisplay.Text != null)
+                {
+                    lblDisplay.Text = "3";
+                }
+                else
+                {
+                    lblDisplay.Text = lblDisplay.Text + "3";
+                }
+            }
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text.Length < 8)
+            {
+                if (lblDisplay.Text == "0" && lblDisplay.Text != null)
+                {
+                    lblDisplay.Text = "4";
+                }
+                else
+                {
+                    lblDisplay.Text = lblDisplay.Text + "4";
+                }
+            }
         }
 
         private void btn5_Click(object sender, EventArgs e)
         {
-            btClick(5);
+            if (lblDisplay.Text.Length < 8)
+            {
+                if (lblDisplay.Text == "0" && lblDisplay.Text != null)
+                {
+                    lblDisplay.Text = "5";
+                }
+                else
+                {
+                    lblDisplay.Text = lblDisplay.Text + "5";
+                }
+            }
         }
 
-      
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text.Length < 8)
+            {
+                if (lblDisplay.Text == "0" && lblDisplay.Text != null)
+                {
+                    lblDisplay.Text = "6";
+                }
+                else
+                {
+                    lblDisplay.Text = lblDisplay.Text + "6";
+                }
+            }
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text.Length < 8)
+            {
+                if (lblDisplay.Text == "0" && lblDisplay.Text != null)
+                {
+                    lblDisplay.Text = "7";
+                }
+                else
+                {
+                    lblDisplay.Text = lblDisplay.Text + "7";
+                }
+            }
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text.Length < 8)
+            {
+                if (lblDisplay.Text == "0" && lblDisplay.Text != null)
+                {
+                    lblDisplay.Text = "8";
+                }
+                else
+                {
+                    lblDisplay.Text = lblDisplay.Text + "8";
+                }
+            }
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text.Length < 8)
+            {
+                if (lblDisplay.Text == "0" && lblDisplay.Text != null)
+                {
+                    lblDisplay.Text = "9";
+                }
+                else
+                {
+                    lblDisplay.Text = lblDisplay.Text + "9";
+                }
+            }
+        }
 
         private void btnPlus_Click(object sender, EventArgs e)
-        {
-            temp = val;
-            val = 0;
-            plusval = true;
+        {         
 
         }
 
@@ -106,28 +176,13 @@ namespace CPE200Lab1
 
         private void btnEqual_Click(object sender, EventArgs e)
         {
-            if(plusval == true)
-            {
-                sum = temp2 + temp + val;
-                temp2 = sum;
-                val = 0;
-                lblDisplay.Text = sum.ToString();
-                
-                plusval = false;
-                 
-            }
-         
-            //lblDisplay.Text = sum.ToString();
-            
+             
         }
 
         private void btnClear_Click(object sender, EventArgs e)
         {
-            sum = 0;
-            val = 0;
-            temp = 0;
-            temp2 = 0;
-            shownum();
+            lblDisplay.Text = "";
+            }
         }
     }
 }
