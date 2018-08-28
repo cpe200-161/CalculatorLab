1fc2027f18993584cc857706b5fc8eea94819afa
Chayootpong Promlee <chayootpong.chrom@gmail.com>
2018-08-28 15:37:39 +0700

    All Additional Button (Square root, Percentage, One over X) Move calculating relate Change UI

---
+++ CPE200Lab1/CPE200Lab1/CalculatorEngine.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPE200Lab1
+{
+    public class CalculatorEngine
+    {
+        public string calculate(string operate, string firstOperand, string secondOperand, string oldOperator, int maxOutputSize = 8)
+        {
+            switch (operate)
+            {
+                case "+":
+                    return (Convert.ToDouble(firstOperand) + Convert.ToDouble(secondOperand)).ToString();
+                case "-":
+                    return (Convert.ToDouble(firstOperand) - Convert.ToDouble(secondOperand)).ToString();
+                case "X":
+                    return (Convert.ToDouble(firstOperand) * Convert.ToDouble(secondOperand)).ToString();
+                case "÷":
+                    // Not allow devide be zero
+                    if (secondOperand != "0")
+                    {
+                        double result;
+                        string[] parts;
+                        int remainLength;
+
+                        result = (Convert.ToDouble(firstOperand) / Convert.ToDouble(secondOperand));
+                        // split between integer part and fractional part
+                        parts = result.ToString().Split('.');
+                        // if integer part length is already break max output, return error
+                        if (parts[0].Length > maxOutputSize)
+                        {
+                            return "E";
+                        }
+                        // calculate remaining space for fractional part.
+                        remainLength = maxOutputSize - parts[0].Length - 1;
+                        // trim the fractional part gracefully. =
+                        return result.ToString("N" + remainLength);
+                    }
+                    break;
+                case "%":
+
+                    if (oldOperator == "+") return (Convert.ToDouble(firstOperand) + Convert.ToDouble(secondOperand)).ToString();
+                    else if (oldOperator == "-") return (Convert.ToDouble(firstOperand) - Convert.ToDouble(secondOperand)).ToString();
+                    else if (oldOperator == "X") return (Convert.ToDouble(firstOperand) * Convert.ToDouble(secondOperand)).ToString();
+                    else if (oldOperator == "÷") return (Convert.ToDouble(firstOperand) / Convert.ToDouble(secondOperand)).ToString();
+                    //your code here
+
+                    break;
+            }
+            return "E";
+        }
+
+        public string percent(string first, string second)
+        {
+            float ans = float.Parse(first) * float.Parse(second) / 100;
+
+            return ans.ToString();
+        }
+
+        public string sqrtRoot(string num)
+        {
+            float ans = (float)Math.Sqrt(float.Parse(num));
+
+            return ans.ToString();
+        }
+
+        public string oneOverX(string num)
+        {
+            float ans = 1 / (float.Parse(num));
+
+            return ans.ToString();
+        }
+    }
+}
--- CPE200Lab1/CPE200Lab1/MainForm.cs
+++ CPE200Lab1/CPE200Lab1/MainForm.cs
@@ -18,6 +18,8 @@ namespace CPE200Lab1
         private bool isAfterEqual;
         private string firstOperand;
         private string operate;
+        public CalculatorEngine engine;
+        private string oldOperator;
 
         private void resetAll()
         {
@@ -26,52 +28,14 @@ namespace CPE200Lab1
             hasDot = false;
             isAfterOperater = false;
             isAfterEqual = false;
-        }
-
-        private string calculate(string operate, string firstOperand, string secondOperand, int maxOutputSize = 8)
-        {
-            switch(operate)
-            {
-                case "+":
-                    return (Convert.ToDouble(firstOperand) + Convert.ToDouble(secondOperand)).ToString();
-                case "-":
-                    return (Convert.ToDouble(firstOperand) - Convert.ToDouble(secondOperand)).ToString();
-                case "X":
-                    return (Convert.ToDouble(firstOperand) * Convert.ToDouble(secondOperand)).ToString();
-                case "÷":
-                    // Not allow devide be zero
-                    if(secondOperand != "0")
-                    {
-                        double result;
-                        string[] parts;
-                        int remainLength;
-
-                        result = (Convert.ToDouble(firstOperand) / Convert.ToDouble(secondOperand));
-                        // split between integer part and fractional part
-                        parts = result.ToString().Split('.');
-                        // if integer part length is already break max output, return error
-                        if(parts[0].Length > maxOutputSize)
-                        {
-                            return "E";
-                        }
-                        // calculate remaining space for fractional part.
-                        remainLength = maxOutputSize - parts[0].Length - 1;
-                        // trim the fractional part gracefully. =
-                        return result.ToString("N" + remainLength);
-                    }
-                    break;
-                case "%":
-                    //your code here
-                    break;
-            }
-            return "E";
-        }
+        }       
 
         public MainForm()
         {
             InitializeComponent();
 
             resetAll();
+            engine = new CalculatorEngine();
         }
 
         private void btnNumber_Click(object sender, EventArgs e)
@@ -121,9 +85,12 @@ namespace CPE200Lab1
                 case "÷":
                     firstOperand = lblDisplay.Text;
                     isAfterOperater = true;
+                    oldOperator = operate;
                     break;
                 case "%":
                     // your code here
+                    string ans = engine.percent(firstOperand, lblDisplay.Text);
+                    lblDisplay.Text = ans;
                     break;
             }
             isAllowBack = false;
@@ -136,7 +103,7 @@ namespace CPE200Lab1
                 return;
             }
             string secondOperand = lblDisplay.Text;
-            string result = calculate(operate, firstOperand, secondOperand);
+            string result = engine.calculate(operate, firstOperand, secondOperand, oldOperator);
             if (result is "E" || result.Length > 8)
             {
                 lblDisplay.Text = "Error";
@@ -227,5 +194,18 @@ namespace CPE200Lab1
                 }
             }
         }
+
+        private void sqrt_Click(object sender, EventArgs e)
+        {
+            firstOperand = engine.sqrtRoot(lblDisplay.Text);
+            lblDisplay.Text = firstOperand;
+        }
+
+        private void oneOver_Click(object sender, EventArgs e)
+        {
+            firstOperand = engine.oneOverX(lblDisplay.Text);
+            lblDisplay.Text = firstOperand;
+        }
+
     }
 }
