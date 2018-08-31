d029c4538f85eb539d909368fc5c2099ceea0a0c
petpusin <pet_ray@windowslive.com>
2018-08-31 15:24:03 +0700

    #4

--- CPE200Lab1/CPE200Lab1/CalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/CalculatorEngine.cs
@@ -76,6 +76,26 @@ namespace CPE200Lab1
                     break;
                 case "%":
                     //your code here
+                    if (secondOperand != "0")
+                    {
+                        double result;
+                        string[] parts;
+                        int remainLength;
+
+                        result = (Convert.ToDouble(firstOperand) / Convert.ToDouble(secondOperand));
+                        // split between integer part and fractional part
+                        parts = result.ToString().Split('.');
+                        // if integer part length is already break max output, return error
+                        if (parts[0].Length > maxOutputSize)
+                        {
+                            return "E";
+                        }
+                        // calculate remaining space for fractional part.
+                        remainLength = maxOutputSize - parts[0].Length - 1;
+                        // trim the fractional part gracefully. =
+                        return result.ToString("N" + remainLength);
+                    }
+
                     break;
             }
             return "E";
