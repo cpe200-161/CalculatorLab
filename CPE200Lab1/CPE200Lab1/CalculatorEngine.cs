39702514046ea32cb0cdcf0a66d8dc405abe38f4
natapong87 <35356327+natapong87@users.noreply.github.com>
2018-08-31 13:27:33 +0700

    Lab1

---
+++ CPE200Lab1/CPE200Lab1/CalculatorEngine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPE200Lab1
+{
+    class CalculatorEngine
+    {
+        private bool hasDot;
+        private bool isAllowBack;
+        private bool isAfterOperater;
+        private bool isAfterEqual;
+        private string firstOperand;
+        private string operate;
+
+        
+
+
+
+
+
+
+
+
+
+
+    }
+}
