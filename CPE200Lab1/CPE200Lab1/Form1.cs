61ca34189629cc7bd2ddc0b09f581e3606b5461f
PAKHATAI KANTANG <pakhatai_kantang@cmu.ac.th>
2018-09-03 16:11:37 +0700

    600612161

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -12,177 +12,372 @@ namespace CPE200Lab1
 {
     public partial class Form1 : Form
     {
-        private double  val = 0;
-        private double je1,sum;
-        private string before = "";
-        private int P =0,r=0;
-        private double W = 0;
+        private double val = 0;
+        private double sum = 0;
+        private string num;
+        private int Oper = 0, i = 0;
+
 
         public Form1()
         {
             InitializeComponent();
         }
-       
- 
-        private void display(double show){
-            lblDisplay.Text = show.ToString();
-        }
 
-        private void numclick(int n,double m=10)
-        {
-            val = val * m + n;
-            display(val);
-        }
-        
-        private void btn1_Click(object sender, EventArgs e)
-        {
-            numclick(1);
-        }
 
-        private void btn2_Click(object sender, EventArgs e)
-        {
-            numclick(2);
-        }
-
-        private void btn3_Click(object sender, EventArgs e)
-        {
-            numclick(3);
-        }
-
-        private void btn4_Click(object sender, EventArgs e)
-        {
-            numclick(4);
-        }
-
-        private void btn5_Click(object sender, EventArgs e)
-        {
-            numclick(5);
-        }
-
-        private void btn6_Click(object sender, EventArgs e)
-        {
-            numclick(6);
-        }
-
-        private void btn7_Click(object sender, EventArgs e)
-        {
-            numclick(7);
-        }
-        private void btn8_Click(object sender, EventArgs e)
-        {
-            numclick(8);
-        }
-
-        private void btn9_Click(object sender, EventArgs e)
-        {
-            numclick(9);
-        }
-        private void btn0_Click(object sender, EventArgs e)
-        {
-            numclick(0);
-        }
-
-        private void lblDisplay_Click(object sender, EventArgs e)
-        {
-            
-           
-        }
-        private void btnSign_Click(object sender, EventArgs e)
-        {
-            val = (val * (-1));
-            display(val);
-
-        }
-       
-        
-        private void btnDot_Click(object sender, EventArgs e)
+        private void display(string text, int i = 0) {
+            if (text.Length > 9)
             {
-            
+                display("Error");
+            }
+            else {
+                switch (i)
+                {
+                    case 1:
+                        lblDisplay.Text = "-" + text;
+                        num = lblDisplay.Text;
+                        break;
+
+                    case 2:
+                        lblDisplay.Text = text + ".";
+                        num = lblDisplay.Text;
+                        break;
+
+                    case 3:
+                        lblDisplay.Text = text + "%";
+                        num = lblDisplay.Text;
+                        break;
+
+                    default:
+                        lblDisplay.Text = text;
+                        break;
+
+                }
+
             }
 
-        private void btnClear_Click(object sender, EventArgs e)
+        }
+        private void numclick(double m)
         {
-            val = 0 ;
-            je1 = 0;
-            sum = 0;
-            display(val);
-
+            if (num == "0")
+            {
+                num = null;
+            }
+            num += m.ToString();
+            display(num);
         }
 
-        private void btnMultiply_Click(object sender, EventArgs e)
-        {
-            P = 3;
-          
-            je1 = val;
-            val = 0;
+            private void check(string num, int i = 0)
+            {
+            if (i == 0)
+            {
+                if (num == null)
+                {
+                    this.num = "0"; }
+            }
+            if (i == 1)
+            {
+                string ans;
+                while ((sum.ToString()).Length > 9)
+                {
+                    ans = (sum.ToString()).Substring(0, (sum.ToString()).Length - 1);
+                    sum = double.Parse(ans);
+                }
+            }
+
+             }
+
+            private void calcu(char oprt, double val, double before)
+            {
+            switch (oprt)
+            {
+                case '+':
+                    sum = val + before;
+                    display(sum.ToString());
+                    break;
+
+                case '-':
+                    sum = val - before;
+                    display(sum.ToString());
+                    break;
+
+                case '*':
+                    sum = val * before;
+                    display(sum.ToString());
+                    break;
+
+                case '/':
+                    sum = val / before;
+                    check(sum.ToString(), 1);
+                    display(sum.ToString());
+                    break;
+
+                case '%':
+                    sum = val / 100;
+                    check(sum.ToString(), 1);
+                    display(sum.ToString());
+                    break;
+            }
+            private void btn1_Click(object sender, EventArgs e)
+            {
+                numclick(1);
+            }
+
+            private void btn2_Click(object sender, EventArgs e)
+            {
+                numclick(2);
+            }
+
+            private void btn3_Click(object sender, EventArgs e)
+            {
+                numclick(3);
+            }
+
+            private void btn4_Click(object sender, EventArgs e)
+            {
+                numclick(4);
+            }
+
+            private void btn5_Click(object sender, EventArgs e)
+            {
+                numclick(5);
+            }
+
+            private void btn6_Click(object sender, EventArgs e)
+            {
+                numclick(6);
+            }
+
+            private void btn7_Click(object sender, EventArgs e)
+            {
+                numclick(7);
+            }
+            private void btn8_Click(object sender, EventArgs e)
+            {
+                numclick(8);
+            }
+
+            private void btn9_Click(object sender, EventArgs e)
+            {
+                numclick(9);
+            }
+            private void btn0_Click(object sender, EventArgs e)
+            {
+                numclick(0);
+            }
+
+            private void lblDisplay_Click(object sender, EventArgs e)
+            {
+
+
+            }
+            private void btnSign_Click(object sender, EventArgs e)
+            {
+                display(num, 1);
+            }
+
+
+            private void btnDot_Click(object sender, EventArgs e)
+            {
+                display(num, 2);
+            }
+
+            private void btnClear_Click(object sender, EventArgs e)
+            {
+                num = null;
+                val = 0;
+                sum = 0;
+                Oper = 0;
+                i = 0;
+                display("0");
+
+            }
+
+            private void btnMultiply_Click(object sender, EventArgs e)
+            {
+                check(num);
+                val = double.Parse(num);
+                if (Oper != 3 && Oper != 0)
+                {
+                    if (Oper == 1)
+                    {
+                        calcu('+', sum, val);
+                    }
+                    else if (Oper == 2)
+                    {
+                        calcu('-', sum, val);
+                    }
+                    else if (Oper == 4)
+                    {
+                        calcu('/', sum, val);
+                    }
+                    val = sum;
+                }
+            }else
+                {
+                if (i != 0)
+                {
+                    calcu('*', sum, val);
+                } else
+                {
+                    i = 1;
+                    calcu('*', val, 1);
+                }
+            }
+            Oper = 3;
+            num = null;
         }
 
         private void btnDivide_Click(object sender, EventArgs e)
         {
-            P = 4;
-            je1 = val;
-            val = 0;
+            check(num);
+            val = double.Parse(num);
+            if (Oper != 4 && Oper != 0)
+            {
+                if (Oper == 1)
+                {
+                    calcu('+', sum, val);
+                }
+                else if (Oper == 2)
+                {
+                    calcu('-', sum, val);
+                }
+                else if (Oper == 3)
+                {
+                    calcu('*', sum, val);
+                }
+                val = sum;
+            }
+            else
+            {
+                if (i != 0)
+                {
+                    calcu('/', sum, val);
+                }
+                else
+                {
+                    i = 1;
+                    calcu('/', val, 1);
+                }
+            }
+            Oper = 4;
+            num = null;
         }
-        
+
+
         private void btnPlus_Click(object sender, EventArgs e)
-        {   P = 1;
-            
-                je1 = val;
-                val = 0;
+        {
+            check(num);
+            val = double.Parse(num);
+            if (Oper != 1 && Oper != 0)
+            {
+                if (Oper == 2)
+                {
+                    calcu('-', sum, val);
+                } else if (Oper == 3)
+                {
+                    calcu('*', sum, val);
+                } else if (Oper == 4)
+                {
+                    calcu('/', sum, val);
+                }
+                val = sum;
+            }
+            else
+            {
+                if (i != 0)
+                {
+                    calcu('+', sum, val);
+                }
+                else
+                {
+                    i = 1;
+                    calcu('+', val, 0);
+                }
+            }
+            Oper = 1;
+            num = null;
         }
 
         private void btnMinus_Click(object sender, EventArgs e)
-         {
-            P = 2;
-            
-            je1 = val;
-            val = 0;
-            sum  = je1 - val;
-            display(sum );
-            val = sum ;
-                      //val = je2 ไม่จำเป็น;
-            sum  = 0;
-            val = 0;
-            //je2 -= je1;
-            // lblDisplay.Text = je2.ToString();
+        {
+            check(num);
+            val = double.Parse(num);
+            if (Oper != 2 && Oper != 0)
+            {
+                if (Oper == 1)
+                {
+                    calcu('+', sum, val);
+                }
+                else if (Oper == 3)
+                {
+                    calcu('*', sum, val);
+                }
+                else if (Oper == 4)
+                {
+                    calcu('/', sum, val);
+                }
+                val = sum;
+            }
+            else
+            {
+                if (i != 0)
+                {
+                    calcu('-', sum, val);
+                }
+                else
+                {
+                    i = 1;
+                    calcu('-', val, 0);
+                }
+            }
+            Oper = 2;
+            num = null;
+        }
+        private void btnBack_Click(object sender, EventArgs e)
+        {
 
-         }
-      
+        }
+        private void btnPercent_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
         private void btnEqual_Click(object sender, EventArgs e)
         {
             r = 1;
             switch (P)
             {
                 case 1:
-                    sum  = je1+val;
-                    display(sum );
-                    val =  sum ;
-                    sum  = 0;
+                    sum = je1 + val;
+                    display(sum);
+                    val = sum;
+                    sum = 0;
                     break;
                 case 2:
-                    sum  = je1 - val;
-                    display(sum );
-                    val = sum ;
-                    sum  = 0;
+                    sum = je1 - val;
+                    display(sum);
+                    val = sum;
+                    sum = 0;
                     break;
                 case 3:
-                    sum  = je1 * val;
-                    display(sum );
-                    val = sum ;
-                    sum  = 0;
+                    sum = je1 * val;
+                    display(sum);
+                    val = sum;
+                    sum = 0;
                     break;
                 case 4:
-                    sum  = je1 / val;
-                    display(sum );
-                    val = sum ;
-                    sum  = 0;
+                    sum = je1 / val;
+                    display(sum);
+                    val = sum;
+                    sum = 0;
                     break;
             }
-           
+
 
         }
-            
-            
+
+          
         
-    }
+    
 }
