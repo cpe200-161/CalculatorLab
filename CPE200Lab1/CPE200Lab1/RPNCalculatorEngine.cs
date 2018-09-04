b8c0af2ce6a772d156e17eb0b1863dd6d8126295
PacharaBu <pachara_bu@cm.ac.th>
2018-09-04 18:10:06 +0700

    draft stack_1

--- CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
@@ -8,9 +8,34 @@ namespace CPE200Lab1
 {
     public class RPNCalculatorEngine : CalculatorEngine
     {
+        private bool isNumber(string str)
+        {
+            double retNum;
+            return Double.TryParse(str, out retNum);
+        }
+
+        private bool isOperator(string str)
+        {
+            switch (str)
+            {
+                case "+":
+                case "-":
+                case "X":
+                case "÷":
+                    return true;
+            }
+            return false;
+        }
+
         public string Process(string str)
         {
             // your code here
+            string[] one_ope = str.Split(' ');
+            if (one_ope.Length < 2) return "E";
+            Stack<string> operation = new Stack<string>();
+            int i = 0;
+            while (i < one_ope.Length) {
+            }
            
             
             
