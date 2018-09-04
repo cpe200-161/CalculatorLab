49f925977a78540f6be60dcb3940a3ff9d11af5e
NANTHASAK SAMADSINWANICH <nanthasak_sa@cmu.ac.th>
2018-09-04 17:47:58 +0700

    ใกล้ละอีกนิดนึง

--- CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
@@ -6,12 +6,41 @@ using System.Threading.Tasks;
 
 namespace CPE200Lab1
 {
-    public class RPNCalculatorEngine
+    public class RPNCalculatorEngine : CalculatorEngine 
     {
         public string Process(string str)
         {
-            // your code here
-            return "E";
+            Stack<string> Stacknumber = new Stack<string>();
+            string[] parts = str.Split(' ');
+
+            foreach (string number in parts)
+            {
+                if (isOperator(number))
+                {
+                    string first, second;
+                    first = Stacknumber.Pop();
+                    second = Stacknumber.Pop();
+                    Stacknumber.Push(calculate(number, second, first));
+                }
+                else 
+                {
+                    Stacknumber.Push(number);
+                }
+            }
+            
+            
+                return Stacknumber.Pop();
+            
+            
+            
+                return "E";
+               
+
+            
+            
+               
+            
+            
         }
     }
 }
