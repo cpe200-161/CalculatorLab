43b37023cc0e45a876fc699e621ef4bbb03da0df
APHISIT KHAMNOI <aphisit_khamnoi@cmu.ac.th>
2018-09-04 14:59:48 +0700

    remove commend

--- CPE200Lab1/CPE200Lab1/MainForm.cs
+++ CPE200Lab1/CPE200Lab1/MainForm.cs
@@ -20,7 +20,6 @@ namespace CPE200Lab1
 		private string secondOperand;
 		private string operate;
 		private string operate0;
-		//private decimal EndResult;
 		private string MemoryStore = "0";
 		public CalculatorEngine engine;
 
@@ -32,45 +31,6 @@ namespace CPE200Lab1
             isAfterOperater = false;
             isAfterEqual = false;
         }
-
-        /*private string calculate(string operate, string firstOperand, string secondOperand, int maxOutputSize = 8)
-        {
-            switch(operate)
-            {
-                case "+":
-                    return (Convert.ToDouble(firstOperand) + Convert.ToDouble(secondOperand)).ToString();
-                case "-":
-                    return (Convert.ToDouble(firstOperand) - Convert.ToDouble(secondOperand)).ToString();
-                case "X":
-                    return (Convert.ToDouble(firstOperand) * Convert.ToDouble(secondOperand)).ToString();
-                case "÷":
-                    // Not allow devide be zero
-                    if(secondOperand != "0")
-                    {
-                        double result;
-                        string[] parts;
-                        int remainLength;
-
-                        result = (Convert.ToDouble(firstOperand) / Convert.ToDouble(secondOperand));
-                        // split between integer part and fractional part
-                        parts = result.ToString().Split('.');
-                        // if integer part length is already break max output, return error
-                        if(parts[0].Length > maxOutputSize)
-                        {
-                            return "E";
-                        }
-                        // calculate remaining space for fractional part.
-                        remainLength = maxOutputSize - parts[0].Length - 1;
-                        // trim the fractional part gracefully. =
-                        return result.ToString("N" + remainLength);
-                    }
-                    break;
-                case "%":
-                    //your code here
-                    break;
-            }
-            return "E";
-        }*/
 
         public MainForm()
         {
@@ -136,13 +96,10 @@ namespace CPE200Lab1
                 case "%":
 					secondOperand = lblDisplay.Text;
 					lblDisplay.Text = engine.calculate(operate, firstOperand, secondOperand);
-					//secondOperand = lblDisplay.Text;
-					//isAfterOperater = true;
 					break;
 				case "√":
 					firstOperand = lblDisplay.Text;
 					engine.calculate(operate, firstOperand, secondOperand);
-					//firstOperand = lblDisplay.Text;
 					break;
 				case "1/X":
 					firstOperand = lblDisplay.Text;
