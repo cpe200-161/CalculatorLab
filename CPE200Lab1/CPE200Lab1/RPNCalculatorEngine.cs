3aa28aee9c29f0f6920b7f2bd19ed03e59ee0a9e
natchap0n <natchap0n@users.noreply.github.com>
2018-09-04 17:09:04 +0700

    1.Start RPN code.

---
+++ CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPE200Lab1
+{
+    public class RPNCalculatorEngine : CalculatorEngine
+    {
+        public string process(string str)
+        {
+            //your code here
+            Stack<string> number = new Stack<string>();
+            string[] parts = str.Split(' ');
+            int i = 0;
+            if (isNumber(parts[i]) == true)
+            {
+                number.Push("parts[i]");
+            }
+            else if(isOperator(parts[i]) == true)
+            {
+                if(number.Count < 2)
+                {
+                    return "E";
+                }
+                else
+                {
+
+                }
+            }
+            
+            return "E";
+        }
+    }
+}
