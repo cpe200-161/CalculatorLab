c54ca58444f145eea0882de3f89b51fcbcad0faf
TANCHAPAS CHANTARANIMI <tanchapas_ch@cmu.ac.th>
2018-09-04 13:57:54 +0700

    Bugs fixed.

--- CPE200Lab1/CPE200Lab1/CalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/CalculatorEngine.cs
@@ -60,6 +60,7 @@ namespace CPE200Lab1
                         {
                             return (Convert.ToDouble(firstOperand) / (Convert.ToDouble(secondOperand) / 100)).ToString();
                         }
+                        return (Convert.ToDouble(secondOperand) / 100).ToString();
                     }
                     break;
                 case "√":
