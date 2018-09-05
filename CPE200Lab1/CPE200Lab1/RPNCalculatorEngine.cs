0ff2d815a698262a506f2c56724c45332b24f591
natchap0n <natchap0n@users.noreply.github.com>
2018-09-05 16:40:07 +0700

    Finally pass all test. Hooray.

--- CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
@@ -8,29 +8,41 @@ namespace CPE200Lab1
 {
     public class RPNCalculatorEngine : CalculatorEngine
     {
-        public string process(string str)
+        public string Process(string str)
         {
             //your code here
             Stack<string> number = new Stack<string>();
             string[] parts = str.Split(' ');
-            int i = 0;
-            if (isNumber(parts[i]) == true)
+            for(int i=0; i<parts.Length; i++)
             {
-                number.Push("parts[i]");
+                if (isNumber(parts[i]) == true)
+                {
+                    number.Push(parts[i]);
+                }
+                else if (isOperator(parts[i]) == true)
+                {
+                    if (number.Count < 2)
+                    {
+                        return "E";
+                    }
+                    else
+                    {
+                        string secondOperand = number.Pop();
+                        string firstOperand = number.Pop();
+                        string result = calculate(parts[i], firstOperand, secondOperand);
+                        number.Push(result);
+                    }
+                }
             }
-            else if(isOperator(parts[i]) == true)
+            if(number.Count == 1)
             {
-                if(number.Count < 2)
-                {
-                    return "E";
-                }
-                else
-                {
-
-                }
+                return number.Pop();
+            }
+            else
+            {
+                return "E";
             }
             
-            return "E";
         }
     }
 }
