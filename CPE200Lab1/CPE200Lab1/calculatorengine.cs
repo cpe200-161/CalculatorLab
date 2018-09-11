83190e4ccccb49dd1fa018710cdcf7ecdbca3106
HeyzzWatcharin <35345564+HeyzzWatcharin@users.noreply.github.com>
2018-09-11 13:49:35 +0700

    test1

--- CPE200Lab1/CPE200Lab1/calculatorengine.cs
+++ CPE200Lab1/CPE200Lab1/calculatorengine.cs
@@ -49,6 +49,7 @@ namespace CPE200Lab1
         }
         public void resetAll()
         {
+            display = "0";
             isAllowBack = true;
             hasDot = false;
             isAfterOperater = false;
