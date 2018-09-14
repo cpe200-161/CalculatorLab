a8f8515dbabf063fd581fa2b9b650b86c50b5cb7
HeyzzWatcharin <35345564+HeyzzWatcharin@users.noreply.github.com>
2018-09-14 17:06:39 +0700

    test2

--- CPE200Lab1/CPE200Lab1/RPNCALCULATORENGINE.cs
+++ CPE200Lab1/CPE200Lab1/RPNCALCULATORENGINE.cs
@@ -6,57 +6,12 @@ using System.Threading.Tasks;
 
 namespace CPE200Lab1
 {
-    class RPNCALCULATORENGINE : CalculatorEngine
+    public class RPNCalculatorEngine
     {
-
-   
-
-        public override string Process(string str)
+        public string Process(string str)
         {
-            string[] parts = str.Split(' ');
-            Stack<string> cha = new Stack<string>();
-            string s1, s2,s3;
-            bool checkemty = false;
-            for(int i=0; i < parts.Length; i++)
-            {
-                if (isNumber(parts[i]))
-                {
-                    cha.Push(parts[i]);
-                }
-                
-                else if(isOperator(parts[i]))
-                {
-                      if (cha.Count() < 2)
-                    {
-                        return "E";
-                    }
-                        s2 = cha.Pop();
-                    s1 = cha.Pop();
-                    s3 = calculate(parts[i], s1, s2, 4);
-                    cha.Push(s3);
-                    //return cha.Peek();
-                    
-                }
-               
-            }
-            if (cha.Count() != 0)
-            {
-                return "E";
-            }
-            /*if (!checkemty)
-            {
-                return "E";
-            }*/
-            else
-            {
-                cha.Pop();
-                return cha.Peek();
-            }
+            // your code here
+            return "E";
         }
-
     }
-          
 }
-
-
-
