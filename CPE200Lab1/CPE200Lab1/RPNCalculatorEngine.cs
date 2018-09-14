1113129419f8750c417b1cd2dd0954089a0fc3cf
teerapatpor <35361062+teerapatpor@users.noreply.github.com>
2018-09-14 17:16:55 +0700

    complete RPNCalculatorEngine

--- CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +7,32 @@ using System.Threading.Tasks;
 
 namespace CPE200Lab1
 {
-    public class RPNCalculatorEngine
+    public class RPNCalculatorEngine:CalculatorEngine
     {
         public string Process(string str)
         {
-            // your code here
+            Stack mystack = new Stack();
+            string first, second;
+            string[] parts = str.Split(' ');
+            Console.WriteLine(parts);
+                    
+            for(int i = 0; i < parts.Length; i++)
+            {
+                if (isOperator(parts[i]))
+                {
+                    if (mystack.Count < 2) return "E";
+                }
+
+                if(isNumber(parts[i])) mystack.Push(parts[i]);
+                else if(isOperator(parts[i]))
+                {
+                    first = (string)mystack.Pop();
+                    second = (string)mystack.Pop();
+                    mystack.Push(calculate(parts[i], second, first, 4));
+                }
+            }           
+            if(mystack.Count==1) return (string)mystack.Pop();
+
             return "E";
         }
     }
