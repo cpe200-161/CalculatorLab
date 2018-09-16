4b8963df8f5dd3f4fe478226de67646ea87a95b5
kiujkio <35343051+kiujkio@users.noreply.github.com>
2018-09-16 12:59:01 +0700

    lab

--- CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,8 +11,37 @@ namespace CPE200Lab1
     {
         public string Process(string str)
         {
-            // your code here
-            return "E";
+            string one,two,sum;
+            Stack myStack = new Stack();
+            string[] parts = str.Split(' ');
+
+            for (int i = 0; i < parts.Length; i++)
+            {
+                if (isNumber(parts[i]))
+                {
+                    myStack.Push(parts[i]);
+                }
+                else if (isOperator(parts[i]) && myStack.Count >= 2)
+                {
+                    two = myStack.Pop().ToString();
+                    one = myStack.Pop().ToString();
+                    sum = calculate(parts[i], one, two);
+                    myStack.Push(sum);
+                }
+                else
+                {
+                    return "E";
+                }
+            }
+            if (myStack.Count==1)
+            {   
+                return myStack.Pop().ToString();
+            }
+            else
+            {
+               return "E";
+            }
+          
         }
     }
 }
