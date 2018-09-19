6108f1b8c6c50d373a453dec9849607cf6ec8ea6
Hyze <pitchayut.jjua@gmail.com>
2018-09-19 14:56:11 +0700

    unary added

--- CPE200Lab1/CPE200Lab1/ExtendForm.cs
+++ CPE200Lab1/CPE200Lab1/ExtendForm.cs
@@ -15,12 +15,12 @@ namespace CPE200Lab1
         private bool isNumberPart = false;
         private bool isContainDot = false;
         private bool isSpaceAllowed = false;
-        private CalculatorEngine engine;
+        private RPNCalculatorEngine engine;
 
         public ExtendForm()
         {
             InitializeComponent();
-            engine = new CalculatorEngine();
+            engine = new RPNCalculatorEngine();
         }
 
         private bool isOperator(char ch)
@@ -30,6 +30,17 @@ namespace CPE200Lab1
                 case '-':
                 case 'X':
                 case '÷':
+                    return true;
+            }
+            return false;
+        }
+
+        private bool isUnary(string ch)
+        {
+            switch (ch)
+            {
+                case "1/x":
+                case "√":
                     return true;
             }
             return false;
@@ -52,6 +63,22 @@ namespace CPE200Lab1
             }
             lblDisplay.Text += ((Button)sender).Text;
             isSpaceAllowed = true;
+        }
+
+        private void btnUnaryOperator_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text is "Error")
+            {
+                return;
+            }
+            isNumberPart = false;
+            isContainDot = false;
+            string current = lblDisplay.Text;
+            if (current[current.Length - 1] != ' ' || isUnary((current[current.Length - 2]).ToString()))
+            {
+                lblDisplay.Text += " " + ((Button)sender).Text + " ";
+                isSpaceAllowed = false;
+            }
         }
 
         private void btnBinaryOperator_Click(object sender, EventArgs e)
--- CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
@@ -1,16 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CPE200Lab1
 {
-    public class RPNCalculatorEngine
+    public class RPNCalculatorEngine : CalculatorEngine
     {
         public string Process(string str)
         {
             // your code here
+            Console.WriteLine("TEST");
+            Stack RPNStack = new Stack();
+            string firstOP, secondOP;
+            string[] splitStr = str.Split(' ');       
+
+            foreach (string s in splitStr)
+            {
+                Console.WriteLine(s);
+                if (isNumber(s))
+                {
+                    RPNStack.Push(s);
+                }
+                else if (isOperator(s) || isUnary(s))
+                {
+                    if (RPNStack.Count <= 1)
+                    {
+                        return "E";
+                    }
+                    else if (isOperator(s) && !isUnary(s))
+                    {
+                        secondOP = RPNStack.Pop().ToString();
+                        firstOP = RPNStack.Pop().ToString();
+                        RPNStack.Push(calculate(s, firstOP, secondOP));
+                    }
+                    else
+                    {                
+                        firstOP = RPNStack.Pop().ToString();
+                        RPNStack.Push(unaryCalculate(s, firstOP));
+                    }
+                }
+                else
+                {
+                    return "E";
+                }
+            }
+            if (RPNStack.Count == 1)
+            {
+                if (splitStr.Length == 1)
+                {
+                    return "E";
+                }
+                return (Convert.ToDecimal(RPNStack.Peek().ToString())).ToString("G29");
+            }
             return "E";
         }
     }
