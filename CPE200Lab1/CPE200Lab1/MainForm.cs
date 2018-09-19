866d4378fc5789d438b3aa20a6b73ca5acf99874
Pitipat <frongtp007@hotmail.com>
2018-09-19 15:55:16 +0700

    fix bugs ,add 1/x,root,%

--- CPE200Lab1/CPE200Lab1/MainForm.cs
+++ CPE200Lab1/CPE200Lab1/MainForm.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Collections;
 namespace CPE200Lab1
 {
     public partial class MainForm : Form
@@ -31,11 +31,20 @@ namespace CPE200Lab1
             firstOperand = null;
         }
 
-      
+
 
         public MainForm()
         {
             InitializeComponent();
+            string teststring = "4 8 -";
+            string[] teststringArr = teststring.Split(' ');
+            Stack testStack = new Stack();
+
+            foreach (string s in teststringArr)
+            {
+                Console.WriteLine(s);
+            }
+
             memory = 0;
             engine = new CalculatorEngine();
             resetAll();
@@ -55,13 +64,13 @@ namespace CPE200Lab1
             {
                 lblDisplay.Text = "0";
             }
-            if(lblDisplay.Text.Length is 8)
+            if (lblDisplay.Text.Length is 8)
             {
                 return;
             }
             isAllowBack = true;
             string digit = ((Button)sender).Text;
-            if(lblDisplay.Text is "0")
+            if (lblDisplay.Text is "0")
             {
                 lblDisplay.Text = "";
             }
@@ -103,7 +112,7 @@ namespace CPE200Lab1
             {
                 return;
             }
-            if(firstOperand != null)
+            if (firstOperand != null)
             {
                 string secondOperand = lblDisplay.Text;
                 string result = engine.calculate(operate, firstOperand, secondOperand);
@@ -188,10 +197,11 @@ namespace CPE200Lab1
             {
                 return;
             }
-            if(lblDisplay.Text[0] is '-')
+            if (lblDisplay.Text[0] is '-')
             {
                 lblDisplay.Text = lblDisplay.Text.Substring(1, lblDisplay.Text.Length - 1);
-            } else
+            }
+            else
             {
                 lblDisplay.Text = "-" + lblDisplay.Text;
             }
@@ -216,16 +226,16 @@ namespace CPE200Lab1
             {
                 return;
             }
-            if(lblDisplay.Text != "0")
+            if (lblDisplay.Text != "0")
             {
                 string current = lblDisplay.Text;
                 char rightMost = current[current.Length - 1];
-                if(rightMost is '.')
+                if (rightMost is '.')
                 {
                     hasDot = false;
                 }
                 lblDisplay.Text = current.Substring(0, current.Length - 1);
-                if(lblDisplay.Text is "" || lblDisplay.Text is "-")
+                if (lblDisplay.Text is "" || lblDisplay.Text is "-")
                 {
                     lblDisplay.Text = "0";
                 }
@@ -234,7 +244,7 @@ namespace CPE200Lab1
 
         private void btnMP_Click(object sender, EventArgs e)
         {
-            if(lblDisplay.Text is "Error")
+            if (lblDisplay.Text is "Error")
             {
                 return;
             }
@@ -249,7 +259,7 @@ namespace CPE200Lab1
 
         private void btnMM_Click(object sender, EventArgs e)
         {
-            if(lblDisplay.Text is "Error")
+            if (lblDisplay.Text is "Error")
             {
                 return;
             }
@@ -259,7 +269,7 @@ namespace CPE200Lab1
 
         private void btnMR_Click(object sender, EventArgs e)
         {
-            if(lblDisplay.Text is "error")
+            if (lblDisplay.Text is "error")
             {
                 return;
             }
--- CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +7,64 @@ using System.Threading.Tasks;
 
 namespace CPE200Lab1
 {
-    public class RPNCalculatorEngine
+    public class RPNCalculatorEngine : CalculatorEngine
     {
         public string Process(string str)
         {
-            // your code here
-            return "E";
+            Console.WriteLine("i am here");
+            string firstOp, secondOp;
+            string[] strArray = str.Split(' ');
+            if (strArray.Length < 3)
+            {
+                return "E";
+            }
+            Stack rpnStack = new Stack();
+
+            foreach (string s in strArray)
+            {
+                Console.WriteLine(s);
+                if (isNumber(s))
+                {
+                    rpnStack.Push(s);
+                }
+                else if (isOperator(s))
+                {
+                    if (rpnStack.Count >= 2)
+                    {
+                        secondOp = rpnStack.Pop().ToString();
+                        firstOp = rpnStack.Pop().ToString();
+
+                        rpnStack.Push(calculate(s, firstOp, secondOp));
+                    }
+                    else
+                    {
+                        return "E";
+                    }
+                }
+            }
+       /*     if (rpnStack.Count == 1)
+            {
+                return string.Format("{0:G29}",decimal.Parse(rpnStack.Peek().ToString()));
+            }
+            else
+            {
+                return "E";
+            }*/
+            if (rpnStack.Count == 1)
+            {
+                if (strArray[1] == "√" || strArray[1] == "1/x")
+                {
+                    string firstnumber;
+                    firstnumber = rpnStack.Peek().ToString();
+                    rpnStack.Pop();
+                    rpnStack.Push(unaryCalculate(strArray[1], firstnumber));
+                }
+                return rpnStack.Peek().ToString();
+            }
+            else
+            {
+                return "E";
+            }
         }
     }
 }
