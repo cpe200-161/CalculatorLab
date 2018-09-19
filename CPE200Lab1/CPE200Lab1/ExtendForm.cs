b0436c3a358ec82599c62ef81ce54a2fe7ec8ab6
isne529 <tong11_tong@hotmail.com>
2018-09-19 23:36:53 +0700

    finish already

--- CPE200Lab1/CPE200Lab1/ExtendForm.cs
+++ CPE200Lab1/CPE200Lab1/ExtendForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,21 +16,34 @@ namespace CPE200Lab1
         private bool isNumberPart = false;
         private bool isContainDot = false;
         private bool isSpaceAllowed = false;
+        private bool isAfterOperater;
+        private string firstOperand;
+        private string operate;
+        private double memory;
         private RPNCalculatorEngine engine;
 
+        private void resetAll()
+        {
+            lblDisplay.Text = "0";
+            isAfterOperater = false;
+        }
         public ExtendForm()
         {
             InitializeComponent();
+            memory = 0;
             engine = new RPNCalculatorEngine();
+            resetAll();
         }
 
         private bool isOperator(char ch)
         {
-            switch(ch) {
+            switch (ch)
+            {
                 case '+':
                 case '-':
                 case 'X':
                 case '÷':
+                case '%':
                     return true;
             }
             return false;
@@ -70,6 +84,30 @@ namespace CPE200Lab1
             }
         }
 
+        private void btnUnaryOperator_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text is "Error")
+            {
+                return;
+            }
+            if (isAfterOperater)
+            {
+                return;
+            }
+            operate = ((Button)sender).Text;
+            firstOperand = lblDisplay.Text;
+            string result = engine.unaryCalculate(operate, firstOperand);
+            if (result is "E" || result.Length > 8)
+            {
+                lblDisplay.Text = "Error";
+            }
+            else
+            {
+                lblDisplay.Text = result;
+            }
+
+        }
+
         private void btnBack_Click(object sender, EventArgs e)
         {
             if (lblDisplay.Text is "Error")
@@ -81,7 +119,8 @@ namespace CPE200Lab1
             if (current[current.Length - 1] is ' ' && current.Length > 2 && isOperator(current[current.Length - 2]))
             {
                 lblDisplay.Text = current.Substring(0, current.Length - 3);
-            } else
+            }
+            else
             {
                 lblDisplay.Text = current.Substring(0, current.Length - 1);
             }
@@ -105,12 +144,10 @@ namespace CPE200Lab1
             if (result is "E")
             {
                 lblDisplay.Text = "Error";
-            } else
+            }
+            else
             {
                 lblDisplay.Text = result;
-                isSpaceAllowed = true;
-                isContainDot = false;
-                isNumberPart = true;
             }
         }
 
@@ -128,14 +165,16 @@ namespace CPE200Lab1
             if (current is "0")
             {
                 lblDisplay.Text = "-";
-            } else if (current[current.Length - 1] is '-')
+            }
+            else if (current[current.Length - 1] is '-')
             {
                 lblDisplay.Text = current.Substring(0, current.Length - 1);
                 if (lblDisplay.Text is "")
                 {
                     lblDisplay.Text = "0";
                 }
-            } else
+            }
+            else
             {
                 lblDisplay.Text = current + "-";
             }
@@ -148,7 +187,7 @@ namespace CPE200Lab1
             {
                 return;
             }
-            if(!isContainDot)
+            if (!isContainDot)
             {
                 isContainDot = true;
                 lblDisplay.Text += ".";
@@ -158,15 +197,58 @@ namespace CPE200Lab1
 
         private void btnSpace_Click(object sender, EventArgs e)
         {
-            if(lblDisplay.Text is "Error")
+            if (lblDisplay.Text is "Error")
             {
                 return;
             }
-            if(isSpaceAllowed)
+            if (isSpaceAllowed)
             {
                 lblDisplay.Text += " ";
                 isSpaceAllowed = false;
             }
         }
+        private void btnMP_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text is "Error")
+            {
+                return;
+            }
+            memory += Convert.ToDouble(lblDisplay.Text);
+            isAfterOperater = true;
+        }
+
+        private void btnMC_Click(object sender, EventArgs e)
+        {
+            memory = 0;
+        }
+
+        private void btnMM_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text is "Error")
+            {
+                return;
+            }
+            memory -= Convert.ToDouble(lblDisplay.Text);
+            isAfterOperater = true;
+        }
+
+        private void btnMR_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text is "error")
+            {
+                return;
+            }
+            lblDisplay.Text = memory.ToString();
+        }
+
+        private void btnMS_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text is "error")
+            {
+                return;
+            }
+            memory = Convert.ToDouble(lblDisplay.Text);
+        }
+
     }
 }
--- CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
@@ -1,42 +1,133 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
 namespace CPE200Lab1
 {
     public class RPNCalculatorEngine : CalculatorEngine
     {
-        public new string Process(string str)
-        {
-            Stack<string> rpnStack = new Stack<string>();
-            List<string> parts = str.Split(' ').ToList<string>();
-            string result;
-            string firstOperand, secondOperand;
 
-            foreach (string token in parts)
+        public string Process(string str)
+        {
+
+            string firstOp, secondOp;
+            string[] strArr = str.Split(' ');
+            int checkNum = 0;
+            int checkOp = 0;
+            Stack rpnStack = new Stack();
+
+            if (strArr.Length < 3)
             {
-                if (isNumber(token))
+                return "E";
+            }
+            else if (isNumber(strArr[0]) && strArr[1] == "√")
+            {
+
+                return unaryCalculate("√", strArr[0]);
+            }
+            else if (isNumber(strArr[0]) && strArr[1] == "1/x")
+            {
+
+                return unaryCalculate("1/x", strArr[0]);
+            }
+            else if (isOperator(strArr[1]) || isOperator(strArr[0]) || strArr[1] == "%")
+
+            {
+                return "E";
+            }
+            else
+            {
+                int i;
+                for (i = 0; i < strArr.Length; i++)
                 {
-                    rpnStack.Push(token);
-                }
-                else if (isOperator(token))
-                {
-                    //FIXME, what if there is only one left in stack?
-                    secondOperand = rpnStack.Pop();
-                    firstOperand = rpnStack.Pop();
-                    result = calculate(token, firstOperand, secondOperand, 4);
-                    if (result is "E")
+                    if (isNumber(strArr[i]))
                     {
-                        return result;
+                        checkNum++;
                     }
-                    rpnStack.Push(result);
+                    else
+                    {
+                        break;
+                    }
+                }
+                for (int x = i; i < strArr.Length; i++)
+                {
+                    if (isOperator(strArr[i]))
+                    {
+                        checkOp++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                if (checkOp == checkNum || checkNum < checkOp)
+                {
+                    return "E";
+                }
+                else
+                {
+                    foreach (string s in strArr)
+                    {
+
+                        if (isNumber(s))
+                        {
+                            rpnStack.Push(s);
+                        }
+                        else
+                        {
+                            if (s == "%")
+                            {
+                                if (rpnStack.Count > 1)
+                                {
+                                    secondOp = rpnStack.Pop().ToString();
+                                    firstOp = rpnStack.Pop().ToString();
+                                    if (firstOp == null || secondOp == null)
+                                    {
+                                        return "E";
+                                    }
+                                    secondOp = ((Convert.ToDouble(firstOp) * Convert.ToDouble(secondOp)) / 100).ToString();
+                                    rpnStack.Push(firstOp);
+                                    rpnStack.Push(secondOp);
+                                }
+                                else
+                                {
+                                    return "E";
+                                }
+                            }
+                            if (isOperator(s) && s != "%")
+                            {
+                                if (rpnStack.Count > 1)
+                                {
+                                    secondOp = rpnStack.Pop().ToString();
+                                    firstOp = rpnStack.Pop().ToString();
+                                    if (firstOp == null || secondOp == null)
+                                    {
+                                        return "E";
+                                    }
+                                    rpnStack.Push(calculate(s, firstOp, secondOp));
+                                }
+                                else
+                                {
+                                    return "E";
+                                }
+                            }
+                        }
+                    }
                 }
             }
-            //FIXME, what if there is more than one, or zero, items in the stack?
-            result = rpnStack.Pop();
-            return result;
+            if (rpnStack.Count == 1)
+            {
+                return decimal.Parse(rpnStack.Peek().ToString()).ToString("G29");
+            }
+            else
+            {
+                return "E";
+            }
         }
     }
 }
