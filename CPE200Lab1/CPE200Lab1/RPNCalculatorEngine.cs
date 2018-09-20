fbe22b96a0db1055f418befcab8fd0019d8b117c
APISARA TUNGPENNUNG <apisara_tungpennung@cmu.ac.th>
2018-09-20 23:03:59 +0700

    เย้ เสร็จแล้ว

--- CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
@@ -10,10 +10,51 @@ namespace CPE200Lab1
     {
         public string Process(string str)
         {
-            string firstOperand;
-            string secondOprand;
-            // your code here
-            return "E";
+            string firstnum;
+            string secondnum;
+            string[] parts = str.Split(' ');
+            Stack<string> number = new Stack<string>();
+
+            for (int i = 0; i < parts.Length; i++)
+            {
+                if (isOperator(parts[i]))
+                {
+                    if (number.Count < 2)
+                    {
+                        if (parts[i] == "1/x" || parts[i] == "√")
+                        {
+                            firstnum = number.Pop();
+                            number.Push(unaryCalculate(parts[i], firstnum));
+                        }
+                        else return "E";
+                    }
+                    else if (parts[i] == "1/x" || parts[i] == "√")
+                    {
+                        firstnum = number.Pop();
+                        number.Push(unaryCalculate(parts[i], firstnum));
+                    }
+                    else if (parts[i] == "%")
+                    {
+                        secondnum = number.Pop();
+                        firstnum = number.Pop();
+                        number.Push(firstnum);
+                        number.Push(calculate(parts[i], firstnum, secondnum));
+                    }
+                    else
+                    {
+                        secondnum = number.Pop();
+                        firstnum = number.Pop();
+                        number.Push(calculate(parts[i], firstnum, secondnum));
+                    }
+                    
+                }
+                else if (isNumber(parts[i]))
+                {
+                    number.Push(parts[i]);
+                }
+            }
+            if (number.Count == 1) return number.Pop();
+            else return "E";
         }
     }
 }
