6b022cd3ef050864d953197f24e7dbcf5287bd81
600610721 <35341243+600610721@users.noreply.github.com>
2018-09-20 20:16:21 +0700

    complete homework

--- CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
@@ -6,12 +6,45 @@ using System.Threading.Tasks;
 
 namespace CPE200Lab1
 {
-    public class RPNCalculatorEngine
+    public class RPNCalculatorEngine : CalculatorEngine
     {
         public string Process(string str)
         {
-            // your code here
-            return "E";
+            string first = null;
+            string second = null;
+            string[] parts = str.Split(' ');
+            Stack<string> cal = new Stack<string>();
+            for(int i = 0; i < parts.Length; i++)
+            {
+                if(isOperator(parts[i]))
+                {
+                    if (cal.Count == 0 || cal.Count == 1)
+                    {
+                        if (parts[i] == "√" || parts[i] == "1/x")
+                        {
+                            first = cal.Pop();
+                            cal.Push(unaryCalculate(parts[i], first));
+                        }
+                        else return "E";
+                    }
+                    else if (parts[i] == "%")
+                    {
+                        second = cal.Pop();
+                        first = cal.Pop();
+                        cal.Push(first);
+                        cal.Push(calculate(parts[i], first, second));
+                    }
+                    else
+                    {
+                        second = cal.Pop();
+                        first = cal.Pop();
+                        cal.Push(calculate(parts[i], first, second));
+                    }
+                }
+                else if(isNumber(parts[i])) cal.Push(parts[i]);
+            }
+            if(cal.Count == 1) return cal.Pop();
+            else return "E";
         }
     }
 }
