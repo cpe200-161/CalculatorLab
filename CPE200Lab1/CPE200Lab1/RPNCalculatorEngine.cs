aa8f5aefb5604bbd74c2ce7d5c7366c63585d352
RAKSAMON KUMPAPHAN <raksamon_k@cmu.ac.th>
2018-09-21 01:46:57 +0700

    All Green But Not Complete

--- CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
@@ -11,7 +11,32 @@ namespace CPE200Lab1
         public string Process(string str)
         {
             // your code here
+            Stack<string> numStack = new Stack<string>();
+            string first, secound;
+            string[] parts = str.Split(' ');
+            Console.WriteLine(parts);
 
+            for(int i = 0; i < parts.Length; i++)
+            {
+                if (isOperator(parts[i]))
+                {
+                    if (numStack.Count < 2) return "E";
+                }
+                if (isNumber(parts[i]))
+                {
+                    numStack.Push(parts[i]);
+                }
+                else if (isOperator(parts[i]))
+                {
+                    first = (string)numStack.Pop();
+                    secound = (string)numStack.Pop();
+                    numStack.Push(calculate(parts[i], secound, first, 4));
+                }
+
+            }
+            if (numStack.Count == 1) {
+                return (string)numStack.Pop();
+            }
             return "E";
         }
     }
