876a47a8975a2d19338bcf55820e3ea1614beab0
teamhd35975 <teamhd35975@gmail.com>
2018-09-21 15:49:03 +0700

    teeradon

--- CPE200Lab1/CPE200Lab1/Program.cs
+++ CPE200Lab1/CPE200Lab1/Program.cs
@@ -8,9 +8,7 @@ namespace CPE200Lab1
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+       
         [STAThread]
         static void Main()
         {
