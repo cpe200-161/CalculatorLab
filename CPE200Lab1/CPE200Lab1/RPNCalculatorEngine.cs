3d8b7acf693a9159d6625630692738fe732e2d8e
forddbk <forddbkzero@hotmail.com>
2018-09-21 16:25:15 +0700

    sdsadad

---
+++ CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
@@ -0,0 +1,60 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPE200Lab1
+{
+
+
+    public class RPNCalculatorEngine : CalculatorEngine
+    {
+
+
+         public override string Process(string str)
+        {
+            if (isSpaceAllowed) return str;
+            string[] parts = str.Split(' ');
+            Stack<string> operands = new Stack<string>();
+            string result = "0";
+            for (int i = 0; i < parts.Length; i++)
+            {
+
+
+
+                if (isNumber(parts[i]))
+                {
+                    operands.Push(parts[i]);
+                }
+                else if (isOperator(parts[i]))
+                {
+                    string ppap1;
+                    string ppap2;
+                    if (operands.Count() > 1)
+                    {
+                        ppap2 = operands.Pop();
+                        ppap1 = operands.Pop();
+                    }
+                    else
+                    {
+                        return "E";
+                        
+                       
+                    }
+                    result = calculate(parts[i], ppap1, ppap2, 4);
+                    operands.Push(result);
+
+                }
+
+
+
+            }
+
+            return result;
+     
+
+        }
+    }
+}
