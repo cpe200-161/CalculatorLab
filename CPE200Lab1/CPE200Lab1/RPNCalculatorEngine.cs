c5cd7e0895cfe43afc9d4d028ad3db7e77c2de0b
Hyze <pitchayut.jjua@gmail.com>
2018-09-21 14:02:58 +0700

    update lab 3

--- CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
@@ -10,6 +10,11 @@ namespace CPE200Lab1
     {
         public new string Process(string str)
         {
+            if (str is null)
+            {
+                return "E";
+            }
+
             Stack<string> rpnStack = new Stack<string>();
             List<string> parts = str.Split(' ').ToList<string>();
             string result;
@@ -19,24 +24,52 @@ namespace CPE200Lab1
             {
                 if (isNumber(token))
                 {
+                    //if (token.Contains("+-X÷") && token.Contains("1234567890"))
+                    //{
+                    //    return "E";
+                    //}
+                    //else
                     rpnStack.Push(token);
+                    
                 }
                 else if (isOperator(token))
                 {
-                    //FIXME, what if there is only one left in stack?
-                    secondOperand = rpnStack.Pop();
-                    firstOperand = rpnStack.Pop();
-                    result = calculate(token, firstOperand, secondOperand, 4);
-                    if (result is "E")
+                    if (rpnStack.Count() <= 1)
                     {
-                        return result;
+                        return "E";
                     }
-                    rpnStack.Push(result);
+                    else
+                    //FIXME, what if there is only one left in stack?
+                    {
+                        secondOperand = rpnStack.Pop();
+                        firstOperand = rpnStack.Pop();
+                        result = calculate(token, firstOperand, secondOperand, 8);
+                        if (result is "E")
+                        {
+                            return result;
+                        }
+                        rpnStack.Push(result);
+                    }
+                }
+                else if (token != "")
+                {
+                    return "E";
                 }
             }
+            if (rpnStack.Count > 1 || rpnStack.Count == 0)
+            {
+                return "E";
+            }
+            else
+            {
+                if(Convert.ToDecimal(rpnStack.Peek()).ToString() != rpnStack.Peek())
+                {
+                    return "E";
+                }
+                result = rpnStack.Pop();
+                return Convert.ToDecimal(result).ToString("0.####");
+            }
             //FIXME, what if there is more than one, or zero, items in the stack?
-            result = rpnStack.Pop();
-            return result;
         }
     }
 }
