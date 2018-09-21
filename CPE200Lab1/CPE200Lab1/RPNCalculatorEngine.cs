ef101126500b4b86bf70f71546ee093bb778290e
User <User@DESKTOP-GA1T7RN>
2018-09-21 17:04:29 +0700

    5

--- CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
@@ -6,12 +6,34 @@ using System.Threading.Tasks;
 
 namespace CPE200Lab1
 {
-    public class RPNCalculatorEngine
+    public class RPNCalculatorEngine : CalculatorEngine;
     {
         public string Process(string str)
         {
             // your code here
-            return "E";
+
+            String[] txt = str.Split(' ');
+            String a1, a2;
+            Stack<string> x = new Stack<string>();
+            for (int i = 0; i < txt.Length; i++)
+            {
+                if (isNumber(txt[i]))
+                {
+                    x.Push(txt[i]);
+                }
+                else if (isOperator(txt[i]))
+                {
+                    string sum;
+                    a1 = x.Pop();
+                    a2 = x.Pop();
+                    sum = calculate(txt[i], a2, a1);
+                    x.Push(sum);
+                }
+         
+            }
+            return x.Peek();
+            
+            
         }
     }
 }
