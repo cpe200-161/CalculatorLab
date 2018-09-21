9f5666f046977612fa62fa89374e5432c80ce44e
Kritsana Manupha <guy_lll@hotmail.com>
2018-09-21 14:33:08 +0700

    copy process lab 2 to this

--- CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
@@ -6,12 +6,43 @@ using System.Threading.Tasks;
 
 namespace CPE200Lab1
 {
-    public class RPNCalculatorEngine
+    public class RPNCalculatorEngine : CalculatorEngine;
     {
         public string Process(string str)
         {
             // your code here
-            return "E";
+
+            Stack<string> cct = new Stack<string>();
+            string[] parts = str.Split(' ');
+
+            for (int i = 0; i < parts.Length - 1; i++)
+            {
+                if (isNumber(parts[i]))
+                {
+                    cct.Push(parts[i]);
+                }
+                if (isOperator2(parts[i]))
+                {
+
+                    secnum = cct.Pop();
+                    stnum = cct.Pop();
+                    resultback = calculate(parts[i], stnum, secnum, 4);
+                    cct.Push(resultback);
+                }
+            }
+
+            if (!(isNumber(parts[0]) && isOperator2(parts[1]) && isNumber(parts[2])))
+            {
+                return cct.Peek();
+
+            }
+            else
+            {
+                return "E";
+
+            }
+
         }
+    }
     }
 }
