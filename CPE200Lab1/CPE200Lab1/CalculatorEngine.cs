1992d653bf59635f6c4f115c37b23dcba44fec90
sasirat <sasirat019@gmail.com>
2018-09-25 02:39:18 +0700

    add documentation comment in CalculatorEngine object

--- CPE200Lab1/CPE200Lab1/CalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/CalculatorEngine.cs
@@ -6,14 +6,35 @@ using System.Threading.Tasks;
 
 namespace CPE200Lab1
 {
+    /// <summary>
+    /// Provide method for calculate string of number.
+    /// </summary>
     public class CalculatorEngine
     {
+        /// <summary>
+        /// Check that is a number.
+        /// </summary>
+        /// <param name="str">
+        /// The string of number.
+        /// </param>
+        /// <returns>
+        /// Return true if stirng is number, otherwise return false. 
+        /// </returns>
         public bool isNumber(string str)
         {
             double retNum;
             return Double.TryParse(str, out retNum);
         }
 
+        /// <summary>
+        /// Check that is an operator.
+        /// </summary>
+        /// <param name="str">
+        /// The string of operator
+        /// </param>
+        /// <returns>
+        /// Return true if the string is operator, otherwise return false.
+        /// </returns>
         public bool isOperator(string str)
         {
             switch(str) {
@@ -26,27 +47,36 @@ namespace CPE200Lab1
             return false;
         }
 
-        public string Process(string str)
-        {
-            string[] parts = str.Split(' ');
-            if(!(isNumber(parts[0]) && isOperator(parts[1]) && isNumber(parts[2])))
-            {
-                return "E";
-            } else
-            {
-                return calculate(parts[1], parts[0], parts[2], 4);
-            }
-
-        }
+        /// <summary>
+        /// Calculate single operand for square root and one over X.
+        /// </summary>
+        /// <param name="operate">
+        /// The operator for calculate.
+        /// </param>
+        /// <param name="operand">
+        /// The operand going to calculate.
+        /// </param>
+        /// <param name="maxOutputSize">
+        /// Defind maximum number of digits for result.
+        /// </param>
+        /// <returns>
+        /// The string of result.
+        /// </returns>
         public string unaryCalculate(string operate, string operand, int maxOutputSize = 8)
         {
             switch (operate)
             {
                 case "√":
+                    // Cannot use try-catch function in this case
+                    // because double can store NaN value
+                    if (Convert.ToDouble(operand) >= 0)
                     {
                         return FixDecimalPart(Math.Sqrt(Convert.ToDouble(operand)), maxOutputSize);
                     }
+                    break;
                 case "1/x":
+                    // cannot use try-catch function in thid case
+                    // because double can store infinity value
                     if(operand != "0")
                     {
                         return FixDecimalPart(1 / Convert.ToDouble(operand), maxOutputSize);
@@ -56,6 +86,18 @@ namespace CPE200Lab1
             return "E";
         }
 
+        /// <summary>
+        /// Fix the number of input is not more than maximum output size.
+        /// </summary>
+        /// <param name="result">
+        /// The input going to fix.
+        /// </param>
+        /// <param name="maxOutputSize">
+        /// Defind maximum of digits for input.
+        /// </param>
+        /// <returns>
+        /// The string of result that is not more than maximum output size.
+        /// </returns>
         private static string FixDecimalPart(double result, int maxOutputSize)
         {
             string[] parts;
@@ -85,6 +127,24 @@ namespace CPE200Lab1
             }
         }
 
+        /// <summary>
+        /// Calculate two number of string and return result. 
+        /// </summary>
+        /// <param name="operate">
+        /// The operator for calculate.
+        /// </param>
+        /// <param name="firstOperand">
+        /// The first operand going to calculate.
+        /// </param>
+        /// <param name="secondOperand">
+        /// The second operand going to calculate.
+        /// </param>
+        /// <param name="maxOutputSize">
+        /// Defind maximum number of digits for result.
+        /// </param>
+        /// <returns>
+        /// The string of result.
+        /// </returns>
         public string calculate(string operate, string firstOperand, string secondOperand, int maxOutputSize = 8)
         {
             switch (operate)
@@ -97,6 +157,8 @@ namespace CPE200Lab1
                     return (Convert.ToDouble(firstOperand) * Convert.ToDouble(secondOperand)).ToString();
                 case "÷":
                     // Not allow devide be zero
+                    // Cannot use try catch for catch this case
+                    // because Double can store infinity value 
                     if (secondOperand != "0")
                     {
                         return FixDecimalPart(Convert.ToDouble(firstOperand) / Convert.ToDouble(secondOperand), maxOutputSize);
