e767c7cb5710efcee182ae6a97dd68ff2974f1e2
600610794 <35362980+600610794@users.noreply.github.com>
2018-09-25 16:26:54 +0700

    complete

--- CPE200Lab1/CPE200Lab1/CalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/CalculatorEngine.cs
@@ -8,11 +8,23 @@ namespace CPE200Lab1
 {
     public class CalculatorEngine
     {
+        /// <summary>
+        /// Check string is Number
+        /// </summary>
+        /// <param name="str"> string for check </param>
+        /// <returns> true if str is Number and false if str is not Number </returns>
+         
         public bool isNumber(string str)
         {
             double retNum;
             return Double.TryParse(str, out retNum);
         }
+
+        /// <summary>
+        /// Check string is Operator
+        /// </summary>
+        /// <param name="str"> string for check </param>
+        /// <returns> true if str is Operator and false if str is not Operator </returns>
 
         public bool isOperator(string str)
         {
@@ -29,6 +41,12 @@ namespace CPE200Lab1
             return false;
         }
 
+        /// <summary>
+        /// input equation for calculate
+        /// </summary>
+        /// <param name="str"> equation for calculate </param>
+        /// <returns>result if is not error case and error if error case </returns>
+
         public string Process(string str)
         {
             string[] parts = str.Split(' ');
@@ -40,8 +58,17 @@ namespace CPE200Lab1
             {
                 return calculate(parts[1], parts[0], parts[2], 4);
             }
-
+            
         }
+
+        /// <summary>
+        /// input equation for calculate
+        /// </summary>
+        /// <param name="operate"> operator for calculate </param>
+        /// <param name="operand"> number for calculate </param>
+        /// <param name="maxOutputSize"> Length of result </param>
+        /// <returns> result of eauation </returns>
+
         public string unaryCalculate(string operate, string operand, int maxOutputSize = 8)
         {
             switch (operate)
@@ -53,16 +80,12 @@ namespace CPE200Lab1
                         int remainLength;
 
                         result = Math.Sqrt(Convert.ToDouble(operand));
-                        // split between integer part and fractional part
                         parts = result.ToString().Split('.');
-                        // if integer part length is already break max output, return error
                         if (parts[0].Length > maxOutputSize)
                         {
                             return "E";
                         }
-                        // calculate remaining space for fractional part.
                         remainLength = maxOutputSize - parts[0].Length - 1;
-                        // trim the fractional part gracefully. =
                         if((Convert.ToDouble(result)*10) % 2 == 0)
                         {
                             return result.ToString();
@@ -77,17 +100,17 @@ namespace CPE200Lab1
                         int remainLength;
 
                         result = (1.0 / Convert.ToDouble(operand));
-                        // split between integer part and fractional part
                         parts = result.ToString().Split('.');
-                        // if integer part length is already break max output, return error
                         if (parts[0].Length > maxOutputSize)
                         {
                             return "E";
                         }
-                        // calculate remaining space for fractional part.
                         remainLength = maxOutputSize - parts[0].Length - 1;
-                        // trim the fractional part gracefully. =
-                        if ((Convert.ToDouble(operand)*10) % 2 == 0)
+                        if (parts.Length <= 1)
+                        {
+                            return result.ToString();
+                        }
+                        else if (parts[1].Length < maxOutputSize)
                         {
                             return result.ToString();
                         }
@@ -97,6 +120,15 @@ namespace CPE200Lab1
             }
             return "E";
         }
+
+        /// <summary>
+        /// input equation for calculate
+        /// </summary>
+        /// <param name="operate"> operator for calculate </param>
+        /// <param name="firstOperand"> first number for calculate </param>
+        /// <param name="secondOperand"> second number for calculate </param>
+        /// <param name="maxOutputSize"> Length of result </param>
+        /// <returns> result of eauation </returns>
 
         public string calculate(string operate, string firstOperand, string secondOperand, int maxOutputSize = 8)
         {
@@ -109,7 +141,6 @@ namespace CPE200Lab1
                 case "X":
                     return (Convert.ToDouble(firstOperand) * Convert.ToDouble(secondOperand)).ToString();
                 case "÷":
-                    // Not allow devide be zero
                     if (secondOperand != "0")
                     {
                         double result;
@@ -117,17 +148,13 @@ namespace CPE200Lab1
                         int remainLength;
 
                         result = (Convert.ToDouble(firstOperand) / Convert.ToDouble(secondOperand));
-                        // split between integer part and fractional part
                         parts = result.ToString().Split('.');
-                        // if integer part length is already break max output, return error
                         if (parts[0].Length > maxOutputSize)
                         {
                             return "E";
                         }
-                        // calculate remaining space for fractional part.
                         remainLength = maxOutputSize - parts[0].Length - 1;
-                        // trim the fractional part gracefully. =
-                        if (Convert.ToDouble(firstOperand) % Convert.ToDouble(secondOperand) == 0)
+                        if (parts.Length < 2  || parts[1].Length < maxOutputSize)
                         {
                             return result.ToString();
                         }
--- CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
@@ -11,6 +11,13 @@ namespace CPE200Lab1
         string result;
         string secondNum;
         string firstNum;
+
+        /// <summary>
+        /// input equation for calculate
+        /// </summary>
+        /// <param name="str"> equation for calculate </param>
+        /// <returns> result if is not error case and error if error case </returns>
+
         public string Process(string str)
         {
 
@@ -25,32 +32,36 @@ namespace CPE200Lab1
                 }
                 if (isOperator(parts[i]))
                 {
-                    if (parts[i] == "1/x" || parts[i] == "√")
+                    try
                     {
-                        firstNum = Set.Pop();
-                        Set.Push(unaryCalculate(parts[i], firstNum));
+                        if (parts[i] == "1/x" || parts[i] == "√")
+                        {
+                                firstNum = Set.Pop();
+                                Set.Push(unaryCalculate(parts[i], firstNum));
+                        }
+                        else if (parts[i] == "%" && Set.Count > 1)
+                        {
+                                secondNum = Set.Pop();
+                                firstNum = Set.Peek();
+                                Set.Push(calculate(parts[i], firstNum, secondNum));
+                        }
+                        else if (parts[i] == "%")
+                        {
+                                firstNum = Set.Pop();
+                                Set.Push(calculate(parts[i], firstNum, 1.ToString()));
+                        }
+                        else
+                        {
+                                secondNum = Set.Pop();
+                                firstNum = Set.Pop();
+                                Set.Push(calculate(parts[i], firstNum, secondNum));
+                        }   
                     }
-                    else if (parts[i] == "%" && Set.Count > 1)
-                    {
-                        secondNum = Set.Pop();
-                        firstNum = Set.Peek();
-                        Set.Push(calculate(parts[i], firstNum, secondNum));
-                    }
-                    else if (parts[i] == "%")
-                    {
-                        firstNum = Set.Pop();
-                        Set.Push(calculate(parts[i], firstNum, 1.ToString()));
-                    }
-                    else if (Set.Count < 1)
+                    catch (Exception e)
                     {
                         return "E";
                     }
-                    else
-                    {
-                        secondNum = Set.Pop();
-                        firstNum = Set.Pop();
-                        Set.Push(calculate(parts[i], firstNum, secondNum));
-                    }
+                    
                 }
 
             }
