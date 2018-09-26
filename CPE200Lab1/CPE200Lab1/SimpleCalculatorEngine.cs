736df444589a6a12ac1aca1af6122c66bf655c0c
VivereGrau <pratompongwiangsamut@gmail.com>
2018-09-26 17:12:35 +0700

    add new class lab3+lab4

--- CPE200Lab1/CPE200Lab1/SimpleCalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/SimpleCalculatorEngine.cs
@@ -6,39 +6,22 @@ using System.Threading.Tasks;
 
 namespace CPE200Lab1
 {
-    public class CalculatorEngine
+    public class SimpleCalculatorEngine : CalculatorEngine
     {
-        protected bool isNumber(string str)
-        {
-            double retNum;
-            return Double.TryParse(str, out retNum);
-        }
-
-        protected bool isOperator(string str)
-        {
-            switch(str) {
-                case "+":
-                case "-":
-                case "X":
-                case "÷":
-                    return true;
-            }
-            return false;
-        }
-
         public string Process(string str)
         {
             //Split input string to multiple parts by space
             List<string> parts = str.Split(' ').ToList<string>();
             string result;
             //As long as we have more than one part
-            while(parts.Count > 1)
+            while (parts.Count > 1)
             {
                 //Check if the first three is ready for calcuation
-                if(!(isNumber(parts[0]) && isOperator(parts[1]) && isNumber(parts[2])))
+                if (!(isNumber(parts[0]) && isOperator(parts[1]) && isNumber(parts[2])))
                 {
                     return "E";
-                } else
+                }
+                else
                 {
                     //Calculate the first three
                     result = calculate(parts[1], parts[0], parts[2], 4);
@@ -50,7 +33,7 @@ namespace CPE200Lab1
             }
             return parts[0];
         }
-        public string Calculate(string operate, string operand, int maxOutputSize = 8)
+        public string unaryCalculate(string operate, string operand, int maxOutputSize = 8)
         {
             switch (operate)
             {
@@ -136,5 +119,6 @@ namespace CPE200Lab1
             }
             return "E";
         }
+
     }
 }
