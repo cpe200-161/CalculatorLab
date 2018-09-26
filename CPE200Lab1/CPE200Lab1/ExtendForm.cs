96a1b00bb834213995139e24d4f87a92025ce169
Poom <lovepoommi@gmail.com>
2018-09-26 13:23:41 +0700

    complete all test case

--- CPE200Lab1/CPE200Lab1/ExtendForm.cs
+++ CPE200Lab1/CPE200Lab1/ExtendForm.cs
@@ -63,7 +63,11 @@ namespace CPE200Lab1
             isNumberPart = false;
             isContainDot = false;
             string current = lblDisplay.Text;
-            if (current[current.Length - 1] != ' ' || isOperator(current[current.Length - 2]))
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = ((Button)sender).Text + " ";
+            }
+            else if (current[current.Length - 1] != ' ' || isOperator(current[current.Length - 2]))
             {
                 lblDisplay.Text += " " + ((Button)sender).Text + " ";
                 isSpaceAllowed = false;
