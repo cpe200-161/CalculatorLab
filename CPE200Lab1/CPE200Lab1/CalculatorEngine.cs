2a6f48b9812b23b6404ab4f18e8c512981d35963
NOPPANUT CHANTARAMANEE <booklet2541@gmail.com>
2018-09-26 16:31:15 +0700

    add try and catch

--- CPE200Lab1/CPE200Lab1/CalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/CalculatorEngine.cs
@@ -114,25 +114,32 @@ namespace CPE200Lab1
                     return (Convert.ToDouble(firstOperand) * Convert.ToDouble(secondOperand)).ToString();
                 case "÷":
                     // Not allow devide be zero
-                     if(secondOperand != "0")
+                    try
                     {
-                        double result;
-                        string[] parts;
-                        int remainLength;
-
-                        result = (Convert.ToDouble(firstOperand) / Convert.ToDouble(secondOperand));
-                        // split between integer part and fractional part
-                        parts = result.ToString().Split('.');
-                        // if integer part length is already break max output, return error
-                        if (parts[0].Length > maxOutputSize)
+                        if (secondOperand != "0")
                         {
-                            return "E";
+                            double result;
+                            string[] parts;
+                            int remainLength;
+
+                            result = (Convert.ToDouble(firstOperand) / Convert.ToDouble(secondOperand));
+                            // split between integer part and fractional part
+                            parts = result.ToString().Split('.');
+                            // if integer part length is already break max output, return error
+                            if (parts[0].Length > maxOutputSize)
+                            {
+                                return "E";
+                            }
+                            // calculate remaining space for fractional part.
+                            remainLength = maxOutputSize - parts[0].Length - 1;
+                            // trim the fractional part gracefully. =
+                            return decimal.Parse(result.ToString("N" + remainLength)).ToString("G29");
                         }
-                        // calculate remaining space for fractional part.
-                        remainLength = maxOutputSize - parts[0].Length - 1;
-                        // trim the fractional part gracefully. =
-                        return decimal.Parse(result.ToString("N" + remainLength)).ToString("G29");
-                    } 
+                    }
+                    catch (OverflowException)
+                    {
+                        return "E";
+                    }
                     break;
                 case "%":
                     //your code here
