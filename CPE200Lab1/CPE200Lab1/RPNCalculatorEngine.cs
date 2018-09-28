42b74475ed121ddcec53c9697593e3790b573d51
petpusin <pet_ray@windowslive.com>
2018-09-28 17:59:13 +0700

    FINNISH

--- CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
@@ -6,12 +6,41 @@ using System.Threading.Tasks;
 
 namespace CPE200Lab1
 {
-    public class RPNCalculatorEngine
+    public class RPNCalculatorEngine : CalculatorEngine
     {
-        public string Process(string str)
+        public override string Process(string str)
         {
             // your code here
-            return "E";
+            Stack<string> rpnStack = new Stack<string>();
+            List<string> parts = str.Split(' ').ToList<string>();
+            string result;
+            string firstOperand, secondOperand;
+
+            foreach (string i in parts)
+            {
+                if (isNumber(i))
+                {
+                    rpnStack.Push(i);
+                }
+                else if (isOperator(i))
+                {
+                    
+                    secondOperand = rpnStack.Pop();
+                    firstOperand = rpnStack.Pop();
+                    result = calculate(i, firstOperand, secondOperand, 4);
+                    if (result is "E")
+                    {
+                        return result;
+                    }
+                    rpnStack.Push(result);
+                }
+            }
+            if (rpnStack.Count == 1)
+            {
+                result = rpnStack.Pop();
+                return result;
+            }else
+                return "E";
         }
     }
 }
