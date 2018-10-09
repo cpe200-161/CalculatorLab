91ed29302a45927f30c0f292d77bc3049d2b579c
wogwhale <35363093+wongwhale@users.noreply.github.com>
2018-10-09 14:56:19 +0700

    Create SimpleCalculatorEngine. Complete MainForm , RPNCalculatorEngine.

--- CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
@@ -8,9 +8,10 @@ namespace CPE200Lab1
 {
     public class RPNCalculatorEngine : CalculatorEngine
     {
+        protected Stack<string> Numbers;
         public string Process(string str)
         {
-            Stack<string> Numbers = new Stack<string>();
+            Numbers = new Stack<string>();
             string[] part = str.Split(' ');
             foreach (string text in part)
             {
@@ -22,9 +23,17 @@ namespace CPE200Lab1
                 {
                     if (Numbers.Count >= 2 && text == "+" || text == "-" || text == "X" || text == "÷" || text == "%")
                     {
-                        string secondOperand = Numbers.Pop();
-                        string firstOperand = Numbers.Pop();
-                        Numbers.Push(calculate(text, firstOperand, secondOperand));
+                        try
+                        {
+                            string secondOperand = Numbers.Pop();
+                            string firstOperand = Numbers.Pop();
+                            Numbers.Push(calculate(text, firstOperand, secondOperand));
+                        }
+                        catch (Exception)
+                        {
+                            return "E";
+                        }
+                        
                     }
                     else if (Numbers.Count >= 1 && text == "√" || text == "1/x")
                     {
