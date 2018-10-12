c5c42cd14ad1a05192f9814f16286ce3ab572437
MSUVI <narongsangaroon@gmail.com>
2018-10-12 13:05:13 +0700

    2169

--- CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
@@ -8,7 +8,7 @@ namespace CPE200Lab1
 {
     public class RPNCalculatorEngine : CalculatorEngine
     {
-        public  object result;
+        //public  object result;
 
         public override string Process(string str)
         {
@@ -17,7 +17,9 @@ namespace CPE200Lab1
             {
                 return "E";
             }
-            
+            else
+            {
+
             string[] parts = str.Split(' ');
 
             Stack<string> operands = new Stack<string>();
@@ -43,14 +45,51 @@ namespace CPE200Lab1
                       }
                     operands.Push(result);
                 }
+                else
+                    {
+                        string NumberOrOperator = parts[i];
+                        for(int j = 0;j<NumberOrOperator.Length;j++)
+                        {
+                            if(isOperator(NumberOrOperator[j].ToString()) && NumberOrOperator[j] != '-')
+                            {
+                                return "E";
+                            }
+                        }
+                }
             }
             //FIXME, what if there is more than one, or zero, items in the stack? 
-            if (operands.Count > 1)
+            if (operands.Count != 1)
             {
                 return "E";
             }
-            try
+            else if (operands.Count == 1)
             {
+                    string number = operands.Pop();
+                    string NumberReturn = number;
+
+                    for (int i = 0; i<number.Length;i++)
+                    {
+                        if (isOperator(number[i].ToString()) && number[0] != '-')
+                            return "E";
+                    }
+
+                    string[] Checktxt = number.Split('.');
+                    string Decimals;
+                    int Decimalsint;
+                    if(Checktxt.Length>1 && isNumber(Checktxt[1]) && Checktxt[1].Length > 4)
+                    {
+                        Decimals = Checktxt[1].Substring(0, 5);
+                        if (Int32.Parse(Decimals[4].ToString())>5)
+                        {
+                            Decimalsint = Int32.Parse(Checktxt[1].Substring(0, 4));
+                            Decimalsint++;
+                            Decimals = Decimalsint.ToString();
+                        }
+                        else
+                        {
+
+                        }
+                    }
                 return operands.Pop();
             }
             catch
