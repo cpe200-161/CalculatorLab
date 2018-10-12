f0f61749c5aa5da29bf68cacd10490c2641e9a19
PAKHATAI KANTANG <pakhatai_kantang@cmu.ac.th>
2018-10-12 15:01:18 +0700

    600612161

--- CPE200Lab1/CPE200Lab1/CalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/CalculatorEngine.cs
@@ -8,36 +8,17 @@ namespace CPE200Lab1
 {
     public class CalculatorEngine
     {
-        protected bool isNumber(string str)
-        {
-            double retNum;
-            return Double.TryParse(str, out retNum);
-        }
-
-        protected bool isOperator(string str)
-        {
-            switch (str)
-            {
-                case "+":
-                case "-":
-                case "X":
-                case "÷":
-                    return true;
-            }
-            return false;
-        }
-
         public virtual string Process(string str)
         {
             //Split input string to multiple parts by space
-            
+
             List<string> parts = str.Split(' ').ToList<string>();
             string result;
             //As long as we have more than one part
             if (parts.Count == 1)
             {
                 result = parts[0];
-                for(int i = 0; i < result.Length; i++)
+                for (int i = 0; i < result.Length; i++)
                 {
                     if (isOperator(result[i].ToString()))
                     {
@@ -63,57 +44,29 @@ namespace CPE200Lab1
                 }
             }
             return parts[0];
-           
+
         }
-        public string unaryCalculate(string operate, string operand, int maxOutputSize = 8)
+        protected bool isNumber(string str)
         {
-            switch (operate)
-            {
-                case "√":
-                    {
-                        double result;
-                        string[] parts;
-                        int remainLength;
-
-                        result = Math.Sqrt(Convert.ToDouble(operand));
-                        // split between integer part and fractional part
-                        parts = result.ToString().Split('.');
-                        // if integer part length is already break max output, return error
-                        if (parts[0].Length > maxOutputSize)
-                        {
-                            return "E";
-                        }
-                        // calculate remaining space for fractional part.
-                        remainLength = maxOutputSize - parts[0].Length - 1;
-                        // trim the fractional part gracefully. =
-                        return result.ToString("G2" + remainLength);
-                    }
-                case "1/x":
-                    if (operand != "0")
-                    {
-                        double result;
-                        string[] parts;
-                        int remainLength;
-
-                        result = (1.0 / Convert.ToDouble(operand));
-                        // split between integer part and fractional part
-                        parts = result.ToString().Split('.');
-                        // if integer part length is already break max output, return error
-                        if (parts[0].Length > maxOutputSize)
-                        {
-                            return "E";
-                        }
-                        // calculate remaining space for fractional part.
-                        remainLength = maxOutputSize - parts[0].Length - 1;
-                        // trim the fractional part gracefully. =
-
-                        return result.ToString("G2" + remainLength);
-                    }
-                    break;
-            }
-            return "E";
+            double retNum;
+            return Double.TryParse(str, out retNum);
         }
 
+        protected bool isOperator(string str)
+        {
+            switch (str)
+            {
+                case "+":
+                case "-":
+                case "X":
+                case "÷":
+                    return true;
+            }
+            return false;
+        }
+
+      
+        
         public string calculate(string operate, string firstOperand, string secondOperand, int maxOutputSize = 8)
         {
             switch (operate)
--- CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
@@ -39,18 +39,58 @@ namespace CPE200Lab1
                     }
                     operands.Push(result);
                 }
-                
-        
+                else
+                {
+                    string Errnum = parts[i];
+                    for (int j = 0; j < Errnum.Length; j++)
+                    {
+                        if (isOperator(Errnum[j].ToString()) && Errnum[j] != '-')
+                        {
+                            return "E";
+                        }
+                    }
+                }
+
+
             //FIXME, what if there is more than one, or zero, items in the stack?
-         
-              if (operands.Count > 1)
+
+            if (operands.Count != 1)
             {
                 return "E";
             }
 
-                return operands.Pop();
+            else if (operands.Count == 1)
+            {
+                string number = operands.Pop();
+                string numberReturn = number;
+                for (int i = 0; i < number.Length; i++)
+                {
+                    if (isOperator(number[i].ToString()) && number[0] != '-')
+                        return "E";
+                }
+                string[] checktxt = number.Split('.');
+                string decimals;
+                int decimalsint;
+                if (checktxt.Length > 1 && isNumber(checktxt[1]) && checktxt[1].Length > 4)
+                {
+                    decimals = checktxt[1].Substring(0, 5);
+                    if (Int32.Parse(decimals[4].ToString()) > 5)
+                    {
+                        decimalsint = Int32.Parse(checktxt[1].Substring(0, 4));
+                        decimalsint++;
+                        decimals = decimalsint.ToString();
+                    }
+                    else
+                    {
+                        decimals = checktxt[1].Substring(0, 4);
+                    }
+                    numberReturn = checktxt[0] + "." + decimals;
+                }
+                operands.Push(numberReturn);
             }
-
+            return operands.Pop();
+        }
+    }
     /*public override void handleSpace()
     {
         base.handleSpace();
@@ -60,5 +100,5 @@ namespace CPE200Lab1
     }*/
 }
     
-}
+
 
