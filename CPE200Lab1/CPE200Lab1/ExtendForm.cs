35f836ec58baa8dcd393e1956dafef513393986f
TANANPORN YANA <tananporn_yana@cmu.ac.th>
2018-11-23 14:11:12 +0700

    Complete add MVC & Observer Pattern

--- CPE200Lab1/CPE200Lab1/ExtendForm.cs
+++ CPE200Lab1/CPE200Lab1/ExtendForm.cs
@@ -10,17 +10,27 @@ using System.Windows.Forms;
 
 namespace CPE200Lab1
 {
-    public partial class ExtendForm : Form
+    public partial class ExtendForm : Form, View
     {
         private bool isNumberPart = false;
         private bool isContainDot = false;
         private bool isSpaceAllowed = false;
         private RPNCalculatorEngine RPN;
+            Model model;
+            Controller controller;
 
         public ExtendForm()
         {
             InitializeComponent();
+            model = new CalculatorModel();
+            controller = new CalculatorController();
+            model.AttachObserver(this);
+            controller.AddModel(model);
             RPN = new RPNCalculatorEngine();
+        }
+        public void Notify(Model m)
+        {
+            lblDisplay.Text = ((CalculatorModel)m).Display();
         }
 
         private bool isOperator(char ch)
