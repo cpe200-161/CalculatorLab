9cb0d2e621ce8dbfa5e0bc62ef40d7a8962f8ca3
Teeradon <teamhd35975@gmail.com>
2018-11-24 17:28:17 +0700

    lab4 finished

--- CPE200Lab1/CPE200Lab1/ExtendForm.cs
+++ CPE200Lab1/CPE200Lab1/ExtendForm.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace CPE200Lab1
 {
     public partial class ExtendForm : Form
--- CPE200Lab1/CPE200Lab1/Program.cs
+++ CPE200Lab1/CPE200Lab1/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace CPE200Lab1
 {
     static class Program
