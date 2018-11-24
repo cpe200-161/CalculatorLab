585a39b03cb798cbf8be38f2b46aae02012b2573
ศุภกร พันธุ์พานิช <machgo008@hotmail.com>
2018-11-24 17:34:26 +0700

    011-600611037-LAB1-HW1

--- CPE200Lab1/CPE200Lab1/CalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/CalculatorEngine.cs
@@ -6,8 +6,32 @@ using System.Threading.Tasks;
 
 namespace CPE200Lab1
 {
+
     class CalculatorEngine
     {
+        private bool hasDot;
+        private bool isAllowBack;
+        private bool isAfterOperater;
+        private bool isAfterEqual;
+        private string firstOperand;
+        private string operate;
+
+        private string display = "0";
+
+        public string Display()
+        {
+            return display;
+        }
+
+        public void resetAll()
+        {
+            isAllowBack = true;
+            hasDot = false;
+            isAfterOperater = false;
+            isAfterEqual = false;
+            display = "0";
+        }
+
         public string calculate(string operate, string firstOperand, string secondOperand, int maxOutputSize = 8)
         {
             switch (operate)
@@ -42,44 +66,152 @@ namespace CPE200Lab1
                     break;
                 case "%":
                     //your code here
-                    return (((Convert.ToDouble(secondOperand)) / 100) * Convert.ToDouble(firstOperand)).ToString();
-                //break;
-                case "√":
-                    double result2;
-                    string[] parts2;
-                    int remainLength2;
+                    return ((Convert.ToDouble(firstOperand) * Convert.ToDouble(secondOperand)) / 100).ToString();
 
-                    result2 = (Math.Sqrt(Convert.ToDouble(firstOperand)));
-                    // split between integer part and fractional part
-                    parts2 = result2.ToString().Split('.');
-                    // if integer part length is already break max output, return error
-                    if (parts2[0].Length > maxOutputSize)
-                    {
-                        return "E";
-                    }
-                    // calculate remaining space for fractional part.
-                    remainLength2 = maxOutputSize - parts2[0].Length - 1;
-                    // trim the fractional part gracefully. =
-                    return result2.ToString("N" + remainLength2);
-                case "1/X":
-                    double result3;
-                    string[] parts3;
-                    int remainLength3;
-
-                    result3 = (1 / Convert.ToDouble(firstOperand));
-                    // split between integer part and fractional part
-                    parts3 = result3.ToString().Split('.');
-                    // if integer part length is already break max output, return error
-                    if (parts3[0].Length > maxOutputSize)
-                    {
-                        return "E";
-                    }
-                    // calculate remaining space for fractional part.
-                    remainLength3 = maxOutputSize - parts3[0].Length - 1;
-                    // trim the fractional part gracefully. =
-                    return result3.ToString("N" + remainLength3);
+                    break;
             }
             return "E";
         }
+
+        public void handleSign()
+        {
+            if (isAfterEqual)
+            {
+                resetAll();
+            }
+            // already contain negative sign
+            if (display.Length is 8)
+            {
+                return;
+            }
+            if (display[0] is '-')
+            {
+                display = display.Substring(1, display.Length - 1);
+            }
+            else
+            {
+                display = "-" + display;
+            }
+        }
+        string digit;
+        public void swit(string dig)
+        {
+            digit = dig;
+        }
+        public void Num()
+        {
+            if (isAfterEqual)
+            {
+                resetAll();
+            }
+            if (isAfterOperater)
+            {
+                display = "0";
+            }
+            if (display.Length is 8)
+            {
+                return;
+            }
+            isAllowBack = true;
+
+            if (display is "0")
+            {
+                display = "";
+            }
+
+            display += digit;
+
+            isAfterOperater = false;
+        }
+        public void switop(string op)
+        {
+            operate = op;
+        }
+
+        public void Operlator()
+        {
+            if (isAfterOperater)
+            {
+                return;
+            }
+
+            switch (operate)
+            {
+                case "+":
+                case "-":
+                case "X":
+                case "÷":
+                    firstOperand = display;
+                    isAfterOperater = true;
+                    break;
+                case "%":
+                    // your code here
+                    Convert.ToDouble(firstOperand);
+                    break;
+            }
+            isAllowBack = false;
+        }
+        public void Dot()
+        {
+            if (isAfterEqual)
+            {
+                resetAll();
+            }
+            if (display.Length is 8)
+            {
+                return;
+            }
+            if (!hasDot)
+            {
+                display += ".";
+                hasDot = true;
+            }
+        }
+        public void Back()
+        {
+            if (isAfterEqual)
+            {
+                return;
+            }
+            if (!isAllowBack)
+            {
+                return;
+            }
+            if (display != "0")
+            {
+                string current = display;
+
+                char rightMost = current[current.Length - 1];
+                if (rightMost is '.')
+                {
+                    hasDot = false;
+                }
+                display = current.Substring(0, current.Length - 1);
+
+                if (display is "" || display is "-")
+                {
+                    display = "0";
+                }
+            }
+        }
+        public void Equal()
+        {
+            string secondOperand = display;
+
+            string result = calculate(operate, firstOperand, secondOperand);
+            if (result is "E" || result.Length > 8)
+            {
+                display = "Error";
+            }
+            else
+            {
+                display = result;
+            }
+            isAfterEqual = true;
+        }
+
+
+
     }
+
 }
